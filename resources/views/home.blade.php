7750f81daf4f1bec45ddb62a7fa14f2a980d4616
Nabil Hassen <nabil.hassen08@gmail.com>
2020-10-30 16:33:35 +0300

    Modify dashboard cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,84 +3,66 @@
 @section('content')
 <div class="columns is-marginless">
     <div class="column">
-        <div class="box">
-            <div class="level">
-                <div class="level-left">
-                    <div class="level-item">
-                        <span class="icon is-large is-size-1 text-green">
-                            <i class="fas fa-truck-loading"></i>
-                        </span>
-                    </div>
+        <div class="box text-green">
+            <div class="columns is-marginless is-vcentered is-mobile">
+                <div class="column is-paddingless">
+                    <span class="icon is-large is-size-1">
+                        <i class="fas fa-truck-loading"></i>
+                    </span>
                 </div>
-                <div class="level-left">
-                    <div class="level-item">
-                        <span class="text-green is-size-6 has-text-weight-bold">
-                            235 
-                            <br>
-                            <span class="is-uppercase is-size-7">
-                                Products
-                            </span>
-                            <br><br>
-                            <span class="text-green is-size-7 is-uppercase has-text-weight-light">
-                                Raw Materials Reorder
-                            </span>
-                        </span>
+                <div class="column is-paddingless">
+                    <div class="is-size-3 has-text-weight-bold">
+                        59
+                    </div>
+                    <div class="is-uppercase is-size-7">
+                        Material Types
+                    </div>
+                    <div class="is-size-7 is-uppercase has-text-weight-light mt-5">
+                        Raw Materials To Reorder
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="column">
-        <div class="box">
-            <div class="level">
-                <div class="level-left">
-                    <div class="level-item">
-                        <span class="icon is-large is-size-1 text-purple">
-                            <i class="fas fa-check"></i>
-                        </span>
-                    </div>
+        <div class="box text-purple">
+            <div class="columns is-marginless is-vcentered is-mobile">
+                <div class="column is-paddingless">
+                    <span class="icon is-large is-size-1">
+                        <i class="fas fa-check"></i>
+                    </span>
                 </div>
-                <div class="level-left">
-                    <div class="level-item">
-                        <span class="text-purple is-size-6 has-text-weight-bold">
-                            235
-                            <br>
-                            <span class="is-uppercase is-size-7">
-                                Product Types
-                            </span>
-                            <br><br>
-                            <span class="text-purple is-size-7 is-uppercase has-text-weight-light">
-                                Available Finished Products
-                            </span>
-                        </span>
+                <div class="column is-paddingless">
+                    <div class="is-size-3 has-text-weight-bold">
+                        13
+                    </div>
+                    <div class="is-uppercase is-size-7">
+                        Product Types
+                    </div>
+                    <div class="is-size-7 is-uppercase has-text-weight-light mt-5">
+                        Available Finished Products
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="column">
-        <div class="box">
-            <div class="level">
-                <div class="level-left">
-                    <div class="level-item">
-                        <span class="icon is-large is-size-1 text-gold">
-                            <i class="fas fa-spinner"></i>
-                        </span>
-                    </div>
+        <div class="box text-gold">
+            <div class="columns is-marginless is-vcentered is-mobile">
+                <div class="column is-paddingless">
+                    <span class="icon is-large is-size-1">
+                        <i class="fas fa-spinner"></i>
+                    </span>
                 </div>
-                <div class="level-left">
-                    <div class="level-item">
-                        <span class="text-gold is-size-6 has-text-weight-bold">
-                            235
-                            <br>
-                            <span class="is-uppercase is-size-7">
-                                Product Types
-                            </span>
-                            <br><br>
-                            <span class="text-gold is-size-7 is-uppercase has-text-weight-light">
-                                Products Being Processed
-                            </span>
-                        </span>
+                <div class="column is-paddingless">
+                    <div class="is-size-3 has-text-weight-bold">
+                        27
+                    </div>
+                    <div class="is-uppercase is-size-7">
+                        Product Types
+                    </div>
+                    <div class="is-size-7 is-uppercase has-text-weight-light mt-5">
+                        Products Being Processed
                     </div>
                 </div>
             </div>
