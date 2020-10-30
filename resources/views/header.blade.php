92a492bf9d908496a661fe9c62438a287c6b727e
Nabil Hassen <nabil.hassen08@gmail.com>
2020-10-30 16:45:17 +0300

    make navbar responsive

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,16 +1,22 @@
 <nav class="navbar is-fixed-top is-transparent bg-green">
-    <div class="container is-fluid">
+    <div class="container is-fluid p-lr-0">
         <div class="navbar-brand">
-            <a class="navbar-item" href="/">
+            <a class="navbar-item is-hidden-touch" href="/">
                 <img src="{{ asset('img/logo.png') }}" width="120" style="max-height: 70px">
                 <span class="has-text-grey-lighter has-text-weight-light is-size-4 mb-1">
                     Inventory
                 </span>
             </a>
-            <a class="navbar-burger burger">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
+            <a class="navbar-item is-hidden-desktop" href="#">
+                <img src="{{ asset('img/logo.png') }}" style="max-height: 40px">
+                <span class="has-text-grey-lighter is-size-6">
+                    Inventories
+                </span>
+            </a>
+            <a id="burger-menu" class="navbar-item to-the-right has-text-white is-size-5 is-hidden-desktop">
+                <span class="icon">
+                    <i class="fas fa-bars"></i>
+                </span>
             </a>
         </div>
         <div class="navbar-menu">
