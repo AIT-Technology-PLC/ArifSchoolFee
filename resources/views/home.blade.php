e4f3c37b4dee54e6cc52f34372982c08a75f27a3
Nabil Hassen <nabil.hassen08@gmail.com>
2020-10-31 11:35:52 +0300

    add card in first row

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="columns is-marginless">
-    <div class="column is-4">
+    <div class="column is-3">
         <div class="box text-green">
             <div class="columns is-marginless is-vcentered is-mobile">
                 <div class="column has-text-centered is-paddingless">
@@ -18,13 +18,13 @@
                         Material Types
                     </div>
                     <div class="is-size-7 is-uppercase has-text-weight-light mt-5">
-                        Raw Materials To Reorder
+                        Out of Stock <br> Raw Materials
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="column is-4">
+    <div class="column is-3">
         <div class="box text-purple">
             <div class="columns is-marginless is-vcentered is-mobile">
                 <div class="column has-text-centered is-paddingless">
@@ -40,13 +40,13 @@
                         Product Types
                     </div>
                     <div class="is-size-7 is-uppercase has-text-weight-light mt-5">
-                        Available Finished Products
-                    </div>
+                        Available <br> Finished Goods
+                    </>
                 </div>
             </div>
         </div>
     </div>
-    <div class="column is-4">
+    <div class="column is-3">
         <div class="box text-gold">
             <div class="columns is-marginless is-vcentered is-mobile">
                 <div class="column has-text-centered is-paddingless">
@@ -62,7 +62,29 @@
                         Product Types
                     </div>
                     <div class="is-size-7 is-uppercase has-text-weight-light mt-5">
-                        Products Being Processed
+                        In Process <br> Products
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="column is-3">
+        <div class="box text-green">
+            <div class="columns is-marginless is-vcentered is-mobile">
+                <div class="column has-text-centered is-paddingless">
+                    <span class="icon is-large is-size-1">
+                        <i class="fas fa-tools"></i>
+                    </span>
+                </div>
+                <div class="column is-paddingless">
+                    <div class="is-size-3 has-text-weight-bold">
+                        27
+                    </div>
+                    <div class="is-uppercase is-size-7">
+                        Items
+                    </div>
+                    <div class="is-size-7 is-uppercase has-text-weight-light mt-5">
+                        Available <br> MRO Items
                     </div>
                 </div>
             </div>
