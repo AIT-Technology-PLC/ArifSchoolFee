7d8a41b84692af402b2fccaac5d8b6703fb5da58
Nabil Hassen <nabil.hassen08@gmail.com>
2020-10-31 12:13:07 +0300

    add merchandise data cards in second row

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -92,6 +92,75 @@
     </div>
 </div>
 
+<div class="columns is-marginless">
+    <div class="column is-4">
+        <div class="box text-green">
+            <div class="columns is-marginless is-vcentered is-mobile">
+                <div class="column has-text-centered is-paddingless">
+                    <span class="icon is-large is-size-1">
+                        <i class="fas fa-check-circle"></i>
+                    </span>
+                </div>
+                <div class="column is-paddingless">
+                    <div class="is-size-3 has-text-weight-bold">
+                        59
+                    </div>
+                    <div class="is-uppercase is-size-7">
+                        Products
+                    </div>
+                    <div class="is-size-7 is-uppercase mt-5 has-text-grey">
+                        Available in Stock
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="column is-4">
+        <div class="box text-purple">
+            <div class="columns is-marginless is-vcentered is-mobile">
+                <div class="column has-text-centered is-paddingless">
+                    <span class="icon is-large is-size-1">
+                        <i class="fas fa-times-circle"></i>
+                    </span>
+                </div>
+                <div class="column is-paddingless">
+                    <div class="is-size-3 has-text-weight-bold">
+                        13
+                    </div>
+                    <div class="is-uppercase is-size-7">
+                        Products
+                    </div>
+                    <div class="is-size-7 is-uppercase mt-5 has-text-grey">
+                        Out Of Stock
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="column is-4">
+        <div class="box text-gold">
+            <div class="columns is-marginless is-vcentered is-mobile">
+                <div class="column has-text-centered is-paddingless">
+                    <span class="icon is-large is-size-1">
+                        <i class="fas fa-exclamation-circle"></i>
+                    </span>
+                </div>
+                <div class="column is-paddingless">
+                    <div class="is-size-3 has-text-weight-bold">
+                        27
+                    </div>
+                    <div class="is-uppercase is-size-7">
+                        Products
+                    </div>
+                    <div class="is-size-7 is-uppercase mt-5 has-text-grey">
+                        Limited Stock
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="columns is-marginless is-vcentered">
     <div class="column is-8">
         <div class="box">
