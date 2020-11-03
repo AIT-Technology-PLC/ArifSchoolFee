e79f6f1d4429deef3b15a07bc1f18a7193a8e433
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-03 14:54:15 +0300

    reorder rows

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -100,6 +100,48 @@ Dashboard - Onrica Inventory
     </div>
 </div>
 
+<div class="columns is-marginless is-vcentered">
+    <div class="column is-6">
+        <div class="box mb-0 radius-bottom-0 pb-1">
+            <p class="text-green has-text-weight-medium">
+                Stock Level (%)
+            </p>
+            <p class="is-size-7 has-text-grey mb-4">
+                Manufacturing Inventory
+            </p>
+        </div>
+        <div class="box radius-top-0">
+            {!! $chartjs->render() !!}
+        </div>
+    </div>
+    <div class="column is-3">
+        <div class="box mb-0 radius-bottom-0 pb-1">
+            <p class="text-green has-text-weight-medium">
+                Finished Products (%)
+            </p>
+            <p class="is-size-7 has-text-grey mb-4">
+                Manufacturing Inventory - Top 5
+            </p>
+        </div>
+        <div class="box radius-top-0">
+            {!! $pie2->render() !!}
+        </div>
+    </div>
+    <div class="column is-3">
+        <div class="box mb-0 radius-bottom-0 pb-1">
+            <p class="text-green has-text-weight-medium">
+                Raw Materials (%)
+            </p>
+            <p class="is-size-7 has-text-grey mb-4">
+                Manufacturing Inventory - Top 5
+            </p>
+        </div>
+        <div class="box radius-top-0">
+            {!! $pie->render() !!}
+        </div>
+    </div>
+</div>
+
 <div class="columns is-marginless">
     <div class="column is-3">
         <div class="box text-green">
@@ -191,48 +233,6 @@ Dashboard - Onrica Inventory
             <div class="is-size-7 is-uppercase has-text-grey">
                 Returned By Clients
             </div>
-        </div>
-    </div>
-</div>
-
-<div class="columns is-marginless is-vcentered">
-    <div class="column is-6">
-        <div class="box mb-0 radius-bottom-0 pb-1">
-            <p class="text-green has-text-weight-medium">
-                Stock Level (%)
-            </p>
-            <p class="is-size-7 has-text-grey mb-4">
-                Manufacturing Inventory
-            </p>
-        </div>
-        <div class="box radius-top-0">
-            {!! $chartjs->render() !!}
-        </div>
-    </div>
-    <div class="column is-3">
-        <div class="box mb-0 radius-bottom-0 pb-1">
-            <p class="text-green has-text-weight-medium">
-                Finished Products (%)
-            </p>
-            <p class="is-size-7 has-text-grey mb-4">
-                Manufacturing Inventory - Top 5
-            </p>
-        </div>
-        <div class="box radius-top-0">
-            {!! $pie2->render() !!}
-        </div>
-    </div>
-    <div class="column is-3">
-        <div class="box mb-0 radius-bottom-0 pb-1">
-            <p class="text-green has-text-weight-medium">
-                Raw Materials (%)
-            </p>
-            <p class="is-size-7 has-text-grey mb-4">
-                Manufacturing Inventory - Top 5
-            </p>
-        </div>
-        <div class="box radius-top-0">
-            {!! $pie->render() !!}
         </div>
     </div>
 </div>
