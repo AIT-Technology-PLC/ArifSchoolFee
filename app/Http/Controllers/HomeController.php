43bf72edc73c09cfd0df6dade32fef068d255fc3
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-03 09:18:20 +0300

    build fourth row charts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,9 +25,15 @@ class HomeController extends Controller
 
         $pie2 = $this->pie2();
 
+        $pieMerchandise = $this->pieMerchandise();
+
+        $pieMerchandiseSales = $this->pieMerchandiseSales();
+
         $chartjs = $this->chartjs();
 
-        return view('home', compact('chartjs', 'pie', 'pie2'));
+        $chartjsMerchandise = $this->chartjsMerchandise();
+
+        return view('home', compact('chartjs', 'pie', 'pie2', 'chartjsMerchandise', 'pieMerchandise', 'pieMerchandiseSales'));
     }
 
     public function chartjs()
@@ -72,6 +78,94 @@ class HomeController extends Controller
             ]);
 
         return $chartjs;
+    }
+
+    public function chartjsMerchandise()
+    {
+        $chartjs = app()->chartjs
+            ->name('barMerchandiseChartTest')
+            ->type('bar')
+            ->size(['width' => 444, 'height' => 200])
+            ->labels(['Jeans', 'Shirts', 'Socks'])
+            ->datasets([
+                [
+                    "label" => "Percentage",
+                    'backgroundColor' => ['rgba(61, 134, 96, 0.5)', 'rgba(134, 61, 99, 0.5)', 'rgba(134, 132, 61, 0.5)', 'rgba(61, 99, 134, 0.5)'],
+                    'borderColor' => ['rgba(61, 134, 96, 1)', 'rgba(134, 61, 99, 1)', 'rgba(134, 132, 61, 1)', 'rgba(61, 99, 134, 1)'],
+                    'borderWidth' => 1,
+                    'data' => [85, 50, 60, 44],
+                ],
+            ])
+            ->options([
+                'legend' => [
+                    'display' => false,
+                ],
+                'scales' => [
+                    'xAxes' => [
+                        [
+                            'stacked' => false,
+                            'gridLines' => [
+                                'display' => false,
+                            ],
+                            'barPercentage' => 0.7,
+                        ],
+                    ],
+                    'yAxes' => [
+                        [
+                            'stacked' => true,
+                            'gridLines' => [
+                                'display' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ]);
+
+        return $chartjs;
+    }
+
+    public function pieMerchandise()
+    {
+        $pie = app()->chartjs
+            ->name('pieMerchandiseChartTest')
+            ->type('doughnut')
+            ->size(['width' => 191, 'height' => 200])
+            ->labels(['Jeans', 'Shirts', 'Socks'])
+            ->datasets([
+                [
+                    'backgroundColor' => ['rgba(61, 134, 96, 0.5)', 'rgba(134, 61, 99, 0.5)', 'rgba(134, 132, 61, 0.5)', 'rgba(61, 99, 134, 0.5)'],
+                    'borderColor' => ['rgba(61, 134, 96, 1)', 'rgba(134, 61, 99, 1)', 'rgba(134, 132, 61, 1)', 'rgba(61, 99, 134, 1)'],
+                    'borderWidth' => 1,
+                    'data' => [65, 20, 15],
+                ],
+            ])
+            ->options([
+                'cutoutPercentage' => 50,
+            ]);
+
+        return $pie;
+    }
+
+    public function pieMerchandiseSales()
+    {
+        $pie = app()->chartjs
+            ->name('pieMerchandiseSalesChartTest')
+            ->type('doughnut')
+            ->size(['width' => 191, 'height' => 200])
+            ->labels(['Sold', 'Unsold'])
+            ->datasets([
+                [
+                    'backgroundColor' => ['rgba(61, 134, 96, 0.5)', 'rgba(134, 61, 99, 0.5)', 'rgba(134, 132, 61, 0.5)', 'rgba(61, 99, 134, 0.5)'],
+                    'borderColor' => ['rgba(61, 134, 96, 1)', 'rgba(134, 61, 99, 1)', 'rgba(134, 132, 61, 1)', 'rgba(61, 99, 134, 1)'],
+                    'borderWidth' => 1,
+                    'data' => [20, 80],
+                ],
+            ])
+            ->options([
+                'cutoutPercentage' => 50,
+            ]);
+
+        return $pie;
     }
 
     public function pie()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    Dashboard - Onrica Inventory
+Dashboard - Onrica Inventory
 @endsection
 
 @section('content')
@@ -214,45 +214,45 @@
     </div>
 </div>
 
-{{-- <div class="columns is-marginless is-vcentered">
+<div class="columns is-marginless is-vcentered">
     <div class="column is-6">
         <div class="box mb-0 radius-bottom-0 pb-1">
             <p class="text-green has-text-weight-medium">
                 Stock Level (%)
             </p>
             <p class="is-size-7 has-text-grey mb-4">
-                Manufacturing Inventory
+                Merchandise Inventory
             </p>
         </div>
         <div class="box radius-top-0">
-            {!! $chartjs->render() !!}
+            {!! $chartjsMerchandise->render() !!}
         </div>
     </div>
     <div class="column is-3">
         <div class="box mb-0 radius-bottom-0 pb-1">
             <p class="text-green has-text-weight-medium">
-                Finished Products (%)
+                Products By Category (%)
             </p>
             <p class="is-size-7 has-text-grey mb-4">
-                Manufacturing Inventory - Top 5
+                Merchandise Inventory
             </p>
         </div>
         <div class="box radius-top-0">
-            {!! $pie2->render() !!}
+            {!! $pieMerchandise->render() !!}
         </div>
     </div>
     <div class="column is-3">
         <div class="box mb-0 radius-bottom-0 pb-1">
             <p class="text-green has-text-weight-medium">
-                Raw Materials (%)
+                Sold Products (%)
             </p>
             <p class="is-size-7 has-text-grey mb-4">
-                Manufacturing Inventory - Top 5
+                Merchandise Inventory
             </p>
         </div>
         <div class="box radius-top-0">
-            {!! $pie->render() !!}
+            {!! $pieMerchandiseSales->render() !!}
         </div>
     </div>
-</div> --}}
+</div>
 @endsection
