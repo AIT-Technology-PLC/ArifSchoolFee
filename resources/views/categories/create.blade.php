fbd35a83aa98cd3af6e22d2317f1d1142970d602
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-04 11:08:34 +0300

    build create category form

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -5,5 +5,59 @@ Create New Product Category - Onrica Technologies PLC
 @endsection
 
 @section('content')
-
+<section class="mt-3 mx-3">
+    <div class="box radius-bottom-0 mb-0 has-background-white-bis">
+        <h1 class="title text-green has-text-weight-medium is-size-5">
+            New Product Category
+        </h1>
+    </div>
+    <div class="box radius-bottom-0 mb-0 radius-top-0">
+        <div class="columns is-marginless">
+            <div class="column is-6">
+                <div class="field">
+                    <label for="name" class="label text-green has-text-weight-normal">Name</label>
+                    <div class="control has-icons-left">
+                        <input id="name" name="name" type="text" class="input" placeholder="Category Name">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-layer-group"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="field mt-5">
+                    <label for="name" class="label text-green has-text-weight-normal">Description</label>
+                    <div class="control has-icons-left">
+                        <textarea name="description" id="description" cols="30" rows="10" class="textarea pl-6" placeholder="Description or note about the new category"></textarea>
+                        <span class="icon is-large is-left">
+                            <i class="fas fa-edit"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="box radius-top-0">
+        <div class="columns is-marginless">
+            <div class="column is-paddingless">
+                <div class="buttons is-right">
+                    <button class="button is-white text-green">
+                        <span class="icon">
+                            <i class="fas fa-times"></i>
+                        </span>
+                        <span>
+                            Cancel
+                        </span>
+                    </button>
+                    <button class="button bg-green has-text-white">
+                        <span class="icon">
+                            <i class="fas fa-save"></i>
+                        </span>
+                        <span>
+                            Save
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
