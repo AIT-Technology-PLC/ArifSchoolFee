49eaa3693a43fab866c3e19e7e547780657e164b
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-05 15:38:04 +0300

    build login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,68 @@
-@extends('layouts.app')
+@extends('auth.layout.auth')
+
+@section('title')
+Login - Onrica Inventory
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+<section class="hero is-fullheight bg-green limit-to-100vh">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns is-marginless is-centered">
+                <div class="column is-5">
+                    <div class="has-text-centered">
+                        <img src="{{ asset('img/logo.png') }}" width="200">
+                    </div>
+                    <div class="box" style="background: #509270;">
+                        <form method="POST" action="{{ route('login') }}" novalidate>
+                            @csrf
+                            <div class="field">
+                                <label for="email" class="label has-text-white is-uppercase is-size-7">E-Mail Address</label>
+                                <div class="control">
+                                    <input id="email" type="email" class="input bg-lightgreen @error('email') is-danger @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+                                    @error('email')
+                                    <span class="help has-text-white">
+                                        {{ $message }}
                                     </span>
-                                @enderror
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                            <div class="field mt-5">
+                                <label for="password" class="label has-text-white is-uppercase is-size-7">Password</label>
+                                <div class="control">
+                                    <input id="password" type="password" class="input bg-lightgreen @error('password') is-danger @enderror" name="password" autocomplete="current-password">
+                                    @error('password')
+                                    <span class="help has-text-white">
+                                        {{ $message }}
                                     </span>
-                                @enderror
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                            <div class="field mt-5">
+                                <div class="control">
+                                    <label class="radio has-text-white">
+                                        <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        Remember Me
                                     </label>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                            <div class="field">
+                                <div class="control">
+                                    <button type="submit" class="button has-text-white bg-blue is-fullwidth is-uppercase is-size-7 has-text-weight-semibold py-5 px-5">
+                                        Login
+                                    </button>
+                                    @if (Route::has('password.request'))
+                                    <a class="help has-text-white mt-5" href="{{ route('password.request') }}">
+                                        Forgot Your Password?
                                     </a>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
 @endsection
