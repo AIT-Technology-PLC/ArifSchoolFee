c363b175d5a280837fc7f85cae2ffe9725bffcb9
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-11 16:27:29 +0300

    change cancel button to reset

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -106,7 +106,7 @@ Add New Employee - Onrica Technologies PLC
             <div class="columns is-marginless">
                 <div class="column is-paddingless">
                     <div class="buttons is-right">
-                        <button class="button is-white text-green">
+                        <button class="button is-white text-green" type="reset">
                             <span class="icon">
                                 <i class="fas fa-times"></i>
                             </span>
