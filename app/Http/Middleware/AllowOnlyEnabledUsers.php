e9396ab2e334636eb26005ce2858036938b64975
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-12 14:57:24 +0300

    add middleware to check if emp is enabled

---
+++ app/Http/Middleware/AllowOnlyEnabledUsers.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class AllowOnlyEnabledUsers
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $isUserEnabled = auth()->user()->employee->enabled;
+
+        if ($isUserEnabled) {
+            return $next($request);
+        } 
+        
+        Auth::logout();
+        return redirect("/login");
+    }
+}
