53ed138a755ceba255f3cdbf0c6a1ebcfc3a7042
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-14 19:50:22 +0300

    remove companies show

--- resources/views/permissions/show.blade.php
+++ resources/views/permissions/show.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('title')
-Your Access Permissions
+Access Permissions - {{ $permission->employee->user->name }}
 @endsection
 
 @section('content')
 <section class="mt-3 mx-3 m-lr-0">
     <div class="box radius-bottom-0 mb-0 has-background-white-bis">
         <h1 class="title text-green has-text-weight-medium is-size-5">
-            Your Access Permissions
+            Access Permissions - {{ $permission->employee->user->name }}
         </h1>
     </div>
     <div class="box radius-bottom-0 mb-0 radius-top-0">
