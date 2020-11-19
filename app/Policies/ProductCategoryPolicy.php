df31d49979da8658e9e0cb335d014f696013e07f
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-19 11:46:32 +0300

    build category auth policy

--- app/Policies/ProductCategoryPolicy.php
+++ app/Policies/ProductCategoryPolicy.php
@@ -12,36 +12,62 @@ class ProductCategoryPolicy
 
     public function viewAny(User $user)
     {
-        //
+        return true;
     }
 
-    public function view(User $user, ProductCategory $productCategory)
+    public function view(User $user, ProductCategory $category)
     {
-        //
+        $isUserOperative = true;
+
+        $doesCategoryBelongToMyCompany = $user->employee->company_id == $category->company_id;
+
+        $canSeeCategory = $isUserOperative && $doesCategoryBelongToMyCompany;
+
+        if ($canSeeCategory) {
+            return true;
+        }
+
+        return false;
     }
 
     public function create(User $user)
     {
-        //
+        $isUserOperative = $user->employee->permission_id == 1 ||
+        $user->employee->permission_id == 2 ||
+        $user->employee->permission_id == 3;
+
+        return $isUserOperative;
     }
 
-    public function update(User $user, ProductCategory $productCategory)
+    public function update(User $user, ProductCategory $category)
     {
-        //
+        $isUserOperative = $user->employee->permission_id == 1 ||
+        $user->employee->permission_id == 2 ||
+        $user->employee->permission_id == 3;
+
+        $doesCategoryBelongToMyCompany = $user->employee->company_id == $category->company_id;
+
+        $canUpdateCategory = $isUserOperative && $doesCategoryBelongToMyCompany;
+
+        if ($canUpdateCategory) {
+            return true;
+        }
+
+        return false;
     }
 
-    public function delete(User $user, ProductCategory $productCategory)
+    public function delete(User $user, ProductCategory $category)
     {
-        //
-    }
+        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
 
-    public function restore(User $user, ProductCategory $productCategory)
-    {
-        //
-    }
+        $doesCategoryBelongToMyCompany = $user->employee->company_id == $category->company_id;
 
-    public function forceDelete(User $user, ProductCategory $productCategory)
-    {
-        //
+        $canDeleteCategory = $isUserAdmin && $doesCategoryBelongToMyCompany;
+
+        if ($canDeleteCategory) {
+            return true;
+        }
+
+        return false;
     }
 }
