581e5e903038aba55cfc14f91a38b08c6e5f1e0b
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-22 15:22:38 +0300

    build supplier model relations and fields

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    protected $fillable = ['company_id', 'created_by', 'updated_by', 'company_name', 'contact_name', 'email', 'phone', 'country'];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function purchases()
+    {
+        return $this->hasMany(Purchase::class);
+    }
 }
