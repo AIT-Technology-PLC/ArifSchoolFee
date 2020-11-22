32b81df093127740de9a8c15ad359ba79c58f99f
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-22 20:53:09 +0300

    build purchase model

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -2,9 +2,51 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Purchase extends Model
 {
-    //
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    protected $casts = [
+        'shipped_at' => datetime,
+        'delivered_at' => datetime,
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function getAll()
+    {
+        return $this->with(['createdBy', 'updatedBy', 'supplier', 'product'])
+            ->where('company_id', auth()->user()->employee->company_id)->get();
+    }
+
+    public function countPurchasesOfCompany()
+    {
+        return $this->where('company_id', auth()->user()->employee->company_id)->count();
+    }
 }
