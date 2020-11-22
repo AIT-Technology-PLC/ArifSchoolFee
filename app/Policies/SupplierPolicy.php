1c6d276f90b3c5d1e32c5f1d108a37d4b1692229
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-22 20:34:42 +0300

    build supplier features auth policies

--- app/Policies/SupplierPolicy.php
+++ app/Policies/SupplierPolicy.php
@@ -12,26 +12,60 @@ class SupplierPolicy
 
     public function viewAny(User $user)
     {
-        //
+        return true;
     }
 
     public function view(User $user, Supplier $supplier)
     {
-        //
+        $doesSupplierBelongToMyCompany = $user->employee->company_id == $supplier->company_id;
+
+        $canSeeSupplier = $doesSupplierBelongToMyCompany;
+
+        if ($canSeeSupplier) {
+            return true;
+        }
+
+        return false;
     }
 
     public function create(User $user)
     {
-        //
+        $isUserOperative = $user->employee->permission_id == 1 ||
+        $user->employee->permission_id == 2 ||
+        $user->employee->permission_id == 3;
+
+        return $isUserOperative;
     }
 
     public function update(User $user, Supplier $supplier)
     {
-        //
+        $isUserOperative = $user->employee->permission_id == 1 ||
+        $user->employee->permission_id == 2 ||
+        $user->employee->permission_id == 3;
+
+        $doesSupplierBelongToMyCompany = $user->employee->company_id == $supplier->company_id;
+
+        $canUpdateSupplier = $isUserOperative && $doesSupplierBelongToMyCompany;
+
+        if ($canUpdateSupplier) {
+            return true;
+        }
+
+        return false;
     }
 
     public function delete(User $user, Supplier $supplier)
     {
-        //
+        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
+
+        $doesSupplierBelongToMyCompany = $user->employee->company_id == $supplier->company_id;
+
+        $canDeleteSupplier = $isUserAdmin && $doesSupplierBelongToMyCompany;
+
+        if ($canDeleteSupplier) {
+            return true;
+        }
+
+        return false;
     }
 }
