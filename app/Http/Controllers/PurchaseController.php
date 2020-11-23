1a1ac8f0c448f8bcc4fd0aae0b233b7d9238f698
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-23 10:50:07 +0300

    purhcase edit and update api

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -65,12 +65,31 @@ class PurchaseController extends Controller
 
     public function edit(Purchase $purchase, Product $product, Supplier $supplier)
     {
-        //
+        $products = $product->getProductNames();
+
+        $suppliers = $supplier->getSupplierNames();
+
+        return view('purchases.edit', compact('purhcase', 'products', 'suppliers'));
     }
 
     public function update(Request $request, Purchase $purchase)
     {
-        //
+        $data = $request->validate([
+            'product_id' => 'required|integer',
+            'supplier_id' => 'nullable|integer',
+            'total_quantity' => 'required|numeric',
+            'total_price' => 'nullable|numeric',
+            'shipping_line' => 'required|string|max:255',
+            'payment_status' => 'required|string|max:255',
+            'shipped_at' => 'nullable|date',
+            'delivered_at' => 'nullable|date',
+        ]);
+
+        $data['updated_by'] = auth()->user()->id;
+
+        $purchase->update($data);
+
+        return redirect()->route('purchases.index');
     }
 
     public function destroy(Purchase $purchase)
