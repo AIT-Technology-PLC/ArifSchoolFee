0d75daf6b4a1a7f2f2777c820166df9ff7d3df4e
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-26 19:08:27 +0300

    create purchaseDetail model relations

---
+++ app/Models/PurchaseDetail.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PurchaseDetail extends Model
+{
+    protected $fillable = ['purchase_id', 'supplier_id', 'product_id', 'quantity', 'unit_price'];
+
+    public function purchase()
+    {
+        return $this->belongsTo(Purchase::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
