a5b3de9d2033dcedb51cae35ceba335275bf6816
Nabil Hassen <nabil.hassen08@gmail.com>
2020-11-28 11:35:51 +0300

    replace jquery with vanilla js

--- public/js/app.js
+++ public/js/app.js
@@ -1,60 +1,78 @@
-var menuScroller = $("#menuLeft");
+const d = document;
 
-$(document).ready(function () {
-    menuScroller.css("overflow", "hidden");
-});
+function hideMainMenuScroller() {
+    d.getElementById("menuLeft").style.overflow = "hidden";
+}
 
-menuScroller.mouseover(function () {
-    $(this).css("overflow", "auto");
-});
-menuScroller.mouseout(function () {
-    $(this).css("overflow", "hidden");
-});
+function hideMainMenuScrollerOnMouseOut() {
+    d.getElementById("menuLeft").style.overflow = "hidden";
+}
+
+function showMainMenuScrollerOnMouseOver() {
+    d.getElementById("menuLeft").style.overflow = "auto";
+}
 
 function keyValueInputFields() {
     let index = 0;
-    $("#newForm").removeClass("is-hidden");
+    const newForm = d.getElementById("newForm");
+    newForm.classList.remove("is-hidden");
+
     return function () {
-        $("#newForm").append(`
+        const keyValueFieldPair = `
         <div class="column is-6">
-            <div class="field">
-                    <label for="key${index}" class="label text-green has-text-weight-normal">Property</label>
-                    <div class="control has-icons-left">
-                        <input id="key${index}" name="properties[${index}][key]" type="text" class="input" placeholder="Color">
-                    </div>
-                </div>
-            </div>
+        <div class="field">
+        <label for="key${index}" class="label text-green has-text-weight-normal">Property</label>
+        <div class="control has-icons-left">
+        <input id="key${index}" name="properties[${index}][key]" type="text" class="input" placeholder="Color">
+        </div>
+        </div>
+        </div>
         <div class="column is-6">
-            <div class="field">
-                <label for="value${index}" class="label text-green has-text-weight-normal">Data</label>
-                <div class="control has-icons-left">
-                    <input id="value${index}" name="properties[${index}][value]" type="text" class="input" placeholder="Green">
-                </div>
-            </div>
-        </div>`);
+        <div class="field">
+        <label for="value${index}" class="label text-green has-text-weight-normal">Data</label>
+        <div class="control has-icons-left">
+        <input id="value${index}" name="properties[${index}][value]" type="text" class="input" placeholder="Green">
+        </div>
+        </div>
+        </div>`;
+
+        newForm.innerHTML += keyValueFieldPair;
 
         index++;
     };
 }
 
-const createFields = keyValueInputFields();
-
-$("#addNewForm").click(function () {
-    createFields();
-});
-
-$("#createMenuButton").click(function () {
-    $("#createMenu").toggleClass("is-hidden");
-});
+function toggleCreateMenu() {
+    d.getElementById("createMenu").classList.toggle("is-hidden");
+}
 
 async function getProductSelected(elementId, productId) {
     const response = await axios.get("/product/uom/" + productId);
     const unitOfMeasurement = response.data;
 
-    document.getElementById(
-        elementId + "Quantity"
-    ).innerText = unitOfMeasurement;
-    document.getElementById(
+    d.getElementById(elementId + "Quantity").innerText = unitOfMeasurement;
+    d.getElementById(
         elementId + "Price"
     ).innerText = `Per ${unitOfMeasurement}`;
 }
+
+d.onreadystatechange = hideMainMenuScroller;
+
+d.getElementById("menuLeft").addEventListener(
+    "mouseout",
+    hideMainMenuScrollerOnMouseOut
+);
+
+d.getElementById("menuLeft").addEventListener(
+    "mouseover",
+    showMainMenuScrollerOnMouseOver
+);
+
+const createFields = keyValueInputFields();
+
+d.getElementById("addNewForm").addEventListener("click", createFields);
+
+d.getElementById("createMenuButton").addEventListener(
+    "click",
+    toggleCreateMenu
+);
