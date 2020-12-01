0eca043ed112d1298aebaacf3b059ee639c03ba6
Nabil Hassen <nabil.hassen08@gmail.com>
2020-12-01 10:01:32 +0300

    build create warehouse form

---
+++ resources/views/warehouses/create.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.app')
+
+@section('title')
+    Create New Warehouse
+@endsection
+
+@section('content')
+    <section class="mt-3 mx-3 m-lr-0">
+        <div class="box radius-bottom-0 mb-0 has-background-white-bis">
+            <h1 class="title text-green has-text-weight-medium is-size-5">
+                New Warehouse
+            </h1>
+        </div>
+        <form action="{{ route('warehouses.store') }}" method="post" enctype="multipart/form-data" novalidate>
+            @csrf
+            <div class="box radius-bottom-0 mb-0 radius-top-0">
+                <div class="columns is-marginless is-multiline">
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="name" class="label text-green has-text-weight-normal">Name <sup class="has-text-danger">*</sup> </label>
+                            <div class="control has-icons-left">
+                                <input id="name" name="name" type="text" class="input" placeholder="Category Name" value="{{ old('name') ?? '' }}">
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-warehouse"></i>
+                                </span>
+                                @error('name')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="location" class="label text-green has-text-weight-normal">Location <sup class="has-text-danger">*</sup> </label>
+                            <div class="control has-icons-left">
+                                <input id="location" name="location" type="text" class="input" placeholder="Location: Building, Street" value="{{ old('location') ?? '' }}">
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-location-arrow"></i>
+                                </span>
+                                @error('location')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field mt-5">
+                            <label for="description" class="label text-green has-text-weight-normal">Description</label>
+                            <div class="control has-icons-left">
+                                <textarea name="description" id="description" cols="30" rows="10" class="textarea pl-6" placeholder="Description or note about the new category">{{ old('description') ?? '' }}</textarea>
+                                <span class="icon is-large is-left">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                                @error('description')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box radius-top-0">
+                <div class="columns is-marginless">
+                    <div class="column is-paddingless">
+                        <div class="buttons is-right">
+                            <button class="button is-white text-green" type="reset">
+                                <span class="icon">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                                <span>
+                                    Cancel
+                                </span>
+                            </button>
+                            <button class="button bg-green has-text-white">
+                                <span class="icon">
+                                    <i class="fas fa-save"></i>
+                                </span>
+                                <span>
+                                    Save
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </section>
+@endsection
