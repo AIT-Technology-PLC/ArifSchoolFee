fe7a4f7f00d275f751015295f1d20e3c05e89d9f
Nabil Hassen <nabil.hassen08@gmail.com>
2020-12-02 10:34:57 +0300

    build merchandise model

--- app/Models/Merchandise.php
+++ app/Models/Merchandise.php
@@ -2,9 +2,49 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Merchandise extends Model
 {
-    //
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    protected $casts = [
+        'expires_on' => 'datetime',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+
+    public function scopeCompanyMerchandises($query)
+    {
+        return $query->where('company_id', auth()->user()->employee->company_id);
+    }
+
+    public function getAll()
+    {
+        return $this->companyMerchandises()->get();
+    }
 }
