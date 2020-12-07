6cbe5e22d524b8de4fa763b04345262ac8c3bed6
Nabil Hassen <nabil.hassen08@gmail.com>
2020-12-07 11:02:49 +0300

    build merchandise auth policies

--- app/Policies/MerchandisePolicy.php
+++ app/Policies/MerchandisePolicy.php
@@ -12,26 +12,62 @@ class MerchandisePolicy
 
     public function viewAny(User $user)
     {
-        //
+        return true;
     }
 
     public function view(User $user, Merchandise $merchandise)
     {
-        //
+        $isUserOperative = true;
+
+        $doesMerchandiseBelongToMyCompany = $user->employee->company_id == $merchandise->company_id;
+
+        $canSeeMerchandise = $isUserOperative && $doesMerchandiseBelongToMyCompany;
+
+        if ($canSeeMerchandise) {
+            return true;
+        }
+
+        return false;
     }
 
     public function create(User $user)
     {
-        //
+        $isUserOperative = $user->employee->permission_id == 1 ||
+        $user->employee->permission_id == 2 ||
+        $user->employee->permission_id == 3;
+
+        return $isUserOperative;
     }
 
     public function update(User $user, Merchandise $merchandise)
     {
-        //
+        $isUserOperative = $user->employee->permission_id == 1 ||
+        $user->employee->permission_id == 2 ||
+        $user->employee->permission_id == 3;
+
+        $doesMerchandiseBelongToMyCompany = $user->employee->company_id == $merchandise->company_id;
+
+        $canUpdateMerchandise = $isUserOperative && $doesMerchandiseBelongToMyCompany;
+
+        if ($canUpdateMerchandise) {
+            return true;
+        }
+
+        return false;
     }
 
     public function delete(User $user, Merchandise $merchandise)
     {
-        //
+        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
+
+        $doesMerchandiseBelongToMyCompany = $user->employee->company_id == $merchandise->company_id;
+
+        $canDeleteMerchandise = $isUserAdmin && $doesMerchandiseBelongToMyCompany;
+
+        if ($canDeleteMerchandise) {
+            return true;
+        }
+
+        return false;
     }
 }
