b63e8ea184daf3a8aa2ec9545a3b37773e19f107
Nabil Hassen <nabil.hassen08@gmail.com>
2020-12-07 15:23:10 +0300

    build customer apis

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -16,17 +16,35 @@ class CustomerController extends Controller
 
     public function index()
     {
-        //
+        $customers = $this->customer->getAll();
+
+        $totalCustomers = $this->customer->countCustomersOfCompany();
+
+        return view('customers.index', compact('customers', 'totalCustomers'));
     }
 
     public function create()
     {
-        //
+        return view('customers.create');
     }
 
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'company_name' => 'required|string|max:255',
+            'contact_name' => 'nullable|string|max:255',
+            'email' => 'nullable|string|email|max:255',
+            'phone' => 'nullable|string|max:255',
+            'country' => 'nullable|string|max:255',
+        ]);
+
+        $data['company_id'] = auth()->user()->employee->company_id;
+        $data['created_by'] = auth()->user()->id;
+        $data['updated_by'] = auth()->user()->id;
+
+        $this->customer->firstOrCreate($data);
+
+        return redirect()->route('customers.index');
     }
 
     public function show(Customer $customer)
@@ -36,12 +54,24 @@ class CustomerController extends Controller
 
     public function edit(Customer $customer)
     {
-        //
+        return view('customers.edit', compact('customer'));
     }
 
     public function update(Request $request, Customer $customer)
     {
-        //
+        $data = $request->validate([
+            'company_name' => 'required|string|max:255',
+            'contact_name' => 'nullable|string|max:255',
+            'email' => 'nullable|string|email|max:255',
+            'phone' => 'nullable|string|max:255',
+            'country' => 'nullable|string|max:255',
+        ]);
+
+        $data['updated_by'] = auth()->user()->id;
+
+        $customer->update($data);
+
+        return redirect()->route('customers.index');
     }
 
     public function destroy(Customer $customer)
