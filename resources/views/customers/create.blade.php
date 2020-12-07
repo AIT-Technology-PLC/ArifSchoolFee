9d5d573829c537879e46d790680b2dd29a82d471
Nabil Hassen <nabil.hassen08@gmail.com>
2020-12-07 15:56:42 +0300

    create customers blade

---
+++ resources/views/customers/create.blade.php
@@ -0,0 +1,134 @@
+@extends('layouts.app')
+
+@section('title')
+    Create New Customer
+@endsection
+
+@section('content')
+    <section class="mt-3 mx-3 m-lr-0">
+        <div class="box radius-bottom-0 mb-0 has-background-white-bis">
+            <h1 class="title text-green has-text-weight-medium is-size-5">
+                New Customer
+            </h1>
+        </div>
+        <form action="{{ route('customers.store') }}" method="POST" enctype="multipart/form-data" novalidate>
+            @csrf
+            <div class="box radius-bottom-0 mb-0 radius-top-0">
+                <div class="columns is-marginless is-multiline">
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="company_name" class="label text-green has-text-weight-normal">Company Name <sup class="has-text-danger">*</sup> </label>
+                            <div class="control has-icons-left">
+                                <input id="company_name" name="company_name" type="text" class="input" placeholder="Company Name" value="{{ old('company_name') ?? '' }}">
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-building"></i>
+                                </span>
+                                @error('company_name')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="contact_name" class="label text-green has-text-weight-normal">Contact Name <sup class="has-text-danger"></sup> </label>
+                            <div class="control has-icons-left">
+                                <input id="contact_name" name="contact_name" type="text" class="input" placeholder="Contact Name" value="{{ old('contact_name') ?? '' }}">
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-address-book"></i>
+                                </span>
+                                @error('contact_name')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="email" class="label text-green has-text-weight-normal">Email <sup class="has-text-danger"></sup> </label>
+                            <div class="control has-icons-left">
+                                <input id="email" name="email" type="text" class="input" placeholder="Email Address" value="{{ old('email') ?? '' }}">
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-at"></i>
+                                </span>
+                                @error('email')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="phone" class="label text-green has-text-weight-normal">Phone <sup class="has-text-danger"></sup> </label>
+                            <div class="control has-icons-left">
+                                <input id="phone" name="phone" type="text" class="input" placeholder="Phone/Telephone" value="{{ old('phone') ?? '' }}">
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-phone"></i>
+                                </span>
+                                @error('phone')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="country" class="label text-green has-text-weight-normal"> Country <sup class="has-text-danger"></sup> </label>
+                            <div class="control has-icons-left">
+                                <div class="select is-fullwidth">
+                                    <select id="country" name="country">
+                                        <option selected disabled>Country</option>
+                                        <option value="China" {{ old('country') == 'China' ? 'selected' : '' }}>China</option>
+                                        <option value="India" {{ old('country') == 'India' ? 'selected' : '' }}>India</option>
+                                        <option value="UAE" {{ old('country') == 'UAE' ? 'selected' : '' }}>UAE</option>
+                                        <option value="KSA" {{ old('country') == 'KSA' ? 'selected' : '' }}>KSA</option>
+                                    </select>
+                                </div>
+                                <div class="icon is-small is-left">
+                                    <i class="fas fa-globe"></i>
+                                </div>
+                                @error('country')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box radius-top-0">
+                <div class="columns is-marginless">
+                    <div class="column is-paddingless">
+                        <div class="buttons is-right">
+                            <button class="button is-white text-green" type="reset">
+                                <span class="icon">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                                <span>
+                                    Cancel
+                                </span>
+                            </button>
+                            <button class="button bg-green has-text-white">
+                                <span class="icon">
+                                    <i class="fas fa-save"></i>
+                                </span>
+                                <span>
+                                    Save
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </section>
+@endsection
---
+++ resources/views/customers/index.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.app')
+
+@section('title')
+    Customers Management
+@endsection
+
+@section('content')
+    <div class="columns is-marginless">
+        <div class="column is-6">
+            <div class="box text-green">
+                <div class="columns is-marginless is-vcentered is-mobile">
+                    <div class="column has-text-centered is-paddingless">
+                        <span class="icon is-large is-size-1">
+                            <i class="fas fa-address-card"></i>
+                        </span>
+                    </div>
+                    <div class="column is-paddingless">
+                        <div class="is-size-3 has-text-weight-bold">
+                            {{ $totalCustomers }}
+                        </div>
+                        <div class="is-uppercase is-size-7">
+                            Total Customers
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column is-6">
+            <div class="box text-purple">
+                <div class="columns is-marginless is-vcentered is-mobile">
+                    <div class="column is-paddingless has-text-centered">
+                        <div class="is-uppercase is-size-7">
+                            Add new customer and assign products to them
+                        </div>
+                        <div class="is-size-3">
+                            <a href="{{ route('customers.create') }}" class="button bg-purple has-text-white has-text-weight-medium is-size-7 px-5 py-4 mt-3">
+                                <span class="icon">
+                                    <i class="fas fa-plus-circle"></i>
+                                </span>
+                                <span>
+                                    Create New Customer
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="mt-3 mx-3 m-lr-0">
+        <div class="box radius-bottom-0 mb-0 has-background-white-bis">
+            <h1 class="title text-green has-text-weight-medium is-size-5">
+                Customer Account Management
+            </h1>
+        </div>
+        <div class="box radius-top-0">
+            <div class="table-container">
+                <table class="table is-hoverable is-fullwidth is-size-7">
+                    <thead>
+                        <tr>
+                            <th><abbr> # </abbr></th>
+                            <th class="text-purple"><abbr> Company </abbr></th>
+                            <th><abbr> Contact </abbr></th>
+                            <th><abbr> Email </abbr></th>
+                            <th><abbr> Phone </abbr></th>
+                            <th><abbr> Country </abbr></th>
+                            <th class="has-text-right"><abbr> Added On </abbr></th>
+                            @can('delete', $customers->first())
+                                <th><abbr> Added By </abbr></th>
+                                <th><abbr> Edited By </abbr></th>
+                            @endcan
+                            <th class="has-text-centered"><abbr> Actions </abbr></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($customers as $customer)
+                            <tr>
+                                <td> {{ $loop->index + 1 }} </td>
+                                <td class="is-capitalized">
+                                    <span class="tag is-small bg-purple has-text-white">
+                                        {{ $customer->company_name ?? 'N/A' }}
+                                    </span>
+                                </td>
+                                <td class="is-capitalized">{{ $customer->contact_name ?? 'N/A' }}</td>
+                                <td>{{ $customer->email ?? 'N/A' }}</td>
+                                <td class="is-capitalized">{{ $customer->phone ?? 'N/A' }}</td>
+                                <td class="is-capitalized">{{ $customer->country ?? 'N/A' }}</td>
+                                <td class="has-text-right"> {{ $customer->created_at->toFormattedDateString() }} </td>
+                                @can('delete', $customer)
+                                    <td> {{ $customer->createdBy->name ?? 'N/A' }} </td>
+                                    <td> {{ $customer->updatedBy->name ?? 'N/A' }} </td>
+                                @endcan
+                                <td class="has-text-centered">
+                                    <a href="{{ route('customers.edit', $customer->id) }}" data-title="Modify Customer Data">
+                                        <span class="tag is-white btn-green is-outlined is-small text-green has-text-weight-medium">
+                                            <span class="icon">
+                                                <i class="fas fa-pen-square"></i>
+                                            </span>
+                                            <span>
+                                                Edit
+                                            </span>
+                                        </span>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+@endsection
