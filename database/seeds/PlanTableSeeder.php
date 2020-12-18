d4f690ab3d62ab22b49669512f4d15a9482e928f
Nabil Hassen <nabil.hassen08@gmail.com>
2020-12-18 14:18:51 +0300

    create plan table seeder

---
+++ database/seeds/PlanTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Plan;
+use Illuminate\Database\Seeder;
+
+class PlanTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $plan = new Plan();
+
+        $plan->create([
+            'name' => 'Premium',
+        ]);
+
+        $plan->create([
+            'name' => 'Professional',
+        ]);
+
+        $plan->create([
+            'name' => 'Standard',
+        ]);
+    }
+}
