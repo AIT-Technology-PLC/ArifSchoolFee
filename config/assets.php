c15fd7387611673106eb9ecff62ce44eb038c98b
Nabil Hassen <nabil.hassen08@gmail.com>
2020-12-31 15:46:42 +0300

    update css and js files automatically

--- config/assets.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'css_version' => env('CSS_VERSION'),
-    'js_version' => env('JS_VERSION'),
-];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css" integrity="sha256-WLKGWSIJYerRN8tbNGtXWVYnUM5wMJTXD8eG4NtGcDM=" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
     {{-- Local Assets --}}
-    <link href="{{ asset('css/app.css?' . config('assets.css_version')) }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?' . filemtime('css/app.css')) }}" rel="stylesheet">
 </head>
 
 <body class="has-navbar-fixed-top">
@@ -37,8 +37,8 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" defer></script>
     {{-- Local Assets --}}
-    <script src="{{ asset('js/app.js?' . config('assets.js_version')) }}" defer></script>
-    <script src="{{ asset('js/caller.js?' . config('assets.js_version')) }}" defer></script>
+    <script src="{{ asset('js/app.js?' . filemtime('js/app.js')) }}" defer></script>
+    <script src="{{ asset('js/caller.js?' . filemtime('js/caller.js')) }}" defer></script>
 
 </body>
 
