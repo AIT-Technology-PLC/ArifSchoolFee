dfa05977f991a9ecdd58f5eca1bc8e8b05aba2a8
Nabil Hassen <nabil.hassen08@gmail.com>
2021-01-07 23:08:23 +0300

    - register siv resource route and create blade files

--- app/Http/Controllers/SivController.php
+++ app/Http/Controllers/SivController.php
@@ -7,77 +7,36 @@ use Illuminate\Http\Request;
 
 class SivController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Siv  $siv
-     * @return \Illuminate\Http\Response
-     */
     public function show(Siv $siv)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Siv  $siv
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Siv $siv)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Siv  $siv
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Siv $siv)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Siv  $siv
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Siv $siv)
     {
         //
