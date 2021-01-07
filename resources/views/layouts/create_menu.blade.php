53e4959170727be908321a4c637771b775621f4e
Nabil Hassen <nabil.hassen08@gmail.com>
2021-01-07 18:17:23 +0300

    implement back button on 403 by html history

--- resources/views/layouts/create_menu.blade.php
+++ resources/views/layouts/create_menu.blade.php
@@ -9,7 +9,7 @@
     </h1>
     <div class="columns is-marginless is-multiline is-centered">
         <div class="column is-3 has-text-centered text-green">
-            <a href="{{ route('products.create') }}" class="button is-rounded text-green" title="Create New Product">
+            <a href="{{ route('products.create') }}" class="button is-rounded text-green" data-title="Create New Product">
                 <span class="icon">
                     <i class="fas fa-boxes"></i>
                 </span>
@@ -19,7 +19,7 @@
             </span>
         </div>
         <div class="column is-3 has-text-centered text-purple">
-            <a href="{{ route('suppliers.create') }}" class="button is-rounded text-purple" title="Create New Supplier">
+            <a href="{{ route('suppliers.create') }}" class="button is-rounded text-purple" data-title="Create New Supplier">
                 <span class="icon">
                     <i class="fas fa-address-card"></i>
                 </span>
@@ -29,7 +29,7 @@
             </span>
         </div>
         <div class="column is-3 has-text-centered text-gold">
-            <a href="{{ route('categories.create') }}" class="button is-rounded text-gold" title="Create New Category">
+            <a href="{{ route('categories.create') }}" class="button is-rounded text-gold" data-title="Create New Category">
                 <span class="icon">
                     <i class="fas fa-layer-group"></i>
                 </span>
@@ -39,7 +39,7 @@
             </span>
         </div>
         <div class="column is-3 has-text-centered text-blue">
-            <a href="{{ route('purchases.create') }}" class="button is-rounded text-blue" title="Create New Purchase">
+            <a href="{{ route('purchases.create') }}" class="button is-rounded text-blue" data-title="Create New Purchase">
                 <span class="icon">
                     <i class="fas fa-shopping-bag"></i>
                 </span>
@@ -49,7 +49,7 @@
             </span>
         </div>
         <div class="column is-3 has-text-centered text-gold">
-            <a href="{{ route('sales.create') }}" class="button is-rounded text-gold" title="Create New Sale">
+            <a href="{{ route('sales.create') }}" class="button is-rounded text-gold" data-title="Create New Sale">
                 <span class="icon">
                     <i class="fas fa-tags"></i>
                 </span>
@@ -59,7 +59,7 @@
             </span>
         </div>
         <div class="column is-3 has-text-centered text-blue">
-            <a href="{{ route('employees.create') }}" class="button is-rounded text-blue" title="Create New Employee">
+            <a href="{{ route('employees.create') }}" class="button is-rounded text-blue" data-title="Create New Employee">
                 <span class="icon">
                     <i class="fas fa-user-tie"></i>
                 </span>
@@ -69,7 +69,7 @@
             </span>
         </div>
         <div class="column is-3 has-text-centered text-purple">
-            <a href="{{ route('warehouses.create') }}" class="button is-rounded text-purple" title="Create New Warehouse">
+            <a href="{{ route('warehouses.create') }}" class="button is-rounded text-purple" data-title="Create New Warehouse">
                 <span class="icon">
                     <i class="fas fa-warehouse"></i>
                 </span>
@@ -79,7 +79,7 @@
             </span>
         </div>
         <div class="column is-3 has-text-centered text-green">
-            <a href="{{ route('customers.create') }}" class="button is-rounded text-green" title="Create New Customer">
+            <a href="{{ route('customers.create') }}" class="button is-rounded text-green" data-title="Create New Customer">
                 <span class="icon">
                     <i class="fas fa-user"></i>
                 </span>
