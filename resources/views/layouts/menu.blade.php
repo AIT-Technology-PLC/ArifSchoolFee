cfdf0d90e88bdcc6cad71f3cdca88615bb3d2e20
Nabil Hassen <nabil.hassen08@gmail.com>
2021-01-22 22:44:07 +0300

    add siv page on menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -61,7 +61,17 @@
                 </ul>
             </li>
             <li>
-                <a name="menuTitles" href="{{ route('warehouses.index') }}" class="has-text-grey has-text-weight-normal is-size-6-5 {{ request()->is('warehouses') ? 'is-active' : '' }}">
+                <a name="menuTitles" href="{{ route('warehouses.index') }}" class="has-text-grey has-text-weight-normal is-size-6-5 {{ request()->is('gdns') ? 'is-active' : '' }}">
+                    <span class="icon">
+                        <i class="fas fa-file-invoice"></i>
+                    </span>
+                    <span>
+                        SIV/GDN Management
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a name="menuTitles" href="#" class="has-text-grey has-text-weight-normal is-size-6-5">
                     <span class="icon">
                         <i class="fas fa-cogs"></i>
                     </span>
