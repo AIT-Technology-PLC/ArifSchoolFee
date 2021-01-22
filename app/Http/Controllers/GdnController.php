fa12ecd58c107db340f91db8eefd9c7096e4f295
Nabil Hassen <nabil.hassen08@gmail.com>
2021-01-22 20:27:57 +0300

    build gdn store method

--- app/Http/Controllers/GdnController.php
+++ app/Http/Controllers/GdnController.php
@@ -7,10 +7,16 @@ use App\Models\Gdn;
 use App\Models\Product;
 use App\Models\Sale;
 use App\Models\Warehouse;
+use App\Services\StoreSaleableProducts;
+use App\Traits\PrependCompanyId;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 
 class GdnController extends Controller
 {
+    use PrependCompanyId;
+
     public function index()
     {
         //
@@ -31,7 +37,44 @@ class GdnController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $request['code'] = $this->prependCompanyId($request->code);
+
+        $gdnData = $request->validate([
+            'code' => 'required|string|unique:gdns',
+            'gdn' => 'required|array',
+            'gdn.*.product_id' => 'required|integer',
+            'gdn.*.warehouse_id' => 'required|integer',
+            'gdn.*.quantity' => 'required|numeric|min:1',
+            'gdn.*.desciption' => 'nullable|string',
+            'customer_id' => 'nullable|integer',
+            'sale_id' => 'nullable|integer',
+            'issued_on' => 'required|date',
+            'status' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $gdnData['company_id'] = auth()->user()->employee->company_id;
+        $gdnData['created_by'] = auth()->user()->id;
+        $gdnData['updated_by'] = auth()->user()->id;
+
+        $basicGdnData = Arr::except($gdnData, 'gdn');
+        $gdnDetailsData = $gdnData['gdn'];
+
+        $isGdnValid = DB::transaction(function () use ($basicGdnData, $gdnDetailsData) {
+            $gdn = $this->gdn->create($basicGdnData);
+            $gdn->gdnDetails()->createMany($gdnDetailsData);
+            $isGdnValid = StoreSaleableProducts::storeSoldProducts($gdn);
+
+            if (!$isGdnValid) {
+                DB::rollback();
+            }
+
+            return $isGdnValid;
+        });
+
+        return $isGdnValid ?
+        redirect()->route('gdns.index') :
+        redirect()->back()->withInput($request->all());
     }
 
     public function show(Gdn $gdn)
