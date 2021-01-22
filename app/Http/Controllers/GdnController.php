5b1a89c2214f39b354f0a189fbda5c0aa30f0401
Nabil Hassen <nabil.hassen08@gmail.com>
2021-01-22 18:36:39 +0300

    create gdn models and controller

---
+++ app/Http/Controllers/GdnController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gdn;
+use Illuminate\Http\Request;
+
+class GdnController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Gdn $gdn)
+    {
+        //
+    }
+
+    public function edit(Gdn $gdn)
+    {
+        //
+    }
+
+    public function update(Request $request, Gdn $gdn)
+    {
+        //
+    }
+
+    public function destroy(Gdn $gdn)
+    {
+        //
+    }
+}
