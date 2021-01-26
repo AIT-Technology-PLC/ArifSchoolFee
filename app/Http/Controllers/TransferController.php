2bfdeb401cabaa2756a8618f02f005c9d16f0b5e
Nabil Hassen <nabil.hassen08@gmail.com>
2021-01-26 17:24:38 +0300

    create transfer model and controller

---
+++ app/Http/Controllers/TransferController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transfer;
+use Illuminate\Http\Request;
+
+class TransferController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+    
+    public function create()
+    {
+        //
+    }
+    
+    public function store(Request $request)
+    {
+        //
+    }
+    
+    public function show(Transfer $transfer)
+    {
+        //
+    }
+    
+    public function edit(Transfer $transfer)
+    {
+        //
+    }
+    
+    public function update(Request $request, Transfer $transfer)
+    {
+        //
+    }
+    
+    public function destroy(Transfer $transfer)
+    {
+        //
+    }
+}
