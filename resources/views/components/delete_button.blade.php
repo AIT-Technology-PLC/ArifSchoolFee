fdc948bcb6d85e873e993137de339361fa441222
Nabil Hassen <nabil.hassen08@gmail.com>
2021-01-27 18:07:22 +0300

    change delete button color to brown

--- resources/views/components/delete_button.blade.php
+++ resources/views/components/delete_button.blade.php
@@ -1,7 +1,7 @@
 <form id="deleteForm{{ $id }}" class="is-inline" action="{{ route($model . '.destroy', $id) }}" method="post" data-delete="{{ $id }}">
     @csrf
     @method('DELETE')
-    <button class="tag is-danger is-outlined is-small has-text-weight-medium is-pointer" data-title="Delete Permanently" style="border: none">
+    <button class="tag bg-brown has-text-white is-small has-text-weight-medium is-pointer" data-title="Delete Permanently" style="border: none">
         <span class="icon">
             <i class="fas fa-trash"></i>
         </span>
