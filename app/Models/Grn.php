818fa46dfd87aa315f615c1258ed1678c65dc651
Nabil Hassen <nabil.hassen08@gmail.com>
2021-01-31 01:05:52 +0300

    build grn models

--- app/Models/Grn.php
+++ app/Models/Grn.php
@@ -2,9 +2,79 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Grn extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    protected $casts = [
+        'issued_on' => 'datetime',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function purchase()
+    {
+        return $this->belongsTo(Purchase::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function grnDetails()
+    {
+        return $this->hasMany(GrnDetail::class);
+    }
+
+    public function scopeCompanyGrn($query)
+    {
+        return $query->where('company_id', auth()->user()->employee->company_id);
+    }
+
+    public function getCodeAttribute($value)
+    {
+        return Str::after($value, auth()->user()->employee->company->id . '_');
+    }
+
+    public function getAll()
+    {
+        return $this->companyGrn()->withCount('grnDetails')->latest()->get();
+    }
+
+    public function countGrnsOfCompany()
+    {
+        return $this->companyGrn()->count();
+    }
+
+    public function changeStatusToAddedToInventory()
+    {
+        $this->status = 'Added To Inventory';
+        $this->save();
+    }
+
+    public function isAddedToInventory()
+    {
+        return $this->status == 'Added To Inventory';
+    }
 }
--- app/Models/GrnDetail.php
+++ app/Models/GrnDetail.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class GrnDetail extends Model
 {
-    //
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    public function grn()
+    {
+        return $this->belongsTo(Grn::class);
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
