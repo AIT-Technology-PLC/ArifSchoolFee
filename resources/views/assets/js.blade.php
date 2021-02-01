5b33777c2943537a95feac57d238cf625564ea1a
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-02 01:05:39 +0300

    change core js version

--- resources/views/assets/js.blade.php
+++ resources/views/assets/js.blade.php
@@ -5,5 +5,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" defer></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.2.3/pace.min.js"></script>
 {{-- Local Assets --}}
-<script src="{{ asset('js/app.v1.js') }}" defer></script>
+<script src="{{ asset('js/app.v2.js') }}" defer></script>
 <script src="{{ asset('js/caller.v1.js') }}" defer></script>
