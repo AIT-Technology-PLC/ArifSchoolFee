7207cb3e066e443c217c8ea5cd280913159f2413
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-04 21:49:47 +0300

    remove merchandise controller store and create

--- app/Http/Controllers/MerchandiseController.php
+++ app/Http/Controllers/MerchandiseController.php
@@ -38,40 +38,6 @@ class MerchandiseController extends Controller
         return view('merchandises.index', compact('onHandMerchandises', 'totalDistinctOnHandMerchandises', 'outOfStockMerchandises', 'totalOutOfStockMerchandises', 'totalDistinctLimitedMerchandises', 'historyMerchandises', 'totalWarehouseInUse'));
     }
 
-    public function create(Product $product, Warehouse $warehouse)
-    {
-        $products = $product->getProductNames();
-
-        $warehouses = $warehouse->getAllWithoutRelations();
-
-        return view('merchandises.create', compact('products', 'warehouses'));
-    }
-
-    public function store(Request $request)
-    {
-        $data = $request->validate([
-            'product_id' => 'required|integer',
-            'warehouse_id' => 'required|integer',
-            'expires_on' => 'nullable|date',
-            'description' => 'nullable|string|max:255',
-            'total_received' => 'required|numeric',
-        ]);
-
-        $data['total_on_hand'] = $data['total_received'];
-        $data['total_sold'] = 0;
-        $data['total_transfer'] = 0;
-        $data['total_broken'] = 0;
-        $data['total_returns'] = 0;
-
-        $data['created_by'] = auth()->user()->id;
-        $data['updated_by'] = auth()->user()->id;
-        $data['company_id'] = auth()->user()->employee->company_id;
-
-        $this->merchandise->create($data);
-
-        return redirect()->route('merchandises.index');
-    }
-
     public function edit(Merchandise $merchandise, Warehouse $warehouse)
     {
         $merchandise->load(['product']);
