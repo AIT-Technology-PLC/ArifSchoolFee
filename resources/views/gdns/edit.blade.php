b7285a9ff6998e715d497e3ca8daef2450b740f3
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-20 10:48:11 +0300

    show description on edit form

--- resources/views/gdns/edit.blade.php
+++ resources/views/gdns/edit.blade.php
@@ -98,7 +98,7 @@
                         <div class="field">
                             <label for="description" class="label text-green has-text-weight-normal">Additional Notes <sup class="has-text-danger"></sup> </label>
                             <div class="control has-icons-left">
-                                <textarea name="description" id="description" cols="30" rows="3" class="textarea pl-6" placeholder="Description or note to be taken">{{ old('description') ?? '' }}</textarea>
+                                <textarea name="description" id="description" cols="30" rows="3" class="textarea pl-6" placeholder="Description or note to be taken">{{ $gdn->description ?? '' }}</textarea>
                                 <span class="icon is-large is-left">
                                     <i class="fas fa-edit"></i>
                                 </span>
