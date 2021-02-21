3fd68cadd0a56b76919b523ef11045c9931034ab
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-21 23:58:40 +0300

    remove old permission

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use App\Models\Company;
-use App\Models\Permission;
 use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -13,7 +12,7 @@ class Employee extends Model
     use SoftDeletes;
 
     protected $fillable = [
-        'user_id', 'company_id', 'permission_id', 'created_by', 'updated_by', 'enabled', 'position',
+        'user_id', 'company_id', 'created_by', 'updated_by', 'enabled', 'position',
     ];
 
     protected $casts = [
@@ -40,11 +39,6 @@ class Employee extends Model
         return $this->belongsTo(Company::class);
     }
 
-    public function permission()
-    {
-        return $this->belongsTo(Permission::class);
-    }
-
     public function scopeCompanyEmployees($query)
     {
         return $query->where('company_id', auth()->user()->employee->company_id);
@@ -52,7 +46,7 @@ class Employee extends Model
 
     public function getAll()
     {
-        return $this->companyEmployees()->with(['user', 'permission', 'createdBy', 'updatedBy'])->get();
+        return $this->companyEmployees()->with(['user', 'createdBy', 'updatedBy'])->get();
     }
 
     public function countAllEmployees()
