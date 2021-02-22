bf05071c09ffebf742f9b1b7507815a120c6cc02
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-22 20:00:39 +0300

    modify warehouse policy

--- app/Policies/WarehousePolicy.php
+++ app/Policies/WarehousePolicy.php
@@ -12,60 +12,32 @@ class WarehousePolicy
 
     public function viewAny(User $user)
     {
-        return true;
+        return $user->can('Read Warehouse');
     }
 
     public function view(User $user, Warehouse $warehouse)
     {
-        $isUserOperative = true;
-
         $doesWarehouseBelongToMyCompany = $user->employee->company_id == $warehouse->company_id;
 
-        $canSeeWarehouse = $isUserOperative && $doesWarehouseBelongToMyCompany;
-
-        if ($canSeeWarehouse) {
-            return true;
-        }
-
-        return false;
+        return $doesWarehouseBelongToMyCompany && $user->can('Read Warehouse');
     }
 
     public function create(User $user)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
-        return $isUserOperative;
+        return $user->can('Create Warehouse');
     }
 
     public function update(User $user, Warehouse $warehouse)
     {
-        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
-
         $doesWarehouseBelongToMyCompany = $user->employee->company_id == $warehouse->company_id;
 
-        $canUpdateWarehouse = $isUserAdmin && $doesWarehouseBelongToMyCompany;
-
-        if ($canUpdateWarehouse) {
-            return true;
-        }
-
-        return false;
+        return $doesWarehouseBelongToMyCompany && $user->can('Update Warehouse');
     }
 
     public function delete(User $user, Warehouse $warehouse)
     {
-        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
-
         $doesWarehouseBelongToMyCompany = $user->employee->company_id == $warehouse->company_id;
 
-        $canDeleteWarehouse = $isUserAdmin && $doesWarehouseBelongToMyCompany;
-
-        if ($canDeleteWarehouse) {
-            return true;
-        }
-
-        return false;
+        return $doesWarehouseBelongToMyCompany && $user->can('Delete Warehouse');
     }
 }
