0b1af01180f540fed7ef15cfd91ef7b65f407c3b
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-22 19:53:53 +0300

    modify sales policy

--- app/Policies/SalePolicy.php
+++ app/Policies/SalePolicy.php
@@ -12,60 +12,32 @@ class SalePolicy
 
     public function viewAny(User $user)
     {
-        return true;
+        return $user->can('Read Sale');
     }
 
     public function view(User $user, Sale $sale)
     {
         $doesSaleBelongToMyCompany = $user->employee->company_id == $sale->company_id;
 
-        $canSeeSale = $doesSaleBelongToMyCompany;
-
-        if ($canSeeSale) {
-            return true;
-        }
-
-        return false;
+        return $doesSaleBelongToMyCompany && $user->can('Read Sale');
     }
 
     public function create(User $user)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
-        return $isUserOperative;
+        return $user->can('Create Sale');
     }
 
     public function update(User $user, Sale $sale)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
         $doesSaleBelongToMyCompany = $user->employee->company_id == $sale->company_id;
 
-        $canUpdateSale = $isUserOperative && $doesSaleBelongToMyCompany;
-
-        if ($canUpdateSale) {
-            return true;
-        }
-
-        return false;
+        return $doesSaleBelongToMyCompany && $user->can('Update Sale');
     }
 
     public function delete(User $user, Sale $sale)
     {
-        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
-
         $doesSaleBelongToMyCompany = $user->employee->company_id == $sale->company_id;
 
-        $canDeleteSale = $isUserAdmin && $doesSaleBelongToMyCompany;
-
-        if ($canDeleteSale) {
-            return true;
-        }
-
-        return false;
+        return $doesSaleBelongToMyCompany && $user->can('Delete Sale');
     }
 }
