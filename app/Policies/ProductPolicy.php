1747dfc1c3624eeebf6b8f5a2fbccd8c2f3f1bf7
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-22 19:46:30 +0300

    modify product policy

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -12,62 +12,32 @@ class ProductPolicy
 
     public function viewAny(User $user)
     {
-        return true;
+        return $user->can('Read Product');
     }
 
     public function view(User $user, Product $product)
     {
-        $isUserOperative = true;
-
         $doesProductBelongToMyCompany = $user->employee->company_id == $product->company_id;
 
-        $canSeeProduct = $isUserOperative && $doesProductBelongToMyCompany;
-
-        if ($canSeeProduct) {
-            return true;
-        }
-
-        return false;
+        return $doesProductBelongToMyCompany && $user->can('Read Product');
     }
 
     public function create(User $user)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
-        return $isUserOperative;
+        return $user->can('Create Product');
     }
 
     public function update(User $user, Product $product)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
         $doesProductBelongToMyCompany = $user->employee->company_id == $product->company_id;
 
-        $canUpdateProduct = $isUserOperative && $doesProductBelongToMyCompany;
-
-        if ($canUpdateProduct) {
-            return true;
-        }
-
-        return false;
+        return $doesProductBelongToMyCompany && $user->can('Update Product');
     }
 
     public function delete(User $user, Product $product)
     {
-        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
-
         $doesProductBelongToMyCompany = $user->employee->company_id == $product->company_id;
 
-        $canDeleteProduct = $isUserAdmin && $doesProductBelongToMyCompany;
-
-        if ($canDeleteProduct) {
-            return true;
-        }
-
-        return false;
+        return $doesProductBelongToMyCompany && $user->can('Delete Product');
     }
 }
