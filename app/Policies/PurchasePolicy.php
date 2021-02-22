382b7087e7d7574585a0c5ae72b29eca1edcb3b2
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-22 19:51:02 +0300

    modify purchase policy

--- app/Policies/PurchasePolicy.php
+++ app/Policies/PurchasePolicy.php
@@ -12,60 +12,32 @@ class PurchasePolicy
 
     public function viewAny(User $user)
     {
-        return true;
+        return $user->can('Read Purchase');
     }
 
     public function view(User $user, Purchase $purchase)
     {
         $doesPurchaseBelongToMyCompany = $user->employee->company_id == $purchase->company_id;
 
-        $canSeePurchase = $doesPurchaseBelongToMyCompany;
-
-        if ($canSeePurchase) {
-            return true;
-        }
-
-        return false;
+        return $doesPurchaseBelongToMyCompany && $user->can('Read Purchase');
     }
 
     public function create(User $user)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
-        return $isUserOperative;
+        return $user->can('Create Purchase');
     }
 
     public function update(User $user, Purchase $purchase)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
         $doesPurchaseBelongToMyCompany = $user->employee->company_id == $purchase->company_id;
 
-        $canUpdatePurchase = $isUserOperative && $doesPurchaseBelongToMyCompany;
-
-        if ($canUpdatePurchase) {
-            return true;
-        }
-
-        return false;
+        return $doesPurchaseBelongToMyCompany && $user->can('Update Purchase');
     }
 
     public function delete(User $user, Purchase $purchase)
     {
-        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
-
         $doesPurchaseBelongToMyCompany = $user->employee->company_id == $purchase->company_id;
 
-        $canDeletePurchase = $isUserAdmin && $doesPurchaseBelongToMyCompany;
-
-        if ($canDeletePurchase) {
-            return true;
-        }
-
-        return false;
+        return $doesPurchaseBelongToMyCompany && $user->can('Delete Purchase');
     }
 }
