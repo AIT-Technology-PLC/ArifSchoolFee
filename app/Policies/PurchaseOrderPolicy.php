fd71a2c1bdd3545278c1c0bd70903513de6c4443
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-22 19:48:52 +0300

    modify PO policy

--- app/Policies/PurchaseOrderPolicy.php
+++ app/Policies/PurchaseOrderPolicy.php
@@ -12,60 +12,32 @@ class PurchaseOrderPolicy
 
     public function viewAny(User $user)
     {
-        return true;
+        return $user->can('Read PO');
     }
 
     public function view(User $user, PurchaseOrder $purchaseOrder)
     {
         $doesPurchaseOrderBelongToMyCompany = $user->employee->company_id == $purchaseOrder->company_id;
 
-        $canSeePurchaseOrder = $doesPurchaseOrderBelongToMyCompany;
-
-        if ($canSeePurchaseOrder) {
-            return true;
-        }
-
-        return false;
+        return $doesPurchaseOrderBelongToMyCompany && $user->can('Read PO');
     }
 
     public function create(User $user)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
-        return $isUserOperative;
+        return $user->can('Create PO');
     }
 
     public function update(User $user, PurchaseOrder $purchaseOrder)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
         $doesPurchaseOrderBelongToMyCompany = $user->employee->company_id == $purchaseOrder->company_id;
 
-        $canUpdatePurchaseOrder = $isUserOperative && $doesPurchaseOrderBelongToMyCompany;
-
-        if ($canUpdatePurchaseOrder) {
-            return true;
-        }
-
-        return false;
+        return $doesPurchaseOrderBelongToMyCompany && $user->can('Update PO');
     }
 
     public function delete(User $user, PurchaseOrder $purchaseOrder)
     {
-        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
-
         $doesPurchaseOrderBelongToMyCompany = $user->employee->company_id == $purchaseOrder->company_id;
 
-        $canDeletePurchaseOrder = $isUserAdmin && $doesPurchaseOrderBelongToMyCompany;
-
-        if ($canDeletePurchaseOrder) {
-            return true;
-        }
-
-        return false;
+        return $doesPurchaseOrderBelongToMyCompany && $user->can('Delete PO');
     }
 }
