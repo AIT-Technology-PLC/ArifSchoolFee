a9c46f1fee95245f79c58c2985d25209270f6dce
Nabil Hassen <nabil.hassen08@gmail.com>
2021-02-22 13:08:10 +0300

    modify gdn policies

--- app/Policies/GdnPolicy.php
+++ app/Policies/GdnPolicy.php
@@ -12,60 +12,32 @@ class GdnPolicy
 
     public function viewAny(User $user)
     {
-        return true;
+        return $user->can('Read GDN');
     }
 
     public function view(User $user, Gdn $gdn)
     {
         $doesGdnBelongToMyCompany = $user->employee->company_id == $gdn->company_id;
 
-        $canSeeGdn = $doesGdnBelongToMyCompany;
-
-        if ($canSeeGdn) {
-            return true;
-        }
-
-        return false;
+        return $doesGdnBelongToMyCompany && $user->can('Read GDN');
     }
 
     public function create(User $user)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
-        return $isUserOperative;
+        return $user->can('Create GDN');
     }
 
     public function update(User $user, Gdn $gdn)
     {
-        $isUserOperative = $user->employee->permission_id == 1 ||
-        $user->employee->permission_id == 2 ||
-        $user->employee->permission_id == 3;
-
         $doesGdnBelongToMyCompany = $user->employee->company_id == $gdn->company_id;
 
-        $canUpdateGdn = $isUserOperative && $doesGdnBelongToMyCompany;
-
-        if ($canUpdateGdn) {
-            return true;
-        }
-
-        return false;
+        return $doesGdnBelongToMyCompany && $user->can('Update GDN');
     }
 
     public function delete(User $user, Gdn $gdn)
     {
-        $isUserAdmin = $user->employee->permission_id == 1 || $user->employee->permission_id == 2;
-
         $doesGdnBelongToMyCompany = $user->employee->company_id == $gdn->company_id;
 
-        $canDeleteGdn = $isUserAdmin && $doesGdnBelongToMyCompany;
-
-        if ($canDeleteGdn) {
-            return true;
-        }
-
-        return false;
+        return $doesGdnBelongToMyCompany && $user->can('Delete GDN');
     }
 }
