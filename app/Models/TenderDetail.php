c892459a7b054818cc0dafafca81d76e128eba45
Nabil Hassen <nabil.hassen08@gmail.com>
2021-03-04 21:25:51 +0300

    build tender detail model relations

--- app/Models/TenderDetail.php
+++ app/Models/TenderDetail.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TenderDetail extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    protected $casts = [
+        'is_from_stock' => 'boolean',
+    ];
+
+    public function tender()
+    {
+        return $this->belongsTo(Tender::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
 }
