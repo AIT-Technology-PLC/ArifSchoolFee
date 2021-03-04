79aa3c81a2c81ed07c90cdc9c5b4ef55d60db920
Nabil Hassen <nabil.hassen08@gmail.com>
2021-03-04 23:00:55 +0300

    build general tender checklist edit blade

---
+++ resources/views/general_tender_checklists/edit.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.app')
+
+@section('title')
+    Edit General Tender Checklist
+@endsection
+
+@section('content')
+    <section class="mt-3 mx-3 m-lr-0">
+        <div class="box radius-bottom-0 mb-0 has-background-white-bis">
+            <h1 class="title text-green has-text-weight-medium is-size-5">
+                Edit General Tender Checklist
+            </h1>
+        </div>
+        <form id="formOne" action="{{ route('general-tender-checklists.update', $generalTenderChecklist->id) }}" method="post" enctype="multipart/form-data" novalidate>
+            @csrf
+            @method('PATCH')
+            <div class="box radius-bottom-0 mb-0 radius-top-0">
+                <div class="columns is-marginless is-multiline">
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="item" class="label text-green has-text-weight-normal">Item <sup class="has-text-danger">*</sup> </label>
+                            <div class="control has-icons-left">
+                                <input id="item" name="item" type="text" class="input" placeholder="Checklist Name" value="{{ $generalTenderChecklist->item }}">
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-check"></i>
+                                </span>
+                                @error('item')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="description" class="label text-green has-text-weight-normal">Description</label>
+                            <div class="control has-icons-left">
+                                <textarea name="description" id="description" cols="30" rows="3" class="textarea pl-6" placeholder="Description or note about this checklist">{{ $generalTenderChecklist->description }}</textarea>
+                                <span class="icon is-large is-left">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                                @error('description')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box radius-top-0">
+                <div class="columns is-marginless">
+                    <div class="column is-paddingless">
+                        <div class="buttons is-right">
+                            <button class="button is-white text-green" type="reset">
+                                <span class="icon">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                                <span>
+                                    Cancel
+                                </span>
+                            </button>
+                            <button id="saveButton" class="button bg-green has-text-white">
+                                <span class="icon">
+                                    <i class="fas fa-save"></i>
+                                </span>
+                                <span>
+                                    Save
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </section>
+@endsection
