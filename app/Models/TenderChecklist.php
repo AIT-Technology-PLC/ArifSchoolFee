07442ab8b7a5339fa9136f318f0dc887bc8f506d
Nabil Hassen <nabil.hassen08@gmail.com>
2021-03-04 22:02:48 +0300

    build tender checklist model relation

--- app/Models/TenderChecklist.php
+++ app/Models/TenderChecklist.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TenderChecklist extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    public function tender()
+    {
+        return $this->belongsTo(Tender::class);
+    }
 }
