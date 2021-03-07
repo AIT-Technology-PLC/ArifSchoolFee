d6951a484d25739756f6e13f441a6ed88a918942
Nabil Hassen <nabil.hassen08@gmail.com>
2021-03-07 15:33:30 +0300

    build tender index

---
+++ resources/views/tenders/index.blade.php
@@ -0,0 +1,158 @@
+@extends('layouts.app')
+
+@section('title')
+    Tenders Management
+@endsection
+
+@section('content')
+    <div class="columns is-marginless">
+        <div class="column is-6">
+            <div class="box text-green">
+                <div class="columns is-marginless is-vcentered is-mobile">
+                    <div class="column has-text-centered is-paddingless">
+                        <span class="icon is-large is-size-1">
+                            <i class="fas fa-file-invoice"></i>
+                        </span>
+                    </div>
+                    <div class="column is-paddingless">
+                        <div class="is-size-3 has-text-weight-bold">
+                            {{ $totalTenders }}
+                        </div>
+                        <div class="is-uppercase is-size-7">
+                            Total Tenders
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column is-6">
+            <div class="box text-purple">
+                <div class="columns is-marginless is-vcentered is-mobile">
+                    <div class="column is-paddingless has-text-centered">
+                        <div class="is-uppercase is-size-7">
+                            Create new Tenders for delivery and moving products out
+                        </div>
+                        <div class="is-size-3">
+                            <a href="{{ route('tenders.create') }}" class="button bg-purple has-text-white has-text-weight-medium is-size-7 px-5 py-4 mt-3">
+                                <span class="icon">
+                                    <i class="fas fa-plus-circle"></i>
+                                </span>
+                                <span>
+                                    Create New Tenders
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="mt-3 mx-3 m-lr-0">
+        <div class="box radius-bottom-0 mb-0 has-background-white-bis">
+            <h1 class="title text-green has-text-weight-medium is-size-5">
+                Tenders Management
+            </h1>
+        </div>
+        <div class="box radius-top-0">
+            @include('components.deleted_message', ['model' => 'Tenders'])
+            <div>
+                <table id="table_id" class="is-hoverable is-size-7 display">
+                    <thead>
+                        <tr>
+                            <th id="firstTarget"><abbr> # </abbr></th>
+                            <th class="text-green"><abbr> Project </abbr></th>
+                            <th><abbr> Type </abbr></th>
+                            <th class="text-gold"><abbr> Status </abbr></th>
+                            <th><abbr> Customer </abbr></th>
+                            <th class="has-text-centered"><abbr> Participants </abbr></th>
+                            <th class="has-text-centered"><abbr> Items </abbr></th>
+                            <th class="has-text-right"><abbr> Total Price </abbr></th>
+                            <th><abbr> Description </abbr></th>
+                            <th class="has-text-right"><abbr> Published On </abbr></th>
+                            <th class="has-text-right"><abbr> Closing Date </abbr></th>
+                            <th class="has-text-right"><abbr> Opening Date </abbr></th>
+                            <th><abbr> Prepared By </abbr></th>
+                            <th><abbr> Edited By </abbr></th>
+                            <th><abbr> Actions </abbr></th>
+                        </tr>
+                    </thead>
+                    <tbody class="list">
+                        @foreach ($tenders as $tender)
+                            <tr>
+                                <td> {{ $loop->index + 1 }} </td>
+                                <td class="is-capitalized">
+                                    <span class="tag is-small bg-green has-text-white">
+                                        {{ $tender->name }}
+                                    </span>
+                                </td>
+                                <td class="is-capitalized">
+                                    <span class="tag is-small bg-gold has-text-white">
+                                        {{ $tender->type }}
+                                    </span>
+                                </td>
+                                <td class="is-capitalized">
+                                    <span class="tag is-small bg-gold has-text-white">
+                                        {{ $tender->status }}
+                                    </span>
+                                </td>
+                                <td>
+                                    {{ $tender->customer->company_name ?? 'N/A' }}
+                                </td>
+                                <td class="has-text-centered">
+                                    {{ $tender->participants }}
+                                </td>
+                                <td class="has-text-centered">
+                                    {{ $tender->tender_details_count }}
+                                </td>
+                                <td class="has-text-right">
+                                    {{ $tender->company->currency }}.
+                                    {{ $tender->totalTenderPrice }}
+                                </td>
+                                <td class="description">
+                                    {!! nl2br(e(substr($tender->description, 0, 40))) ?? 'N/A' !!}
+                                </td>
+                                <td class="has-text-right">
+                                    {{ $tender->published_on->toFormattedDateString() }}
+                                </td>
+                                <td class="has-text-right">
+                                    {{ $tender->closing_date->toFormattedDateString() }}
+                                </td>
+                                <td class="has-text-right">
+                                    {{ $tender->opening_date->toFormattedDateString() }}
+                                </td>
+                                <td> {{ $tender->createdBy->name ?? 'N/A' }} </td>
+                                <td> {{ $tender->updatedBy->name ?? 'N/A' }} </td>
+                                <td>
+                                    <a class="is-block" href="{{ route('tenders.show', $tender->id) }}" data-title="View Details">
+                                        <span class="tag mb-3 is-white btn-purple is-outlined is-small text-green has-text-weight-medium">
+                                            <span class="icon">
+                                                <i class="fas fa-info-circle"></i>
+                                            </span>
+                                            <span>
+                                                Details
+                                            </span>
+                                        </span>
+                                    </a>
+                                    <a class="is-block" href="{{ route('tenders.edit', $tender->id) }}" data-title="Modify Tender Data">
+                                        <span class="tag mb-3 is-white btn-green is-outlined is-small text-green has-text-weight-medium">
+                                            <span class="icon">
+                                                <i class="fas fa-pen-square"></i>
+                                            </span>
+                                            <span>
+                                                Edit
+                                            </span>
+                                        </span>
+                                    </a>
+                                    <span class="is-block">
+                                        @include('components.delete_button', ['model' => 'tenders',
+                                        'id' => $tender->id])
+                                    </span>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+@endsection
