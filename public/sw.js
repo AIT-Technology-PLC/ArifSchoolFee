7052b7dcff5f0c58371a1018252d14a6ec05c823
Nabil Hassen <nabil.hassen08@gmail.com>
2021-03-16 23:10:42 +0300

    update sw version

--- public/sw.js
+++ public/sw.js
@@ -58,6 +58,18 @@ precacheAndRoute([
         revision: null,
     },
     {
+        url: "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js",
+        revision: null,
+    },
+    {
+        url: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js",
+        revision: null,
+    },
+    {
+        url: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js",
+        revision: null,
+    },
+    {
         url: "https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js",
         revision: null,
     },
