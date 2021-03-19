21b8f3d6a203359bf30a034ccd5828a01a6db9bc
Nabil Hassen <nabil.hassen08@gmail.com>
2021-03-19 10:49:03 +0300

    remove import from route file

--- routes/standard.php
+++ routes/standard.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\PriceController;
-
 Route::post('merchandises/add-to-inventory/{purchase}',
     'MerchandiseInventoryTransactionController@addToInventory')
     ->name('merchandises.addToInventory');
