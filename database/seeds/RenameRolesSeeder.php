25c1d599cf849315b3bfa768bfb40a015651a03b
Nabil Hassen <nabil.hassen08@gmail.com>
2021-03-28 22:54:17 +0300

    rename roles

---
+++ database/seeds/RenameRolesSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RenameRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        DB::transaction(function () {
+            Role::findByName('Sales Manager')->delete();
+            Role::findByName('Tender')->update(['name' => 'Tender Officer']);
+        });
+    }
+}
