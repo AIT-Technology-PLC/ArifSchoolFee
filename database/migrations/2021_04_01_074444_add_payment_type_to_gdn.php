942fee02e5a9a0b169ab68e6e91b552d44ac7545
Nabil Hassen <nabil.hassen08@gmail.com>
2021-04-01 10:47:40 +0300

    add payment type to gdn

---
+++ database/migrations/2021_04_01_074444_add_payment_type_to_gdn.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPaymentTypeToGdn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('gdns', function (Blueprint $table) {
+            $table->string('payment_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('gdns', function (Blueprint $table) {
+            $table->dropColumn(['payment_type']);
+        });
+    }
+}
