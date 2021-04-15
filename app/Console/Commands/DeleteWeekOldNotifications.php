08599e54573953bf8c470b98a0b4cb298e24f0dd
Nabil Hassen <nabil.hassen08@gmail.com>
2021-04-15 16:26:51 +0300

    Delete notifications every eeek

---
+++ app/Console/Commands/DeleteWeekOldNotifications.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Notifications\DatabaseNotification;
+
+class DeleteWeekOldNotifications extends Command
+{
+    protected $signature = 'delete:notifications';
+
+    protected $description = 'Delete notifications that were created week ago or more';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle(DatabaseNotification $databaseNotification)
+    {
+        $databaseNotification->whereRaw('DATEDIFF(CURRENT_DATE, created_at) >= 7')->delete();
+    }
+}
