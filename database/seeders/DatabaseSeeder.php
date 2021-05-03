9d252c13822eb2c0c1da635fa0fda5c270b8103b
Nabil Hassen <nabil.hassen08@gmail.com>
2021-05-03 12:34:04 +0300

    rafactor databaseseeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            Plan::class,
             PermissionSeeder::class,
             NewCompanySeeder::class,
-            AddNewEmployeeSeeder::class,
         ]);
     }
 }
