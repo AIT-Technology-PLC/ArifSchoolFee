5fbb8329727c638d62e89166005f3d0a4c9baaa5
Nabil Hassen <nabil.hassen08@gmail.com>
2021-05-26 21:50:26 +0300

    make composite unqiue in merchandise table (warehouse, product)

--- database/migrations/2021_05_26_104740_remove_columns_from_merchandises.php
+++ database/migrations/2021_05_26_104740_remove_columns_from_merchandises.php
@@ -31,6 +31,8 @@ class RemoveColumnsFromMerchandises extends Migration
                 'expires_on',
                 'description',
             ]);
+
+            $table->unique(['product_id', 'warehouse_id']);
         });
     }
 
