80feef3cb795eaee827820393ad123e9396bb1e7
Nabil Hassen <nabil.hassen08@gmail.com>
2021-05-28 23:57:34 +0300

    build siv migrations

--- database/migrations/2020_10_28_070207_core_v1.php
+++ database/migrations/2020_10_28_070207_core_v1.php
@@ -604,6 +604,44 @@ class CoreV1 extends Migration
             $table->timestamps();
         });
 
+        Schema::create('sivs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('company_id')->nullable()->unsigned();
+            $table->bigInteger('created_by')->nullable()->unsigned();
+            $table->bigInteger('updated_by')->nullable()->unsigned();
+            $table->bigInteger('approved_by')->nullable()->unsigned();
+            $table->string('code')->unique();
+            $table->longText('description')->nullable();
+            $table->string('received_by')->nullable();
+            $table->dateTime('issued_on')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('company_id');
+
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('approved_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+        });
+
+        Schema::create('siv_details', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('siv_id')->nullable()->unsigned();
+            $table->bigInteger('warehouse_id')->nullable()->unsigned();
+            $table->bigInteger('product_id')->nullable()->unsigned();
+            $table->decimal('quantity', 22);
+            $table->longText('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('siv_id');
+
+            $table->foreign('siv_id')->references('id')->on('sivs')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+        });
+
         Schema::enableForeignKeyConstraints();
     }
 
@@ -645,5 +683,7 @@ class CoreV1 extends Migration
         Schema::drop('tender_details');
         Schema::drop('tender_checklists');
         Schema::drop('notifications');
+        Schema::drop('sivs');
+        Schema::drop('siv_details');
     }
 }
