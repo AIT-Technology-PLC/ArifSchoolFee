f0748409bacfb98012ecfa2c113d1c1c3339a0da
Nabil Hassen <nabil.hassen08@gmail.com>
2021-05-30 23:57:12 +0300

    add permissions for siv

---
+++ database/seeders/AddSivPermissions.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class AddSivPermissions extends Seeder
+{
+
+    public function run()
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        DB::transaction(function () {
+            $systemManager = Role::where('name', 'System Manager')->first();
+
+            DB::table('permissions')->insert([
+                [
+                    'name' => 'Create SIV',
+                    'guard_name' => 'web',
+                ],
+                [
+                    'name' => 'Read SIV',
+                    'guard_name' => 'web',
+                ],
+                [
+                    'name' => 'Update SIV',
+                    'guard_name' => 'web',
+                ],
+                [
+                    'name' => 'Delete SIV',
+                    'guard_name' => 'web',
+                ],
+                [
+                    'name' => 'Approve SIV',
+                    'guard_name' => 'web',
+                ],
+                [
+                    'name' => 'Execute SIV',
+                    'guard_name' => 'web',
+                ],
+                [
+                    'name' => 'Delete Approved SIV',
+                    'guard_name' => 'web',
+                ],
+            ]);
+
+            $systemManager->syncPermissions(Permission::all());
+        });
+    }
+}
