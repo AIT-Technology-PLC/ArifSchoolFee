97bc1f15b19f618ac3e36bd64d775888a0af93de
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-08 17:15:04 +0300

    fix typo in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            Plan::class,
+            PlanTableSeeder::class,
             PermissionSeeder::class,
             NewCompanySeeder::class,
         ]);
