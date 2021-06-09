a74879d5d3913c6d8b401d38668ce3dca73cc1a9
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-09 09:10:34 +0300

    build damage tables

--- database/migrations/2021_06_09_084616_create_damages_table.php
+++ database/migrations/2021_06_09_084616_create_damages_table.php
@@ -15,7 +15,40 @@ class CreateDamagesTable extends Migration
     {
         Schema::create('damages', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('company_id')->nullable()->unsigned();
+            $table->bigInteger('created_by')->nullable()->unsigned();
+            $table->bigInteger('updated_by')->nullable()->unsigned();
+            $table->bigInteger('approved_by')->nullable()->unsigned();
+            $table->string('code')->unique();
+            $table->string('status');
+            $table->longText('description')->nullable();
+            $table->dateTime('issued_on')->nullable();
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('company_id');
+
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('approved_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+        });
+
+        Schema::create('damage_details', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('damage_id')->nullable()->unsigned();
+            $table->bigInteger('warehouse_id')->nullable()->unsigned();
+            $table->bigInteger('product_id')->nullable()->unsigned();
+            $table->decimal('quantity', 22);
+            $table->longText('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('damage_id');
+
+            $table->foreign('damage_id')->references('id')->on('damages')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
@@ -26,6 +59,7 @@ class CreateDamagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('damages');
+        Schema::drop('damage_details');
+        Schema::drop('damages');
     }
 }
