e634ab57e17b5b9f074b28638862137f7f567370
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-09 09:22:39 +0300

    build damage contoller

--- app/Http/Controllers/DamageController.php
+++ app/Http/Controllers/DamageController.php
@@ -3,42 +3,96 @@
 namespace App\Http\Controllers;
 
 use App\Models\Damage;
+use App\Models\Product;
+use App\Models\Warehouse;
+use App\Traits\SubtractInventory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DamageController extends Controller
 {
+    use SubtractInventory;
+
     public function index()
     {
-        //
+        $damages = Damage::companyDamage()->load(['damageDetails', 'createdBy', 'updatedBy', 'approvedBy', 'company']);
+
+        $totalDamages = $damages->count();
+
+        $totalNotApproved = $damages->whereNull('approved_by')->count();
+
+        $totalNotSubtracted = $damages->where('status', 'Not Subtracted From Inventory')->whereNotNull('approved_by')->count();
+
+        return view('damages.index', compact('damages', 'totalDamages', 'totalNotApproved', 'totalNotSubtracted'));
     }
 
-    public function create()
+    public function create(Product $product, Warehouse $warehouse)
     {
-        //
+        $products = $product->getProductNames();
+
+        $warehouses = $warehouse->getAllWithoutRelations();
+
+        $currentDamageCode = (Damage::select('code')->companyDamage()->latest()->first()->code) ?? 0;
+
+        return view('damages.create', compact('products', 'warehouses', 'currentDamageCode'));
     }
 
     public function store(Request $request)
     {
-        //
+        $damage = DB::transaction(function () use ($request) {
+            $damage = $this->damage->create($request->except('damage'));
+
+            $damage->damageDetails()->createMany($request->damage);
+
+            return $damage;
+        });
+
+        return redirect()->route('damages.show', $damage->id);
     }
 
     public function show(Damage $damage)
     {
-        //
+        $damage->load(['damageDetails.product', 'damageDetails.warehouse', 'company']);
+
+        return view('damages.show', compact('damage'));
     }
 
-    public function edit(Damage $damage)
+    public function edit(Damage $damage, Product $product, Warehouse $warehouse)
     {
-        //
+        $products = $product->getProductNames();
+
+        $warehouses = $warehouse->getAllWithoutRelations();
+
+        $damage->load(['damageDetails.product', 'damageDetails.warehouse']);
+
+        return view('damages.edit', compact('damage', 'products', 'warehouses'));
     }
 
     public function update(Request $request, Damage $damage)
     {
-        //
+        DB::transaction(function () use ($request, $damage) {
+            $damage->update($request->except('damage'));
+
+            for ($i = 0; $i < count($request->damage); $i++) {
+                $damage->damageDetails[$i]->update($request->damage[$i]);
+            }
+        });
+
+        return redirect()->route('damages.show', $damage->id);
     }
 
     public function destroy(Damage $damage)
     {
-        //
+        if ($damage->isSubtracted()) {
+            return view('errors.permission_denied');
+        }
+
+        if ($damage->isApproved() && !auth()->user()->can('Delete Approved Damage')) {
+            return view('errors.permission_denied');
+        }
+
+        $damage->forceDelete();
+
+        return redirect()->back()->with('deleted', 'Deleted Successfully');
     }
 }
