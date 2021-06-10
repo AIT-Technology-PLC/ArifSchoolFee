0de0b548c8bec1de399b48fa89cce714f267aeb1
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-10 11:47:21 +0300

    attach SIV automatically for DO and transfer

--- app/Http/Controllers/GdnSivController.php
+++ app/Http/Controllers/GdnSivController.php
@@ -3,20 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\Gdn;
-use Illuminate\Http\Request;
+use App\Models\Siv;
+use Illuminate\Support\Facades\DB;
 
 class GdnSivController extends Controller
 {
-    public function __invoke(Gdn $gdn, Request $request)
+    public function __invoke(Gdn $gdn)
     {
         $this->authorize('view', $gdn);
 
-        $request->merge([
-            'purpose' => 'DO',
-            'ref_num' => $gdn->code,
-            'siv' => $gdn->gdnDetails->toArray(),
-        ]);
+        $this->authorize('create', Siv::class);
 
-        return redirect()->route('sivs.create')->withInput($request->all());
+        $siv = DB::transaction(function () use ($gdn) {
+            $currentSivCode = (Siv::select('code')->companySiv()->latest()->first()->code) ?? 0;
+
+            $siv = Siv::create([
+                'code' => $currentSivCode + 1,
+                'purpose' => 'DO',
+                'ref_num' => $gdn->code,
+                'issued_on' => today(),
+                'created_by' => auth()->user()->id,
+                'updated_by' => auth()->user()->id,
+                'company_id' => userCompany()->id,
+            ]);
+
+            $sivDetails = $gdn->gdnDetails()
+                ->get(['product_id', 'warehouse_id', 'quantity'])
+                ->toArray();
+
+            $siv->sivDetails()->createMany($sivDetails);
+
+            return $siv;
+        });
+
+        return redirect()->route('sivs.show', $siv->id);
     }
 }
--- app/Http/Controllers/TransferSivController.php
+++ app/Http/Controllers/TransferSivController.php
@@ -2,21 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Siv;
 use App\Models\Transfer;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TransferSivController extends Controller
 {
-    public function __invoke(Transfer $transfer, Request $request)
+    public function __invoke(Transfer $transfer)
     {
         $this->authorize('view', $transfer);
 
-        $request->merge([
-            'purpose' => 'Transfer',
-            'ref_num' => $transfer->code,
-            'siv' => $transfer->transferDetails->toArray(),
-        ]);
+        $this->authorize('create', Siv::class);
 
-        return redirect()->route('sivs.create')->withInput($request->all());
+        $siv = DB::transaction(function () use ($transfer) {
+            $currentSivCode = (Siv::select('code')->companySiv()->latest()->first()->code) ?? 0;
+
+            $siv = Siv::create([
+                'code' => $currentSivCode + 1,
+                'purpose' => 'Transfer',
+                'ref_num' => $transfer->code,
+                'issued_on' => today(),
+                'created_by' => auth()->user()->id,
+                'updated_by' => auth()->user()->id,
+                'company_id' => userCompany()->id,
+            ]);
+
+            $sivDetails = $transfer->transferDetails()
+                ->get(['product_id', 'warehouse_id', 'quantity'])
+                ->toArray();
+
+            $siv->sivDetails()->createMany($sivDetails);
+
+            return $siv;
+        });
+
+        return redirect()->route('sivs.show', $siv->id);
     }
 }
