d8aa196f698895ce854a146ad53fc379ce96e284
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-13 18:16:36 +0300

    build adjustment edit blade

---
+++ resources/views/adjustments/edit.blade.php
@@ -0,0 +1,207 @@
+@extends('layouts.app')
+
+@section('title')
+    Edit Adjustment
+@endsection
+
+@section('content')
+    <section class="mt-3 mx-3 m-lr-0">
+        <div class="box radius-bottom-0 mb-0 has-background-white-bis">
+            <h1 class="title text-green has-text-weight-medium is-size-5">
+                Edit Adjustment
+            </h1>
+        </div>
+        <form id="formOne" action="{{ route('adjustments.update', $adjustment->id) }}" method="POST" enctype="multipart/form-data" novalidate>
+            @csrf
+            @method('PATCH')
+            <div class="box radius-bottom-0 mb-0 radius-top-0">
+                <div class="columns is-marginless is-multiline">
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="code" class="label text-green has-text-weight-normal">Adjustment Number <sup class="has-text-danger">*</sup> </label>
+                            <div class="control has-icons-left">
+                                <input class="input" type="number" name="code" id="code" value="{{ $adjustment->code }}">
+                                <span class="icon is-large is-left">
+                                    <i class="fas fa-hashtag"></i>
+                                </span>
+                                @error('code')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="issued_on" class="label text-green has-text-weight-normal"> Issued On <sup class="has-text-danger">*</sup> </label>
+                            <div class="control has-icons-left">
+                                <input class="input" type="date" name="issued_on" id="issued_on" placeholder="mm/dd/yyyy" value="{{ $adjustment->issued_on->toDateString() }}">
+                                <div class="icon is-small is-left">
+                                    <i class="fas fa-calendar-alt"></i>
+                                </div>
+                                @error('issued_on')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="description" class="label text-green has-text-weight-normal">Description <sup class="has-text-danger"></sup></label>
+                            <div class="control has-icons-left">
+                                <textarea name="description" id="description" cols="30" rows="3" class="textarea pl-6" placeholder="Description or note to be taken">{{ $adjustment->description ?? '' }}</textarea>
+                                <span class="icon is-large is-left">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                                @error('description')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @foreach ($adjustment->adjustmentDetails as $adjustmentDetail)
+                    <div class="has-text-weight-medium has-text-left mt-5">
+                        <span class="tag bg-green has-text-white is-medium radius-bottom-0">
+                            Item {{ $loop->index + 1 }}
+                        </span>
+                    </div>
+                    <div class="box has-background-white-bis radius-top-0">
+                        <div name="adjustmentFormGroup" class="columns is-marginless is-multiline">
+                            <div class="column is-6">
+                                <div class="field">
+                                    <label for="adjustment[{{ $loop->index }}][product_id]" class="label text-green has-text-weight-normal"> Product <sup class="has-text-danger">*</sup> </label>
+                                    <div class="control has-icons-left">
+                                        <div class="select is-fullwidth">
+                                            <select id="adjustment[{{ $loop->index }}][product_id]" name="adjustment[{{ $loop->index }}][product_id]" onchange="getProductSelected(this.id, this.value)">
+                                                <option selected disabled>Select Product</option>
+                                                @foreach ($products as $product)
+                                                    <option value="{{ $product->id }}" {{ $adjustmentDetail->product_id == $product->id ? 'selected' : '' }}>{{ $product->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="icon is-small is-left">
+                                            <i class="fas fa-th"></i>
+                                        </div>
+                                        @error('adjustment.' . $loop->index . '.product_id')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column is-6">
+                                <label for="adjustment[{{ $loop->index }}][quantity]" class="label text-green has-text-weight-normal">Quantity <sup class="has-text-danger">*</sup> </label>
+                                <div class="field has-addons">
+                                    <div class="control has-icons-left is-expanded">
+                                        <input id="adjustment[{{ $loop->index }}][quantity]" name="adjustment[{{ $loop->index }}][quantity]" type="number" class="input" placeholder="Quantity" value="{{ $adjustmentDetail->quantity ?? '' }}">
+                                        <span class="icon is-small is-left">
+                                            <i class="fas fa-balance-scale"></i>
+                                        </span>
+                                        @error('adjustment.' . $loop->index . '.quantity')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <div class="control">
+                                        <button id="adjustment[{{ $loop->index }}][product_id]Quantity" class="button bg-green has-text-white" type="button"></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column is-6">
+                                <div class="field">
+                                    <label for="adjustment[{{ $loop->index }}][is_subtract]" class="label text-green has-text-weight-normal"> Type <sup class="has-text-danger">*</sup> </label>
+                                    <div class="control has-icons-left">
+                                        <div class="select is-fullwidth">
+                                            <select id="adjustment[{{ $loop->index }}][is_subtract]" name="adjustment[{{ $loop->index }}][is_subtract]">
+                                                <option value="0" {{ $adjustmentDetail->is_subtract ? '' : 'selected' }}> Add </option>
+                                                <option value="1" {{ $adjustmentDetail->is_subtract ? 'selected' : '' }}> Subtract </option>
+                                            </select>
+                                        </div>
+                                        <div class="icon is-small is-left">
+                                            <i class="fas fa-sort"></i>
+                                        </div>
+                                        @error('adjustment.' . $loop->index . '.is_subtract')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column is-6">
+                                <div class="field">
+                                    <label for="adjustment[{{ $loop->index }}][warehouse_id]" class="label text-green has-text-weight-normal"> Warehouse <sup class="has-text-danger">*</sup> </label>
+                                    <div class="control has-icons-left">
+                                        <div class="select is-fullwidth">
+                                            <select id="adjustment[{{ $loop->index }}][warehouse_id]" name="adjustment[{{ $loop->index }}][warehouse_id]">
+                                                @foreach ($warehouses as $warehouse)
+                                                    <option value="{{ $warehouse->id }}" {{ $adjustmentDetail->warehouse->id == $warehouse->id ? 'selected' : '' }}>{{ $warehouse->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="icon is-small is-left">
+                                            <i class="fas fa-warehouse"></i>
+                                        </div>
+                                        @error('adjustment.' . $loop->index . '.warehouse_id')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column is-6">
+                                <div class="field">
+                                    <label for="adjustment[{{ $loop->index }}][reason]" class="label text-green has-text-weight-normal">Reason <sup class="has-text-danger">*</sup></label>
+                                    <div class="control has-icons-left">
+                                        <textarea name="adjustment[{{ $loop->index }}][reason]" id="adjustment[{{ $loop->index }}][reason]" cols="30" rows="3" class="textarea pl-6" placeholder="Describe reason for adjusting this product level">{{ $adjustmentDetail->reason ?? '' }}</textarea>
+                                        <span class="icon is-large is-left">
+                                            <i class="fas fa-edit"></i>
+                                        </span>
+                                        @error('adjustment.' . $loop->index . '.reason')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="box radius-top-0">
+                <div class="columns is-marginless">
+                    <div class="column is-paddingless">
+                        <div class="buttons is-right">
+                            <button class="button is-white text-green" type="reset">
+                                <span class="icon">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                                <span>
+                                    Cancel
+                                </span>
+                            </button>
+                            <button id="saveButton" class="button bg-green has-text-white">
+                                <span class="icon">
+                                    <i class="fas fa-save"></i>
+                                </span>
+                                <span>
+                                    Save
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </section>
+@endsection
