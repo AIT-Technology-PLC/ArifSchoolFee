673b388a6c8742e8fd5eee1e84147cfbd3be5a40
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-15 14:52:03 +0300

    assign features to plans

--- database/seeders/FeatureTableSeeder.php
+++ database/seeders/FeatureTableSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Feature;
+use App\Models\Plan;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class FeatureTableSeeder extends Seeder
 {
@@ -14,99 +16,117 @@ class FeatureTableSeeder extends Seeder
      */
     public function run()
     {
-        Feature::firstOrCreate([
-            'name' => 'Merchandise Inventory Level',
-            'is_enabled' => 1,
-        ]);
+        DB::transaction(function () {
+            Feature::firstOrCreate([
+                'name' => 'Merchandise Inventory Level',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Inventory History',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Inventory History',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Gdn',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Gdn',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Grn',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Grn',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Transfer',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Transfer',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Damage',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Damage',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Inventory Adjustments',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Inventory Adjustments',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Siv',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Siv',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Sales Invoice',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Sales Invoice',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Proforma Invoices',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Proforma Invoices',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Customer Management',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Customer Management',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Purchase Order',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Purchase Order',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Tender Management',
-            'is_enabled' => 1,
-        ]);
+            $tenderFeature = Feature::firstOrCreate([
+                'name' => 'Tender Management',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Purchase Management',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Purchase Management',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Supplier Management',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Supplier Management',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Product Management',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Product Management',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'Warehouse Management',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'Warehouse Management',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'User Mangement',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'User Mangement',
+                'is_enabled' => 1,
+            ]);
 
-        Feature::firstOrCreate([
-            'name' => 'General Settings',
-            'is_enabled' => 1,
-        ]);
+            Feature::firstOrCreate([
+                'name' => 'General Settings',
+                'is_enabled' => 1,
+            ]);
+
+            $tender = Plan::where('name', 'tender')->first();
+            $professional = Plan::where('name', 'professional')->first();
+            $premium = Plan::where('name', 'premium')->first();
+            $enterprise = Plan::where('name', 'enterprise')->first();
+            $manufacture = Plan::where('name', 'manufacture')->first();
+
+            $tender->features()->save($tenderFeature);
+
+            $professional->features()->saveMany(Feature::all());
+
+            $premium->features()->saveMany(Feature::all());
+
+            $enterprise->features()->saveMany(Feature::all());
+
+            $manufacture->features()->saveMany(Feature::all());
+        });
     }
 }
