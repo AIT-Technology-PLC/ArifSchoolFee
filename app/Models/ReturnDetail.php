66729f3d59008087d9a13cf41a4263618c41df4f
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-21 20:47:33 +0300

    build return models

---
+++ app/Models/ReturnDetail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ReturnDetail extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    public function returns()
+    {
+        return $this->belongsTo(Returnn::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+
+    public function getByWarehouseAndProduct($warehouse, $product)
+    {
+        return $this->where([
+            ['warehouse_id', $warehouse->id],
+            ['product_id', $product->id],
+        ])
+            ->whereIn('return_id', function ($query) {
+                $query->select('id')
+                    ->from('returns')
+                    ->where('company_id', userCompany()->id)
+                    ->whereNotNull('returned_by');
+            })
+            ->get()
+            ->load(['return.customer', 'product']);
+    }
+}
---
+++ app/Models/Returnn.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Approvable;
+use App\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
+class Returnn extends Model
+{
+    use HasFactory, SoftDeletes, Approvable;
+
+    protected $table = "returns";
+
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    protected $casts = [
+        'issued_on' => 'datetime',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function returnedBy()
+    {
+        return $this->belongsTo(User::class, 'returned_by');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function returnDetails()
+    {
+        return $this->hasMany(ReturnDetail::class);
+    }
+
+    public function scopeCompanyReturn($query)
+    {
+        return $query->where('company_id', userCompany()->id);
+    }
+
+    public function getCodeAttribute($value)
+    {
+        return Str::after($value, userCompany()->id . '_');
+    }
+
+    public function getTotalCreditAttribute()
+    {
+        $totalPrice = $this->returnDetails
+            ->reduce(function ($carry, $item) {
+                return $carry + ($item->unit_price * $item->quantity);
+            }, 0);
+
+        return $totalPrice;
+    }
+
+    public function getTotalCreditAfterVATAttribute()
+    {
+        return $this->totalCredit * 1.15;
+    }
+
+    public function returnn()
+    {
+        $this->returned_by = auth()->id();
+
+        $this->save();
+    }
+
+    public function isReturned()
+    {
+        if (is_null($this->returned_by)) {
+            return false;
+        }
+
+        return true;
+    }
+}
