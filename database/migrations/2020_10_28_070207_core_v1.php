1b8026db62a95cd1ec81414d2ad0eff4f7bb3fe7
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-22 15:34:01 +0300

    update corev1 migration

--- database/migrations/2020_10_28_070207_core_v1.php
+++ database/migrations/2020_10_28_070207_core_v1.php
@@ -797,6 +797,49 @@ class CoreV1 extends Migration
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
         });
 
+        Schema::create('returns', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('customer_id')->nullable()->unsigned();
+            $table->bigInteger('company_id')->nullable()->unsigned();
+            $table->bigInteger('created_by')->nullable()->unsigned();
+            $table->bigInteger('updated_by')->nullable()->unsigned();
+            $table->bigInteger('approved_by')->nullable()->unsigned();
+            $table->bigInteger('returned_by')->nullable()->unsigned();
+            $table->string('code')->unique();
+            $table->longText('description')->nullable();
+            $table->dateTime('issued_on')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('company_id');
+            $table->index('customer_id');
+
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('approved_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('returned_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+        });
+
+        Schema::create('return_details', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('return_id')->nullable()->unsigned();
+            $table->bigInteger('warehouse_id')->nullable()->unsigned();
+            $table->bigInteger('product_id')->nullable()->unsigned();
+            $table->decimal('quantity', 22);
+            $table->decimal('unit_price', 22)->nullable();
+            $table->longText('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('return_id');
+
+            $table->foreign('return_id')->references('id')->on('returns')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+        });
+
         Schema::enableForeignKeyConstraints();
     }
 
@@ -850,5 +893,7 @@ class CoreV1 extends Migration
         Schema::drop('adjustments');
         Schema::drop('featurables');
         Schema::drop('features');
+        Schema::drop('return_details');
+        Schema::drop('returns');
     }
 }
