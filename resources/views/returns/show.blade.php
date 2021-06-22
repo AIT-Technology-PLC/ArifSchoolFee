3995df7bad1b08fb7c4197af060bb028deb0b562
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-22 14:05:38 +0300

    build returns show view

---
+++ resources/views/returns/show.blade.php
@@ -0,0 +1,286 @@
+@extends('layouts.app')
+
+@section('title')
+    Return Details
+@endsection
+
+@section('content')
+    <div class="box mt-3 mx-3 m-lr-0">
+        <div class="columns is-marginless is-multiline">
+            <div class="column is-6">
+                <div>
+                    <div class="columns is-marginless is-vcentered is-mobile text-green">
+                        <div class="column is-1">
+                            <span class="icon is-size-3">
+                                <i class="fas fa-file-invoice"></i>
+                            </span>
+                        </div>
+                        <div class="column m-lr-20">
+                            <div class="is-size- has-text-weight-bold">
+                                {{ $return->code ?? 'N/A' }}
+                            </div>
+                            <div class="is-uppercase is-size-7">
+                                Return No
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-6">
+                <div>
+                    <div class="columns is-marginless is-vcentered is-mobile text-green">
+                        <div class="column is-1">
+                            <span class="icon is-size-3">
+                                <i class="fas fa-user"></i>
+                            </span>
+                        </div>
+                        <div class="column m-lr-20">
+                            <div class="is-size- has-text-weight-bold">
+                                {{ $return->customer->company_name ?? 'N/A' }}
+                            </div>
+                            <div class="is-uppercase is-size-7">
+                                Customer
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-6">
+                <div>
+                    <div class="columns is-marginless is-vcentered is-mobile text-green">
+                        <div class="column is-1">
+                            <span class="icon is-size-3">
+                                <i class="fas fa-calendar-day"></i>
+                            </span>
+                        </div>
+                        <div class="column m-lr-20">
+                            <div class="is-size- has-text-weight-bold">
+                                {{ $return->issued_on->toFormattedDateString() ?? 'N/A' }}
+                            </div>
+                            <div class="is-uppercase is-size-7">
+                                Issued On
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-6">
+                <div>
+                    <div class="columns is-marginless is-vcentered is-mobile text-green">
+                        <div class="column is-1">
+                            <span class="icon is-size-3">
+                                <i class="fas fa-dollar-sign"></i>
+                            </span>
+                        </div>
+                        <div class="column m-lr-20">
+                            <div class="is-size- has-text-weight-bold">
+                                {{ number_format($return->totalCredit, 2) }}
+                            </div>
+                            <div class="is-uppercase is-size-7">
+                                Total Credit ({{ $return->company->currency }})
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-6">
+                <div>
+                    <div class="columns is-marginless is-vcentered is-mobile text-purple">
+                        <div class="column is-1">
+                            <span class="icon is-size-3">
+                                <i class="fas fa-dollar-sign"></i>
+                            </span>
+                        </div>
+                        <div class="column m-lr-20">
+                            <div class="is-size- has-text-weight-bold">
+                                {{ number_format($return->totalCreditAfterVAT, 2) }}
+                            </div>
+                            <div class="is-uppercase is-size-7">
+                                Total Credit After VAT ({{ $return->company->currency }})
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-12">
+                <div>
+                    <div class="columns is-marginless is-vcentered text-green">
+                        <div class="column">
+                            <div class="has-text-weight-bold">
+                                Details
+                            </div>
+                            <div class="is-size-7 mt-3">
+                                {!! is_null($return->description) ? 'N/A' : nl2br(e($return->description)) !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="mt-3 mx-3 m-lr-0">
+        <div class="box radius-bottom-0 mb-0 has-background-white-bis">
+            <div class="level">
+                <div class="level-left">
+                    <div class="level-item is-justify-content-left">
+                        <div>
+                            <h1 class="title text-green has-text-weight-medium is-size-5">
+                                Return Details
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="level-right">
+                    <div class="level-item is-justify-content-left">
+                        <div>
+                            @if ($return->isApproved())
+                                <button id="printReturn" class="button is-small bg-purple has-text-white is-hidden-mobile" onclick="openInNewTab('/returns/{{ $return->id }}/print')">
+                                    <span class="icon">
+                                        <i class="fas fa-print"></i>
+                                    </span>
+                                    <span>
+                                        Print
+                                    </span>
+                                </button>
+                            @endif
+                            <a href="{{ route('returns.edit', $return->id) }}" class="button is-small bg-green has-text-white">
+                                <span class="icon">
+                                    <i class="fas fa-pen"></i>
+                                </span>
+                                <span>
+                                    Edit
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="box radius-bottom-0 mb-0 radius-top-0">
+            <div class="notification bg-green has-text-white has-text-weight-medium {{ session('successMessage') ? '' : 'is-hidden' }}">
+                <span class="icon">
+                    <i class="fas fa-check-circle"></i>
+                </span>
+                <span>
+                    {{ session('successMessage') }}
+                </span>
+            </div>
+            @if ($return->isApproved() && $return->isReturned())
+                <div class="box is-shadowless bg-lightgreen has-text-left mb-6">
+                    <p class="has-text-grey text-green is-size-6">
+                        <span class="icon">
+                            <i class="fas fa-check-circle"></i>
+                        </span>
+                        <span>
+                            Products have been added to the inventory.
+                        </span>
+                    </p>
+                </div>
+            @endif
+            @if ($return->isApproved() && !$return->isReturned())
+                @can('Make Return')
+                    <div class="box has-background-white-ter has-text-left mb-6">
+                        <p class="has-text-grey text-purple is-size-7">
+                            Product(s) listed below are still not added to the inventory.
+                            <br>
+                            Click on the button below to add product(s) to the inventory.
+                        </p>
+                        <form id="formOne" action="{{ route('returns.add', $return->id) }}" method="post" novalidate>
+                            @csrf
+                            <button data-type="Return" data-action="add" data-description="" class="button bg-purple has-text-white mt-5 is-size-7-mobile">
+                                <span class="icon">
+                                    <i class="fas fa-plus-circle"></i>
+                                </span>
+                                <span>
+                                    Add to Inventory
+                                </span>
+                            </button>
+                        </form>
+                    </div>
+                @else
+                    <div class="box is-shadowless bg-lightpurple has-text-left mb-6">
+                        <p class="has-text-grey text-purple is-size-6">
+                            <span class="icon">
+                                <i class="fas fa-exclamation-circle"></i>
+                            </span>
+                            <span>
+                                Product(s) listed below are still not added to the inventory.
+                            </span>
+                        </p>
+                    </div>
+                @endcan
+            @endif
+            @if (!$return->isApproved())
+                @can('Approve Return')
+                    <div class="box has-background-white-ter has-text-left mb-6">
+                        <p class="has-text-grey text-purple is-size-7">
+                            This Return has not been approved.
+                            <br>
+                            Click on the button below to approve this Return.
+                        </p>
+                        <form id="formOne" action="{{ route('returns.approve', $return->id) }}" method="post" novalidate>
+                            @csrf
+                            <button data-type="Return" data-action="approve" data-description="" class="button bg-purple has-text-white mt-5 is-size-7-mobile">
+                                <span class="icon">
+                                    <i class="fas fa-signature"></i>
+                                </span>
+                                <span>
+                                    Approve Return
+                                </span>
+                            </button>
+                        </form>
+                    </div>
+                @else
+                    <div class="box is-shadowless bg-lightpurple has-text-left mb-6">
+                        <p class="has-text-grey text-purple is-size-6">
+                            <span class="icon">
+                                <i class="fas fa-exclamation-circle"></i>
+                            </span>
+                            <span>
+                                This Return has not been approved.
+                            </span>
+                        </p>
+                    </div>
+                @endcan
+            @endif
+            <div class="table-container">
+                <table class="table is-hoverable is-fullwidth is-size-7">
+                    <thead>
+                        <tr>
+                            <th><abbr> # </abbr></th>
+                            <th><abbr> To </abbr></th>
+                            <th><abbr> Product </abbr></th>
+                            <th><abbr> Quantity </abbr></th>
+                            <th><abbr> Unit Price </abbr></th>
+                            <th><abbr> Description </abbr></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($return->returnDetails as $returnDetail)
+                            <tr>
+                                <td> {{ $loop->index + 1 }} </td>
+                                <td class="is-capitalized">
+                                    {{ $returnDetail->warehouse->name }}
+                                </td>
+                                <td class="is-capitalized">
+                                    {{ $returnDetail->product->name }}
+                                </td>
+                                <td>
+                                    {{ number_format($returnDetail->quantity, 2) }}
+                                    {{ $returnDetail->product->unit_of_measurement }}
+                                </td>
+                                <td>
+                                    {{ $return->company->currency }}.
+                                    {{ number_format($returnDetail->unit_price, 2) }}
+                                </td>
+                                <td>
+                                    {!! nl2br(e($returnDetail->description)) !!}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+@endsection
