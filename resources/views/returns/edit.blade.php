dd2e912af2e9c89c64acc386e0d35c806d861c34
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-22 13:47:38 +0300

    build returns edit view

---
+++ resources/views/returns/edit.blade.php
@@ -0,0 +1,224 @@
+@extends('layouts.app')
+
+@section('title')
+    Edit Return
+@endsection
+
+@section('content')
+    <section class="mt-3 mx-3 m-lr-0">
+        <div class="box radius-bottom-0 mb-0 has-background-white-bis">
+            <h1 class="title text-green has-text-weight-medium is-size-5">
+                Edit Return
+            </h1>
+        </div>
+        <form id="formOne" action="{{ route('returns.update', $return->id) }}" method="POST" enctype="multipart/form-data" novalidate>
+            @csrf
+            @method('PATCH')
+            <div class="box radius-bottom-0 mb-0 radius-top-0">
+                <div class="columns is-marginless is-multiline">
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="code" class="label text-green has-text-weight-normal">Return Number <sup class="has-text-danger">*</sup> </label>
+                            <div class="control has-icons-left">
+                                <input class="input" type="number" name="code" id="code" value="{{ $return->code }}">
+                                <span class="icon is-large is-left">
+                                    <i class="fas fa-hashtag"></i>
+                                </span>
+                                @error('code')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="customer_id" class="label text-green has-text-weight-normal"> Customer <sup class="has-text-danger"></sup> </label>
+                            <div class="control has-icons-left">
+                                <div class="select is-fullwidth">
+                                    <select id="customer_id" name="customer_id">
+                                        <option selected disabled>Select Customer</option>
+                                        @foreach ($customers as $customer)
+                                            <option value="{{ $customer->id }}" {{ $return->customer_id == $customer->id ? 'selected' : '' }}>{{ $customer->company_name }}</option>
+                                        @endforeach
+                                        <option value="">None</option>
+                                    </select>
+                                </div>
+                                <div class="icon is-small is-left">
+                                    <i class="fas fa-user"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="issued_on" class="label text-green has-text-weight-normal"> Issued On <sup class="has-text-danger">*</sup> </label>
+                            <div class="control has-icons-left">
+                                <input class="input" type="date" name="issued_on" id="issued_on" placeholder="mm/dd/yyyy" value="{{ $return->issued_on->toDateString() }}">
+                                <div class="icon is-small is-left">
+                                    <i class="fas fa-calendar-alt"></i>
+                                </div>
+                                @error('issued_on')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="field">
+                            <label for="description" class="label text-green has-text-weight-normal">Description <sup class="has-text-danger"></sup> </label>
+                            <div class="control has-icons-left">
+                                <textarea name="description" id="description" cols="30" rows="3" class="textarea pl-6" placeholder="Description or note to be taken">{{ $return->description ?? '' }}</textarea>
+                                <span class="icon is-large is-left">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                                @error('description')
+                                    <span class="help has-text-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @foreach ($return->returnDetails as $returnDetail)
+                    <div class="has-text-weight-medium has-text-left mt-5">
+                        <span class="tag bg-green has-text-white is-medium radius-bottom-0">
+                            Item {{ $loop->index + 1 }}
+                        </span>
+                    </div>
+                    <div class="box has-background-white-bis radius-top-0">
+                        <div name="returnFormGroup" class="columns is-marginless is-multiline">
+                            <div class="column is-6">
+                                <div class="field">
+                                    <label for="return[{{ $loop->index }}][product_id]" class="label text-green has-text-weight-normal"> Product <sup class="has-text-danger">*</sup> </label>
+                                    <div class="control has-icons-left">
+                                        <div class="select is-fullwidth">
+                                            <select id="return[{{ $loop->index }}][product_id]" name="return[{{ $loop->index }}][product_id]" onchange="getProductSelected(this.id, this.value)">
+                                                <option selected disabled>Select Product</option>
+                                                @foreach ($products as $product)
+                                                    <option value="{{ $product->id }}" {{ $returnDetail->product_id == $product->id ? 'selected' : '' }}>{{ $product->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="icon is-small is-left">
+                                            <i class="fas fa-th"></i>
+                                        </div>
+                                        @error('return.0.product_id')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column is-6">
+                                <div class="field">
+                                    <label for="return[{{ $loop->index }}][warehouse_id]" class="label text-green has-text-weight-normal"> To <sup class="has-text-danger">*</sup> </label>
+                                    <div class="control has-icons-left">
+                                        <div class="select is-fullwidth">
+                                            <select id="return[{{ $loop->index }}][warehouse_id]" name="return[{{ $loop->index }}][warehouse_id]">
+                                                @foreach ($warehouses as $warehouse)
+                                                    <option value="{{ $warehouse->id }}" {{ $returnDetail->warehouse_id == $warehouse->id ? 'selected' : '' }}>{{ $warehouse->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="icon is-small is-left">
+                                            <i class="fas fa-warehouse"></i>
+                                        </div>
+                                        @error('return.0.warehouse_id')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column is-6">
+                                <label for="return[{{ $loop->index }}][quantity]" class="label text-green has-text-weight-normal">Quantity <sup class="has-text-danger">*</sup> </label>
+                                <div class="field has-addons">
+                                    <div class="control has-icons-left is-expanded">
+                                        <input id="return[{{ $loop->index }}][quantity]" name="return[{{ $loop->index }}][quantity]" type="number" class="input" placeholder="Quantity" value="{{ $returnDetail->quantity }}">
+                                        <span class="icon is-small is-left">
+                                            <i class="fas fa-balance-scale"></i>
+                                        </span>
+                                        @error('return.0.quantity')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <div class="control">
+                                        <button id="return[{{ $loop->index }}][product_id]Quantity" class="button bg-green has-text-white" type="button">{{ $returnDetail->product->unit_of_measurement }}</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column is-6">
+                                <label for="return[{{ $loop->index }}][unit_price]" class="label text-green has-text-weight-normal">Unit Price<sup class="has-text-weight-light"> (Before VAT)</sup> <sup class="has-text-danger"></sup> </label>
+                                <div class="field has-addons">
+                                    <div class="control has-icons-left is-expanded">
+                                        <input id="return[{{ $loop->index }}][unit_price]" name="return[{{ $loop->index }}][unit_price]" type="number" class="input" placeholder="Sale Price" value="{{ $returnDetail->unit_price ?? '0.00' }}">
+                                        <span class="icon is-small is-left">
+                                            <i class="fas fa-money-bill"></i>
+                                        </span>
+                                        @error('return.0.unit_price')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <div class="control">
+                                        <button id="return[{{ $loop->index }}][product_id]Price" class="button bg-green has-text-white" type="button">{{ $returnDetail->product->unit_of_measurement }}</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column is-6">
+                                <div class="field">
+                                    <label for="return[{{ $loop->index }}][description]" class="label text-green has-text-weight-normal">Additional Notes <sup class="has-text-danger"></sup></label>
+                                    <div class="control has-icons-left">
+                                        <textarea name="return[{{ $loop->index }}][description]" id="return[{{ $loop->index }}][description]" cols="30" rows="3" class="textarea pl-6" placeholder="Description or note to be taken">{{ $returnDetail->description ?? '' }}</textarea>
+                                        <span class="icon is-large is-left">
+                                            <i class="fas fa-edit"></i>
+                                        </span>
+                                        @error('return.0.description')
+                                            <span class="help has-text-danger" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="box radius-top-0">
+                <div class="columns is-marginless">
+                    <div class="column is-paddingless">
+                        <div class="buttons is-right">
+                            <button class="button is-white text-green" type="reset">
+                                <span class="icon">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                                <span>
+                                    Cancel
+                                </span>
+                            </button>
+                            <button id="saveButton" class="button bg-green has-text-white">
+                                <span class="icon">
+                                    <i class="fas fa-save"></i>
+                                </span>
+                                <span>
+                                    Save
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </section>
+@endsection
