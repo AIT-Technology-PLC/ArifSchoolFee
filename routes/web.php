047a9ea3f10445257c832a6b36627c2ccd067f71
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-22 22:16:52 +0300

    remove auth routes and list them manually

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,15 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth as Auth;
 use App\Http\Controllers\OfflineController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
+Route::get('/offline', [OfflineController::class, 'offline']);
 
-Route::get('offline', [OfflineController::class, 'offline']);
+Route::get('/register', [Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [Auth\RegisterController::class, 'register']);
 
-Auth::routes();
+Route::get('/login', [Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/auth/login', [Auth\LoginController::class, 'login'])->name('post.login');
 
-Route::post('/auth/login', [LoginController::class, 'login'])->name('post.login');
+Route::post('logout', [Auth\LoginController::class, 'logout'])->name('logout');
