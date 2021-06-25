49d418b890408c73a31d4d1f025cedf77d424bea
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-25 12:21:31 +0300

    add more tables on dt init

--- public/js/app.js
+++ public/js/app.js
@@ -876,7 +876,7 @@ function initiateDataTables() {
     const table = d.getElementById("table_id");
     let dateTargets = JSON.parse(table.dataset.date);
     let numericTargets = JSON.parse(table.dataset.numeric);
-    $("#table_id").DataTable({
+    $("#table_id, #table2, #table3").DataTable({
         order: [[0, "desc"]],
         responsive: true,
         scrollCollapse: true,
