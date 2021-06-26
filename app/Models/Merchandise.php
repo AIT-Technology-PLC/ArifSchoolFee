583db5f3db318269cd2d39f136c943132706f830
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-26 23:03:57 +0300

    refactor merchandise model

--- app/Models/Merchandise.php
+++ app/Models/Merchandise.php
@@ -82,7 +82,10 @@ class Merchandise extends Model
 
     public function getProductAvailableTotalBalance($availableMerchandises, $productId)
     {
-        return number_format($availableMerchandises->where('product_id', $productId)->sum('available'), 2, '.', '');
+        return number_format(
+            $availableMerchandises->where('product_id', $productId)->sum('available'),
+            2, '.', ''
+        );
     }
 
     public function getProductReservedInWarehouse($reservedMerchandises, $productId, $warehouseId)
@@ -95,30 +98,28 @@ class Merchandise extends Model
 
     public function getProductReservedTotalBalance($reservedMerchandises, $productId)
     {
-        return number_format($reservedMerchandises->where('product_id', $productId)->sum('reserved'), 2, '.', '');
+        return number_format(
+            $reservedMerchandises->where('product_id', $productId)->sum('reserved'),
+            2, '.', ''
+        );
     }
 
     public function getProductOnHandInWarehouse($onHandMerchandises, $productId, $warehouseId)
     {
-        $reserved = $onHandMerchandises->where('product_id', $productId)
-            ->where('warehouse_id', $warehouseId)
-            ->first()
-            ->reserved ?? 0.00;
-
-        $onHand = $onHandMerchandises->where('product_id', $productId)
-            ->where('warehouse_id', $warehouseId)
-            ->first()
-            ->available ?? 0.00;
-
-        return number_format($reserved + $onHand, 2, '.', '');
+        return number_format(
+            $onHandMerchandises->where('product_id', $productId)
+                ->where('warehouse_id', $warehouseId)
+                ->first()
+                ->on_hand ?? 0.00,
+            2, '.', ''
+        );
     }
 
     public function getProductOnHandTotalBalance($onHandMerchandises, $productId)
     {
-        $reserved = $onHandMerchandises->where('product_id', $productId)->sum('reserved');
-
-        $onHand = $onHandMerchandises->where('product_id', $productId)->sum('available');
-
-        return number_format($reserved + $onHand, 2, '.', '');
+        return number_format(
+            $onHandMerchandises->where('product_id', $productId)->sum('on_hand'),
+            2, '.', ''
+        );
     }
 }
