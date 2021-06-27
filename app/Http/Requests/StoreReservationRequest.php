f711b748e30be6db36e690e1a34a7910b128968e
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-27 22:18:41 +0300

    implement reservation store request validation

---
+++ app/Http/Requests/StoreReservationRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\PrependCompanyId;
+use App\Services\SetDataOwnerService;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreReservationRequest extends FormRequest
+{
+    use PrependCompanyId;
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'code' => 'required|string|unique:reservations',
+            'reservation' => 'required|array',
+            'reservation.*.product_id' => 'required|integer',
+            'reservation.*.warehouse_id' => 'required|integer',
+            'reservation.*.unit_price' => 'nullable|numeric',
+            'reservation.*.quantity' => 'required|numeric|min:1',
+            'reservation.*.description' => 'nullable|string',
+            'customer_id' => 'nullable|integer',
+            'issued_on' => 'required|date',
+            'payment_type' => 'required|string',
+            'description' => 'nullable|string',
+            'cash_received_in_percentage' => 'required|numeric|between:0,100',
+        ];
+    }
+
+    public function prepareForValidation()
+    {
+        $this->merge([
+            'code' => $this->prependCompanyId($this->code),
+        ]);
+    }
+
+    public function passedValidation()
+    {
+        $this->merge(SetDataOwnerService::forNonTransaction());
+    }
+}
