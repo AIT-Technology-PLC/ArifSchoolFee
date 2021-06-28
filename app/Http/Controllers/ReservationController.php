fd3f71d68dc2df244603f71411db76f7362f7cd4
Nabil Hassen <nabil.hassen08@gmail.com>
2021-06-28 10:17:38 +0300

    build reservation controller index method

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,82 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
+use App\Traits\NotifiableUsers;
+use App\Traits\SubtractInventory;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    use NotifiableUsers, SubtractInventory;
+
+    public function __construct()
     {
-        //
+        $this->middleware('\App\Http\Middleware\AllowOnlyEnabledFeatures:Reservation Management');
+
+        $this->authorizeResource(Reservation::class, 'reservation');
+
+        $this->permission = 'Make Reservation';
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function index()
+    {
+        $reservations = Reservation::companyReservation()
+            ->with(['reservationDetails', 'createdBy', 'updatedBy', 'approvedBy', 'company', 'customer'])
+            ->latest()->get();
+
+        $totalReservations = $reservations->count();
+
+        $totalConverted = $reservations->whereNotNull('converted_by')->count();
+
+        $totalReserved = $reservations->whereNotNull('reserved_By')->whereNull('converted_by')->count();
+
+        $totalCancelled = $reservations->whereNotNull('cancelled_By')->count();
+
+        $totalApproved = $reservations->whereNotNull('approved_By')->whereNull('converted_by', 'reserved_by')->count();
+
+        return view('reservations.index', compact('reservations', 'totalReservations', 'totalConverted', 'totalReserved', 'totalCancelled', 'totalApproved'));
+    }
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(Reservation $reservation)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit(Reservation $reservation)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(Request $request, Reservation $reservation)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy(Reservation $reservation)
     {
         //
     }
