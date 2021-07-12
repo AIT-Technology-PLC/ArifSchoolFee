ae983596ecd270b275f6c7cda71db5f549502cb7
Nabil Hassen <nabil.hassen08@gmail.com>
2021-07-12 17:00:27 +0300

    implement tender checklist types validation via formrequest

---
+++ app/Http/Requests/StoreTenderChecklistTypeRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Services\SetDataOwnerService;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTenderChecklistTypeRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+        ];
+    }
+
+    public function passedValidation()
+    {
+        $this->merge(SetDataOwnerService::forNonTransaction());
+    }
+}
