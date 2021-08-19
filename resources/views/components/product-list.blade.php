77a58bdc944861972a0d35ad04c519706425ebf2
Nabil Hassen <nabil.hassen08@gmail.com>
2021-08-20 02:39:39 +0300

    fix double space occurence in product category for select2 lists

--- resources/views/components/product-list.blade.php
+++ resources/views/components/product-list.blade.php
@@ -2,7 +2,7 @@
     <select id="{{ $name . '[product_id]' }}" name="{{ $name . '[product_id]' }}" class="select2-products" onchange="getProductSelected(this.id, this.value)">
         <option data-code="" data-category=""></option>
         @foreach ($products as $product)
-            <option value="{{ $product->id }}" {{ $selectedProductId == $product->id ? 'selected' : '' }} data-code="{{ $product->code ?? '' }}" data-category="{{ $product->productCategory->name }}">{{ $product->name }}</option>
+            <option value="{{ $product->id }}" {{ $selectedProductId == $product->id ? 'selected' : '' }} data-code="{{ $product->code ?? '' }}" data-category="{{ Str::of($product->productCategory->name)->replace('  ', ' ') }}">{{ $product->name }}</option>
         @endforeach
         @if (!is_numeric($selectedProductId))
             <option value="{{ $selectedProductId }}" selected data-code="{{ $selectedProductId }}" data-category="{{ $selectedProductId }}">{{ $selectedProductId }}</option>
