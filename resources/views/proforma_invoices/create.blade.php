e6ca2b5bab20653f144aca6c4a623fe1277e8a2d
Nabil Hassen <nabil.hassen08@gmail.com>
2021-08-21 23:16:21 +0300

    show price method

--- resources/views/proforma_invoices/create.blade.php
+++ resources/views/proforma_invoices/create.blade.php
@@ -162,7 +162,7 @@
                                         </div>
                                     </div>
                                     <div class="column is-6">
-                                        <label for="proformaInvoice[{{ $loop->index }}][unit_price]" class="label text-green has-text-weight-normal">Unit Price<sup class="has-text-weight-light"> (Before VAT)</sup> <sup class="has-text-danger">*</sup> </label>
+                                        <label for="proformaInvoice[{{ $loop->index }}][unit_price]" class="label text-green has-text-weight-normal">Unit Price<sup class="has-text-weight-light"> ({{ userCompany()->getPriceMethod() }})</sup> <sup class="has-text-danger">*</sup> </label>
                                         <div class="field has-addons">
                                             <div class="control has-icons-left is-expanded">
                                                 <input id="proformaInvoice[{{ $loop->index }}][unit_price]" name="proformaInvoice[{{ $loop->index }}][unit_price]" type="number" class="input" placeholder="Unit Price" value="{{ $proformaInvoiceDetail['unit_price'] ?? '' }}">
