a533b3a68c01bab380747bdc60cd62321d9abd10
Nabil Hassen <nabil.hassen08@gmail.com>
2021-08-24 16:45:51 +0300

    add new columns to warehouse request form classes

--- app/Http/Requests/StoreWarehouseRequest.php
+++ app/Http/Requests/StoreWarehouseRequest.php
@@ -17,6 +17,10 @@ class StoreWarehouseRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'location' => 'required|string|max:255',
+            'is_sales_store' => 'required|boolean',
+            'can_be_sold_from' => 'required|boolean',
+            'email' => 'nullable|string|email',
+            'phone' => 'nullable|string',
             'description' => 'nullable|string',
         ];
     }
