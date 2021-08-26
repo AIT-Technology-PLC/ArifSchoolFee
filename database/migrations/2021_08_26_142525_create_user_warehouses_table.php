e9ee3d135c18aa0c5832bdef515181cfd4a65ae7
Nabil Hassen <nabil.hassen08@gmail.com>
2021-08-26 14:59:07 +0300

    create new table for users permission per warehouse

---
+++ database/migrations/2021_08_26_142525_create_user_warehouses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserWarehousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_warehouses', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->nullable()->unsigned();
+            $table->bigInteger('warehouse_id')->nullable()->unsigned();
+            $table->string('type');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_warehouses');
+    }
+}
