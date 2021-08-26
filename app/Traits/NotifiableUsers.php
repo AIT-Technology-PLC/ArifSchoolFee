882d136a6fa361d306d7c8b4f161bbf012cefb14
Nabil Hassen <nabil.hassen08@gmail.com>
2021-08-26 22:56:56 +0300

    show notifications by branch and only if necessary

--- app/Traits/NotifiableUsers.php
+++ app/Traits/NotifiableUsers.php
@@ -8,6 +8,10 @@ trait NotifiableUsers
 {
     public function notifiableUsers($permission, $creator = null)
     {
+        if (auth()->user()->can($permission)) {
+            return $creator ?? [];
+        }
+
         $users = User::permission($permission)
             ->whereIn('id', function ($query) {
                 $query->select('user_id')
@@ -15,6 +19,10 @@ trait NotifiableUsers
                     ->where('company_id', userCompany()->id)
                     ->where('id', '<>', auth()->user()->employee->id);
             })->get();
+
+        if ($users->contains('warehouse_id', userWarehouse()->id)) {
+            $users = $users->where('warehouse_id', userWarehouse()->id);
+        }
 
         if ($creator) {
             $users->push($creator);
