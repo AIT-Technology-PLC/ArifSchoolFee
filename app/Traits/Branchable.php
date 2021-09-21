5a0a1cb51205b67a8d44085ae07d947f078758a3
Nabil Hassen <nabil.hassen08@gmail.com>
2021-09-21 17:25:56 +0300

    build branchable trait

---
+++ app/Traits/Branchable.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Warehouse;
+
+trait Branchable
+{
+    protected static function bootBranchable()
+    {
+        static::creating(function ($model) {
+            if (auth()->check()) {
+                $model->warehouse_id = auth()->user()->warehouse_id;
+            }
+        });
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+}
