b91f2753822809ee6413d07f8e7a116ea12fcd4a
Nabil Hassen <nabil.hassen08@gmail.com>
2021-09-21 16:21:43 +0300

    implement UserStamp trait

---
+++ app/Traits/HasUserstamps.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Traits;
+
+use App\User;
+
+trait HasUserstamps
+{
+    public function bootHasUserstamps()
+    {
+        static::creating(function ($model) {
+            if (auth()->check()) {
+                $model->created_by = auth()->id();
+            }
+        });
+
+        static::updating(function ($model) {
+            if (auth()->check()) {
+                $model->updated_by = auth()->id();
+            }
+        });
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+}
