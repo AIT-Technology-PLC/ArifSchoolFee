7a2cbade9635a901f7ffec491e8a6d947a587762
Nabil Hassen <nabil.hassen08@gmail.com>
2021-09-24 01:51:57 +0300

    refactor rate accessor for tender

--- resources/views/tenders/show.blade.php
+++ resources/views/tenders/show.blade.php
@@ -133,7 +133,7 @@
                         </div>
                         <div class="column m-lr-20">
                             <div class="is-size- has-text-weight-bold">
-                                {{ $tender->getTenderChecklistsCompletionRate() }}%
+                                {{ $tender->tenderChecklistsCompletionRate }}%
                             </div>
                             <div class="is-uppercase is-size-7">
                                 Checklist Completion
