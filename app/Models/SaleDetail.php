092e554e8af9da620ac55f6b2a2d0328f40850e8
Nabil Hassen <nabil.hassen08@gmail.com>
2021-09-23 23:04:44 +0300

    replace PricingTicket with PricingProduct in saledetail

--- app/Models/SaleDetail.php
+++ app/Models/SaleDetail.php
@@ -3,14 +3,14 @@
 namespace App\Models;
 
 use App\Traits\Discountable;
-use App\Traits\PricingTicket;
+use App\Traits\PricingProduct;
 use App\Traits\TouchParentUserstamp;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SaleDetail extends Model
 {
-    use SoftDeletes, TouchParentUserstamp, PricingTicket, Discountable;
+    use SoftDeletes, TouchParentUserstamp, PricingProduct, Discountable;
 
     protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
 
