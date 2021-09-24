4af6955ba5c1a9fd0f7c7fb798b857a854d5659a
Nabil Hassen <nabil.hassen08@gmail.com>
2021-09-24 18:10:56 +0300

    pass option parameter to format method

--- app/Services/InventoryHistory/TransferDetailHistoryService.php
+++ app/Services/InventoryHistory/TransferDetailHistoryService.php
@@ -13,30 +13,38 @@ class TransferDetailHistoryService implements DetailHistoryServiceInterface
         return (new TransferDetail())->getByWarehouseAndProduct($warehouse, $product);
     }
 
-    public static function format($transferDetails)
+    public static function format($transferDetails, $warehouse = null)
     {
-        return $transferDetails->map(function ($transferDetail) {
-            return [
-                'type' => 'TRANSFER',
-                'code' => $transferDetail->transfer->code,
-                'date' => $transferDetail->transfer->issued_on,
-                'quantity' => $transferDetail->quantity,
-                'balance' => 0.00,
-                'unit_of_measurement' => $transferDetail->product->unit_of_measurement,
+        return $transferDetails
+            ->filter(function ($transferDetail) use ($warehouse) {
+                if ($transferDetail->transfer->transferred_to == $warehouse->id && !$transferDetail->transfer->isAdded()) {
+                    return false;
+                }
 
-                'details' => $transferDetail->transfer->transferred_from == $this->warehouse->id ?
-                Str::of('Transferred')->append(...[' from ', $transferDetail->transfer->transferredFrom->name]) :
-                Str::of('Transferred')->append(...[' to ', $transferDetail->transfer->transferredTo->name]),
+                return true;
+            })
+            ->map(function ($transferDetail) use ($warehouse) {
+                return [
+                    'type' => 'TRANSFER',
+                    'code' => $transferDetail->transfer->code,
+                    'date' => $transferDetail->transfer->issued_on,
+                    'quantity' => $transferDetail->quantity,
+                    'balance' => 0.00,
+                    'unit_of_measurement' => $transferDetail->product->unit_of_measurement,
 
-                'function' => $transferDetail->transfer->transferred_from == $this->warehouse->id ? 'subtract' : 'add',
-            ];
-        });
+                    'details' => $transferDetail->transfer->transferred_from == $warehouse->id ?
+                    Str::of('Transferred')->append(' from ', $transferDetail->transfer->transferredFrom->name) :
+                    Str::of('Transferred')->append(' to ', $transferDetail->transfer->transferredTo->name),
+
+                    'function' => $transferDetail->transfer->transferred_from == $warehouse->id ? 'subtract' : 'add',
+                ];
+            });
     }
 
     public static function formatted($warehouse, $product)
     {
         $transferDetails = self::get($warehouse, $product);
 
-        return self::format($transferDetails);
+        return self::format($transferDetails, $warehouse);
     }
 }
