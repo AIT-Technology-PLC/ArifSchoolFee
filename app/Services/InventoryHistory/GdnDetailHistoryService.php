fe1ffe6f1c4ecc023ba9f96bd348d0e5f4dc6c65
Nabil Hassen <nabil.hassen08@gmail.com>
2021-09-24 17:34:07 +0300

    build gdnDetail history service

---
+++ app/Services/InventoryHistory/GdnDetailHistoryService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services\InventoryHistory;
+
+use App\Interfaces\DetailHistoryServiceInterface;
+use App\Models\GdnDetail;
+use Illuminate\Support\Str;
+
+class GdnDetailHistoryService implements DetailHistoryServiceInterface
+{
+    public static function get($warehouse, $product)
+    {
+        return (new GdnDetail())->getByWarehouseAndProduct($warehouse, $product);
+    }
+
+    public static function format($gdnDetails)
+    {
+        return $gdnDetails->map(function ($gdnDetail) {
+            return [
+                'type' => 'DO',
+                'code' => $gdnDetail->gdn->code,
+                'date' => $gdnDetail->gdn->issued_on,
+                'quantity' => $gdnDetail->quantity,
+                'balance' => 0.00,
+                'unit_of_measurement' => $gdnDetail->product->unit_of_measurement,
+                'details' => Str::of($gdnDetail->gdn->customer->company_name ?? 'Unknown')->prepend('Submitted to '),
+                'function' => 'subtract',
+            ];
+        });
+    }
+
+    public static function formatted($warehouse, $product)
+    {
+        $gdnDetails = self::get($warehouse, $product);
+
+        return self::format($gdnDetails);
+    }
+}
