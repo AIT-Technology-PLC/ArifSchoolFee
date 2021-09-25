fef8b98a290d766478ad0b3e1c55df7235af9bea
Nabil Hassen <nabil.hassen08@gmail.com>
2021-09-25 03:12:28 +0300

    refactor sending tender dealine notif command

--- app/Console/Commands/SendTenderDeadlineNotifications.php
+++ app/Console/Commands/SendTenderDeadlineNotifications.php
@@ -3,11 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Models\Company;
-use App\Models\Employee;
 use App\Models\Tender;
-use App\Notifications\TenderDeadlineIsClose;
 use App\Models\User;
+use App\Notifications\TenderDeadlineIsClose;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Notification;
 
 class SendTenderDeadlineNotifications extends Command
@@ -39,13 +39,11 @@ class SendTenderDeadlineNotifications extends Command
                 continue;
             }
 
-            $notifiableUsersId = User::role(['Tender Officer', 'System Manager'])->get()->pluck('id')->toArray();
-
-            $users = Employee::with('user')
-                ->where('company_id', $company->id)
-                ->whereIn('user_id', $notifiableUsersId)
-                ->get()
-                ->pluck('user');
+            $users = User::role(['Tender Officer', 'System Manager'])
+                ->whereHas('employee', function (Builder $query) use ($company) {
+                    $query->where('company_id', $company->id);
+                })
+                ->get();
 
             if ($users->isEmpty()) {
                 continue;
