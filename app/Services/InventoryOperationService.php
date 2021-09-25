74926851c06f43cb8c471fa4db606fda271eab45
Nabil Hassen <nabil.hassen08@gmail.com>
2021-09-26 01:27:04 +0300

    refactoring inventoryoperationservice

--- app/Services/InventoryOperationService.php
+++ app/Services/InventoryOperationService.php
@@ -3,187 +3,161 @@
 namespace App\Services;
 
 use App\Factory\InventoryTypeFactory;
-use Illuminate\Support\Facades\DB;
 
 class InventoryOperationService
 {
+    private static $unavailableProducts = [];
+
     public static function add($details)
     {
-        DB::transaction(function () use ($details) {
-            foreach ($details as $detail) {
-                $type = InventoryTypeFactory::make($detail->product->type);
+        foreach ($details as $detail) {
+            $type = InventoryTypeFactory::make($detail->product->type);
 
-                $type->add(
-                    $detail->product_id,
-                    $detail->warehouse_id,
-                    $detail->quantity
-                );
-            }
-        });
+            $type->add(
+                $detail->product_id,
+                $detail->warehouse_id,
+                $detail->quantity
+            );
+        }
     }
 
     public static function subtract($details, $from)
     {
-        $result = DB::transaction(function () use ($details, $from) {
-            $unavailableProducts = [];
+        foreach ($details as $detail) {
+            $type = InventoryTypeFactory::make($detail->product->type);
 
-            foreach ($details as $detail) {
-                $type = InventoryTypeFactory::make($detail->product->type);
-
-                if (!$type->isAvailable($detail->product_id, $detail->warehouse_id, $detail->quantity, $from)) {
-                    array_push($unavailableProducts, "{$detail->product->name} is not available or not enough in {$detail->warehouse->name}");
-                }
+            if (!$type->isAvailable($detail->product_id, $detail->warehouse_id, $detail->quantity, $from)) {
+                array_push(self::$unavailableProducts, "{$detail->product->name} is not available or not enough in {$detail->warehouse->name}");
             }
+        }
 
-            if (count($unavailableProducts)) {
-                return [
-                    'isSubtracted' => false,
-                    'unavailableProducts' => $unavailableProducts,
-                ];
-            }
+        if (count(self::$unavailableProducts)) {
+            return [
+                'isSubtracted' => false,
+                'unavailableProducts' => self::$unavailableProducts,
+            ];
+        }
 
-            foreach ($details as $detail) {
-                $type = InventoryTypeFactory::make($detail->product->type);
+        foreach ($details as $detail) {
+            $type = InventoryTypeFactory::make($detail->product->type);
 
-                $type->subtract(
-                    $detail->product_id,
-                    $detail->warehouse_id,
-                    $detail->quantity,
-                    $from
-                );
-            }
+            $type->subtract(
+                $detail->product_id,
+                $detail->warehouse_id,
+                $detail->quantity,
+                $from
+            );
+        }
 
-            return ['isSubtracted' => true];
-        });
-
-        return $result;
+        return ['isSubtracted' => true];
     }
 
     public static function transfer($details, $isSubtracted)
     {
-        $result = DB::transaction(function () use ($details, $isSubtracted) {
-            if (!$isSubtracted) {
-                $unavailableProducts = [];
-
-                foreach ($details as $detail) {
-                    $type = InventoryTypeFactory::make($detail->product->type);
-
-                    if (!$type->isAvailable($detail->product_id, $detail->transfer->transferred_from, $detail->quantity)) {
-                        array_push($unavailableProducts, "{$detail->product->name} is not available or not enough in {$detail->transfer->transferredFrom->name}");
-                    }
-                }
-
-                if (count($unavailableProducts)) {
-                    return [
-                        'isTransferred' => false,
-                        'unavailableProducts' => $unavailableProducts,
-                    ];
-                }
-            }
+        if (!$isSubtracted) {
 
             foreach ($details as $detail) {
                 $type = InventoryTypeFactory::make($detail->product->type);
 
-                $type->transfer(
-                    $detail->product_id,
-                    $detail->transfer->transferred_to,
-                    $detail->transfer->transferred_from,
-                    $detail->quantity,
-                    $isSubtracted
-                );
+                if (!$type->isAvailable($detail->product_id, $detail->transfer->transferred_from, $detail->quantity)) {
+                    array_push(self::$unavailableProducts, "{$detail->product->name} is not available or not enough in {$detail->transfer->transferredFrom->name}");
+                }
             }
 
-            return ['isTransferred' => true];
-        });
+            if (count(self::$unavailableProducts)) {
+                return [
+                    'isTransferred' => false,
+                    'unavailableProducts' => self::$unavailableProducts,
+                ];
+            }
+        }
 
-        return $result;
+        foreach ($details as $detail) {
+            $type = InventoryTypeFactory::make($detail->product->type);
+
+            $type->transfer(
+                $detail->product_id,
+                $detail->transfer->transferred_to,
+                $detail->transfer->transferred_from,
+                $detail->quantity,
+                $isSubtracted
+            );
+        }
+
+        return ['isTransferred' => true];
     }
 
     public static function adjust($details)
     {
-        $result = DB::transaction(function () use ($details) {
-            $unavailableProducts = [];
+        foreach ($details as $detail) {
+            $type = InventoryTypeFactory::make($detail->product->type);
 
-            foreach ($details as $detail) {
-                $type = InventoryTypeFactory::make($detail->product->type);
-
-                if ($detail->is_subtract && !$type->isAvailable($detail->product_id, $detail->warehouse_id, $detail->quantity)) {
-                    array_push($unavailableProducts, "{$detail->product->name} is not available or not enough in {$detail->warehouse->name}");
-                }
+            if ($detail->is_subtract && !$type->isAvailable($detail->product_id, $detail->warehouse_id, $detail->quantity)) {
+                array_push(self::$unavailableProducts, "{$detail->product->name} is not available or not enough in {$detail->warehouse->name}");
             }
+        }
 
-            if (count($unavailableProducts)) {
-                return [
-                    'isAdjusted' => false,
-                    'unavailableProducts' => $unavailableProducts,
-                ];
-            }
+        if (count(self::$unavailableProducts)) {
+            return [
+                'isAdjusted' => false,
+                'unavailableProducts' => self::$unavailableProducts,
+            ];
+        }
 
-            foreach ($details as $detail) {
-                $type = InventoryTypeFactory::make($detail->product->type);
+        foreach ($details as $detail) {
+            $type = InventoryTypeFactory::make($detail->product->type);
 
-                $type->adjust(
-                    $detail->product_id,
-                    $detail->warehouse_id,
-                    $detail->quantity,
-                    $detail->is_subtract
-                );
-            }
+            $type->adjust(
+                $detail->product_id,
+                $detail->warehouse_id,
+                $detail->quantity,
+                $detail->is_subtract
+            );
+        }
 
-            return ['isAdjusted' => true];
-        });
-
-        return $result;
+        return ['isAdjusted' => true];
     }
 
     public static function reserve($details)
     {
-        $result = DB::transaction(function () use ($details) {
-            $unavailableProducts = [];
+        foreach ($details as $detail) {
+            $type = InventoryTypeFactory::make($detail->product->type);
 
-            foreach ($details as $detail) {
-                $type = InventoryTypeFactory::make($detail->product->type);
-
-                if (!$type->isAvailable($detail->product_id, $detail->warehouse_id, $detail->quantity)) {
-                    array_push($unavailableProducts, "{$detail->product->name} is not available or not enough in {$detail->warehouse->name}");
-                }
+            if (!$type->isAvailable($detail->product_id, $detail->warehouse_id, $detail->quantity)) {
+                array_push(self::$unavailableProducts, "{$detail->product->name} is not available or not enough in {$detail->warehouse->name}");
             }
+        }
 
-            if (count($unavailableProducts)) {
-                return [
-                    'isReserved' => false,
-                    'unavailableProducts' => $unavailableProducts,
-                ];
-            }
+        if (count(self::$unavailableProducts)) {
+            return [
+                'isReserved' => false,
+                'unavailableProducts' => self::$unavailableProducts,
+            ];
+        }
 
-            foreach ($details as $detail) {
-                $type = InventoryTypeFactory::make($detail->product->type);
+        foreach ($details as $detail) {
+            $type = InventoryTypeFactory::make($detail->product->type);
 
-                $type->reserve(
-                    $detail->product_id,
-                    $detail->warehouse_id,
-                    $detail->quantity,
-                );
-            }
+            $type->reserve(
+                $detail->product_id,
+                $detail->warehouse_id,
+                $detail->quantity,
+            );
+        }
 
-            return ['isReserved' => true];
-        });
-
-        return $result;
+        return ['isReserved' => true];
     }
 
     public static function cancelReservation($details)
     {
-        DB::transaction(function () use ($details) {
-            foreach ($details as $detail) {
-                $type = InventoryTypeFactory::make($detail->product->type);
+        foreach ($details as $detail) {
+            $type = InventoryTypeFactory::make($detail->product->type);
 
-                $type->cancelReservation(
-                    $detail->product_id,
-                    $detail->warehouse_id,
-                    $detail->quantity,
-                );
-            }
-        });
+            $type->cancelReservation(
+                $detail->product_id,
+                $detail->warehouse_id,
+                $detail->quantity,
+            );
+        }
     }
 }
