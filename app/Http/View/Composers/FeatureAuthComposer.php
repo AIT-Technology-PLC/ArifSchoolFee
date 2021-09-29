6790c2b2ccdc71e46f2aa6f63e3c76f3bfe64e95
Nabil Hassen <nabil.hassen08@gmail.com>
2021-09-29 22:08:23 +0300

    cache enabledFeatures composer

--- app/Http/View/Composers/FeatureAuthComposer.php
+++ app/Http/View/Composers/FeatureAuthComposer.php
@@ -3,43 +3,22 @@
 namespace App\Http\View\Composers;
 
 use App\Models\Feature;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class FeatureAuthComposer
 {
     public function compose(View $view)
     {
-        $excludedViews = collect([
-            'layouts.app',
-            'assets.css',
-            'pwa.tags',
-            'layouts.header',
-            'layouts.create-menu',
-            'layouts.menu',
-            'layouts.footer',
-            'assets.js',
-            'components.delete-button',
-            'components.deleted-message',
-            'components.product-list',
-            'livewire.notifications',
-            'livewire.notification-counter',
-            'warehouses.merchandises.on-hand',
-            'warehouses.merchandises.out-of',
-            'merchandises.on-hand',
-            'merchandises.available',
-            'merchandises.reserved',
-            'merchandises.out-of',
-            'datatables::script',
-        ]);
-
-        if ($excludedViews->contains($view->getName())) {
-            return false;
-        }
-
         if (!auth()->check()) {
-            return false;
+            return;
         }
 
-        $view->with('enabledFeatures', Feature::getAllEnabledFeaturesOfCompany());
+        $enabledFeatures = Cache::store('array')
+            ->rememberForever('enabledFeatures', function () {
+                return Feature::getAllEnabledFeaturesOfCompany();
+            });
+
+        $view->with('enabledFeatures', $enabledFeatures);
     }
 }
