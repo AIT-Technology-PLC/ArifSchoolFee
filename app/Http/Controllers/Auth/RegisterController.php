d48a8886a83dc5263dd4d0a89a09f93ec841fd31
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-05 00:46:18 +0300

    implement CreateUserAction in RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,56 +2,33 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Actions\CreateUserAction;
 use App\Http\Controllers\Controller;
 use App\Models\Company;
 use App\Models\Employee;
 use App\Models\Plan;
 use App\Models\Warehouse;
 use App\Providers\RouteServiceProvider;
-use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-     */
-
     use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    private $action;
+
+    public function __construct(CreateUserAction $action)
     {
+        $this->action = $action;
+
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -62,12 +39,6 @@ class RegisterController extends Controller
         ]);
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
     protected function create(array $data)
     {
         return DB::transaction(function () use ($data) {
@@ -75,20 +46,7 @@ class RegisterController extends Controller
                 'name' => $data['company_name'],
                 'currency' => 'ETB',
                 'enabled' => 0,
-                'plan_id' => Plan::where('name', 'premium')->first()->id,
-            ]);
-
-            $user = User::create([
-                'name' => $data['name'],
-                'email' => $data['email'],
-                'password' => Hash::make($data['password']),
-            ]);
-
-            Employee::create([
-                'user_id' => $user->id,
-                'company_id' => $company->id,
-                'enabled' => 1,
-                'position' => 'Onrica Support Department',
+                'plan_id' => Plan::firstWhere('name', 'premium')->id,
             ]);
 
             $warehouse = Warehouse::create([
@@ -96,13 +54,25 @@ class RegisterController extends Controller
                 'name' => 'Main Warehouse',
                 'location' => 'Unknown',
                 'is_sales_store' => 0,
-                'created_by' => $user->id,
-                'updated_by' => $user->id,
             ]);
 
-            $user->assignRole("System Manager");
+            $request = new Request([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => $data['password'],
+                'warehouse_id' => $warehouse->id,
+                'enabled' => 1,
+                'position' => 'Onrica Support Department',
+                'role' => 'System Manager',
+            ]);
 
-            $user->warehouse()->associate($warehouse)->save();
+            $user = $this->action->execute($request);
+
+            $user->employee->company()->associate($company)->save();
+
+            $warehouse->createdBy()->associate($user)->save();
+
+            $warehouse->updatedBy()->associate($user)->save();
 
             return $user;
         });
