5407498efcc4385930c73d685cc11ef64699d965
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-05 01:02:16 +0300

    change login and signup page titles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,60 @@
 @extends('auth.layout.auth')
 
 @section('title')
-Login - SmartWork by Onrica
+    Login - Onrica SmartWork
 @endsection
 
 @section('content')
-<section class="hero is-fullheight bg-green limit-to-100vh">
-    <div class="hero-body">
-        <div class="container">
-            <div class="columns is-marginless is-centered">
-                <div class="column is-5">
-                    <div class="has-text-centered">
-                        <img src="{{ asset('img/logo.png') }}" width="200">
-                    </div>
-                    <div class="box" style="background: #509270;">
-                        <form id="formOne" method="POST" action="{{ route('post.login') }}" novalidate autocomplete="off">
-                            @csrf
-                            <div class="field">
-                                <label for="email" class="label has-text-white is-uppercase is-size-7">E-Mail Address</label>
-                                <div class="control">
-                                    <input id="email" type="email" class="input bg-lightgreen @error('email') is-danger @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
-                                    @error('email')
-                                    <span class="help has-text-white">
-                                        {{ $message }}
-                                    </span>
-                                    @enderror
+    <section class="hero is-fullheight bg-green limit-to-100vh">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-marginless is-centered">
+                    <div class="column is-5">
+                        <div class="has-text-centered">
+                            <img src="{{ asset('img/logo.png') }}" width="200">
+                        </div>
+                        <div class="box" style="background: #509270;">
+                            <form id="formOne" method="POST" action="{{ route('post.login') }}" novalidate autocomplete="off">
+                                @csrf
+                                <div class="field">
+                                    <label for="email" class="label has-text-white is-uppercase is-size-7">E-Mail Address</label>
+                                    <div class="control">
+                                        <input id="email" type="email" class="input bg-lightgreen @error('email') is-danger @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+                                        @error('email')
+                                            <span class="help has-text-white">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="field mt-5">
-                                <label for="password" class="label has-text-white is-uppercase is-size-7">Password</label>
-                                <div class="control">
-                                    <input id="password" type="password" class="input bg-lightgreen @error('password') is-danger @enderror" name="password" autocomplete="current-password">
-                                    @error('password')
-                                    <span class="help has-text-white">
-                                        {{ $message }}
-                                    </span>
-                                    @enderror
+                                <div class="field mt-5">
+                                    <label for="password" class="label has-text-white is-uppercase is-size-7">Password</label>
+                                    <div class="control">
+                                        <input id="password" type="password" class="input bg-lightgreen @error('password') is-danger @enderror" name="password" autocomplete="current-password">
+                                        @error('password')
+                                            <span class="help has-text-white">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="field mt-6">
-                                <div class="control">
-                                    <button id="saveButton" type="submit" class="button has-text-white bg-blue is-fullwidth is-uppercase is-size-6 has-text-weight-semibold py-5 px-5">
-                                        <span class="icon">
-                                            <i class="fas fa-sign-in-alt"></i>
-                                        </span>
-                                        <span>
-                                            Login 
-                                        </span>
-                                    </button>
+                                <div class="field mt-6">
+                                    <div class="control">
+                                        <button id="saveButton" type="submit" class="button has-text-white bg-blue is-fullwidth is-uppercase is-size-6 has-text-weight-semibold py-5 px-5">
+                                            <span class="icon">
+                                                <i class="fas fa-sign-in-alt"></i>
+                                            </span>
+                                            <span>
+                                                Login
+                                            </span>
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,83 +1,83 @@
 @extends('auth.layout.auth')
 
 @section('title')
-Register - Onrica Inventory
+    Sign up - Onrica SmartWork
 @endsection
 
 @section('content')
-<section class="hero is-fullheight bg-green limit-to-100vh">
-    <div class="hero-head">
-        <div class="container">
-            <div class="columns is-marginless is-centered">
-                <div class="column is-5">
-                    <div class="has-text-centered">
-                        <img src="{{ asset('img/logo.png') }}" width="200">
-                    </div>
-                    <div class="box" style="background: #509270;">
-                        <form method="POST" action="{{ route('register') }}" novalidate>
-                            @csrf
-                            <div class="field">
-                                <label for="company_name" class="label has-text-white is-uppercase is-size-7"> Company Name </label>
-                                <div class="control">
-                                    <input id="company_name" type="text" class="input bg-lightgreen @error('company_name') is-danger @enderror" name="company_name" value="{{ old('company_name') }}" autocomplete="company_name" autofocus>
-                                    @error('company_name')
-                                    <span class="help has-text-white" role="alert">
-                                        {{ $message }}
-                                    </span>
-                                    @enderror
+    <section class="hero is-fullheight bg-green limit-to-100vh">
+        <div class="hero-head">
+            <div class="container">
+                <div class="columns is-marginless is-centered">
+                    <div class="column is-5">
+                        <div class="has-text-centered">
+                            <img src="{{ asset('img/logo.png') }}" width="200">
+                        </div>
+                        <div class="box" style="background: #509270;">
+                            <form method="POST" action="{{ route('register') }}" novalidate>
+                                @csrf
+                                <div class="field">
+                                    <label for="company_name" class="label has-text-white is-uppercase is-size-7"> Company Name </label>
+                                    <div class="control">
+                                        <input id="company_name" type="text" class="input bg-lightgreen @error('company_name') is-danger @enderror" name="company_name" value="{{ old('company_name') }}" autocomplete="company_name" autofocus>
+                                        @error('company_name')
+                                            <span class="help has-text-white" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="field mt-5">
-                                <label for="name" class="label has-text-white is-uppercase is-size-7"> Your Name </label>
-                                <div class="control">
-                                    <input id="name" type="text" class="input bg-lightgreen @error('name') is-danger @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
-                                    @error('name')
-                                    <span class="help has-text-white" role="alert">
-                                        {{ $message }}
-                                    </span>
-                                    @enderror
+                                <div class="field mt-5">
+                                    <label for="name" class="label has-text-white is-uppercase is-size-7"> Your Name </label>
+                                    <div class="control">
+                                        <input id="name" type="text" class="input bg-lightgreen @error('name') is-danger @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                                        @error('name')
+                                            <span class="help has-text-white" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="field mt-5">
-                                <label for="email" class="label has-text-white is-uppercase is-size-7"> Email Address </label>
-                                <div class="control">
-                                    <input id="email" type="email" class="input bg-lightgreen @error('email') is-danger @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
-                                    @error('email')
-                                    <span class="help has-text-white" role="alert">
-                                        {{ $message }}
-                                    </span>
-                                    @enderror
+                                <div class="field mt-5">
+                                    <label for="email" class="label has-text-white is-uppercase is-size-7"> Email Address </label>
+                                    <div class="control">
+                                        <input id="email" type="email" class="input bg-lightgreen @error('email') is-danger @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
+                                        @error('email')
+                                            <span class="help has-text-white" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="field mt-5">
-                                <label for="password" class="label has-text-white is-uppercase is-size-7"> Password </label>
-                                <div class="control">
-                                    <input id="password" type="password" class="input bg-lightgreen @error('password') is-danger @enderror" name="password" autocomplete="new-password">
-                                    @error('password')
-                                    <span class="help has-text-white" role="alert">
-                                        {{ $message }}
-                                    </span>
-                                    @enderror
+                                <div class="field mt-5">
+                                    <label for="password" class="label has-text-white is-uppercase is-size-7"> Password </label>
+                                    <div class="control">
+                                        <input id="password" type="password" class="input bg-lightgreen @error('password') is-danger @enderror" name="password" autocomplete="new-password">
+                                        @error('password')
+                                            <span class="help has-text-white" role="alert">
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="field mt-5">
-                                <label for="password-confirm" class="label has-text-white is-uppercase is-size-7"> Confirm Password </label>
-                                <div class="control">
-                                    <input id="password-confirm" type="password" class="input bg-lightgreen" name="password_confirmation" autocomplete="new-password">
+                                <div class="field mt-5">
+                                    <label for="password-confirm" class="label has-text-white is-uppercase is-size-7"> Confirm Password </label>
+                                    <div class="control">
+                                        <input id="password-confirm" type="password" class="input bg-lightgreen" name="password_confirmation" autocomplete="new-password">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="field mt-6 has-text-centered">
-                                <div class="control">
-                                    <button type="submit" class="button has-text-white bg-blue is-fullwidth is-uppercase is-size-7 has-text-weight-semibold py-5 px-5">
-                                        Register
-                                    </button>
+                                <div class="field mt-6 has-text-centered">
+                                    <div class="control">
+                                        <button type="submit" class="button has-text-white bg-blue is-fullwidth is-uppercase is-size-7 has-text-weight-semibold py-5 px-5">
+                                            Register
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
