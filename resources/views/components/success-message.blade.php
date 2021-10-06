7ae75925a456b948b59a58444e70071d10e964ec
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-07 02:17:44 +0300

    change mb from 6 to 5 in success-message

--- resources/views/components/success-message.blade.php
+++ resources/views/components/success-message.blade.php
@@ -1,7 +1,7 @@
 @props(['message'])
 
 @if ($message)
-    <div {{ $attributes->merge(['class' => 'box is-shadowless bg-lightgreen has-text-left mb-6']) }}>
+    <div {{ $attributes->merge(['class' => 'box is-shadowless bg-lightgreen has-text-left mb-5']) }}>
         <p class="has-text-grey text-green is-size-6">
             <span class="icon">
                 <i class="fas fa-check-circle"></i>
