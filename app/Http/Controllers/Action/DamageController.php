3557f46d2c8b904b056485fbdcbf8b33e853c3e3
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-06 19:35:25 +0300

    implement approve transaction action in damagecontroller

--- app/Http/Controllers/Action/DamageController.php
+++ app/Http/Controllers/Action/DamageController.php
@@ -2,18 +2,31 @@
 
 namespace App\Http\Controllers\Action;
 
+use App\Actions\ApproveTransactionAction;
 use App\Http\Controllers\Controller;
-use App\Traits\ApproveInventory;
+use App\Models\Damage;
+use App\Notifications\DamageApproved;
 use App\Traits\SubtractInventory;
 
 class DamageController extends Controller
 {
-    use SubtractInventory, ApproveInventory;
+    use SubtractInventory;
 
     public function __construct()
     {
         $this->middleware('isFeatureAccessible:Damage Management');
+    }
 
-        $this->permission = 'Subtract Damage';
+    public function approve(Damage $damage, ApproveTransactionAction $action)
+    {
+        $this->authorize('approve', $damage);
+
+        [$isExecuted, $message] = $action->execute($damage, DamageApproved::class, 'Subtract Damage');
+
+        if (!$isExecuted) {
+            return redirect()->back()->with('failedMessage', $message);
+        }
+
+        return redirect()->back()->with('successMessage', $message);
     }
 }
