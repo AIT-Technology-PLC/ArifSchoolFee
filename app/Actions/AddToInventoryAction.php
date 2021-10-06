6d4ec4092cda0d6ad885ac6e36d6c9708e01f147
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-06 21:24:57 +0300

    build AddToInventoryAction

---
+++ app/Actions/AddToInventoryAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Actions;
+
+use App\Services\InventoryOperationService;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Notification;
+
+class AddToInventoryAction
+{
+    private const ADD_FAILED_MESSAGE = 'This transaction is already added to inventory';
+
+    private const APPROVE_FAILED_MESSAGE = 'This transaction is already approved';
+
+    private const SUCCESS_MESSAGE = 'Products have been added to inventory successfully';
+
+    public function execute($model, $notification, $permission)
+    {
+        if ($model->isApproved()) {
+            return [false, static::APPROVE_FAILED_MESSAGE];
+        }
+
+        if ($model->isAdded()) {
+            return [false, static::ADD_FAILED_MESSAGE];
+        }
+
+        DB::transaction(function () use ($model, $notification, $permission) {
+            InventoryOperationService::add($model->details());
+
+            $model->add();
+
+            Notification::send(
+                notifiables($permission, $model->createdBy),
+                new $notification($model)
+            );
+        });
+
+        return [true, static::SUCCESS_MESSAGE];
+    }
+}
