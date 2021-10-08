b1c9489182fcd7c51798ba1ab52c5a9376b7fbcc
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-08 16:48:54 +0300

    build validation rule to ensure that resource belongs to company

---
+++ app/Rules/MustBelongToCompany.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class MustBelongToCompany implements Rule
+{
+    private $tableName;
+
+    public function __construct($tableName)
+    {
+        $this->tableName = $tableName;
+    }
+
+    public function passes($attribute, $value)
+    {
+        return DB::table($this->tableName)
+            ->where('company_id', userCompany()->id)
+            ->where('id', $value)
+            ->exists();
+    }
+
+    public function message()
+    {
+        return 'The ' . Str::singular($this->tableName) . ' selected does not belong to your company.';
+    }
+}
