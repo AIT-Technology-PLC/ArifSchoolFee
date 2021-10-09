1403c72b9faff0555062449f68d88edafd9f6c16
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-09 20:21:55 +0300

    implement fail-message componenet in gdn show

--- resources/views/gdns/show.blade.php
+++ resources/views/gdns/show.blade.php
@@ -323,16 +323,7 @@
                         </form>
                     </div>
                 @else
-                    <div class="box is-shadowless bg-lightpurple has-text-left mb-6">
-                        <p class="has-text-grey text-purple is-size-6">
-                            <span class="icon">
-                                <i class="fas fa-exclamation-circle"></i>
-                            </span>
-                            <span>
-                                Product(s) listed below are still not subtracted from your inventory.
-                            </span>
-                        </p>
-                    </div>
+                    <x-fail-message message="Product(s) listed below are still not subtracted from your inventory." />
                 @endcan
             @endif
             @if (!$gdn->isApproved())
@@ -356,16 +347,7 @@
                         </form>
                     </div>
                 @else
-                    <div class="box is-shadowless bg-lightpurple has-text-left mb-6">
-                        <p class="has-text-grey text-purple is-size-6">
-                            <span class="icon">
-                                <i class="fas fa-exclamation-circle"></i>
-                            </span>
-                            <span>
-                                This Delivery Order has not been approved.
-                            </span>
-                        </p>
-                    </div>
+                    <x-fail-message message="This Delivery Order has not been approved." />
                 @endcan
             @endif
             <div class="table-container">
