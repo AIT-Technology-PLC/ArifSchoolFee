d4fd0b9c4d839ea48e067667a4dc96eaddc5159f
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-09 20:56:01 +0300

    implement fail-message componenet in siv show

--- resources/views/sivs/show.blade.php
+++ resources/views/sivs/show.blade.php
@@ -219,16 +219,7 @@
                         </form>
                     </div>
                 @else
-                    <div class="box is-shadowless bg-lightpurple has-text-left mb-6">
-                        <p class="has-text-grey text-purple is-size-6">
-                            <span class="icon">
-                                <i class="fas fa-exclamation-circle"></i>
-                            </span>
-                            <span>
-                                This SIV is not approved.
-                            </span>
-                        </p>
-                    </div>
+                    <x-fail-message message="This SIV is not approved." />
                 @endcan
             @endif
             <div class="table-container">
