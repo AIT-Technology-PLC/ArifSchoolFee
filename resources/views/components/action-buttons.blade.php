f3a63e0207e6b0b66efaebdd276b90aa3a9c569e
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-09 22:29:37 +0300

    import some scripts to run sweetalerts

--- resources/views/components/action-buttons.blade.php
+++ resources/views/components/action-buttons.blade.php
@@ -9,3 +9,8 @@
     </span>
 </a>
 <x-delete-button :model="$model" :id="$id" />
+
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" defer></script>
+<script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/caller.js') }}" defer></script>
