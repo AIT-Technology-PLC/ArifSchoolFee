32b99312d9ab7d0ea81cc8afa19aadbe92aff499
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-17 00:27:41 +0300

    implement content component in adjustments index

--- resources/views/adjustments/index.blade.php
+++ resources/views/adjustments/index.blade.php
@@ -77,108 +77,109 @@
             </div>
         </div>
     </div>
-    <section class="mt-3 mx-3 m-lr-0">
-        <div class="box radius-bottom-0 mb-0 has-background-white-bis">
-            <h1 class="title text-green has-text-weight-medium is-size-5">
-                Adjustment Management
-            </h1>
-        </div>
-        <div class="box radius-top-0">
+
+    <x-content-wrapper>
+
+        <x-content.header>
+            Adjustment Management
+        </x-content.header>
+
+        <x-content.footer>
             <x-success-message :message="session('deleted')" />
-            <div>
-                <table class="regular-datatable is-hoverable is-size-7 display nowrap" data-date="[4]" data-numeric="[]">
-                    <thead>
-                        <tr>
-                            <th><abbr> # </abbr></th>
-                            <th class="has-text-centered"><abbr> Adjustment No </abbr></th>
-                            <th><abbr> Status </abbr></th>
-                            <th><abbr> Description </abbr></th>
-                            <th class="has-text-right"><abbr> Issued On </abbr></th>
-                            <th><abbr> Prepared By </abbr></th>
-                            <th><abbr> Approved By </abbr></th>
-                            <th><abbr> Adjusted By </abbr></th>
-                            <th><abbr> Edited By </abbr></th>
-                            <th><abbr> Actions </abbr></th>
-                        </tr>
-                    </thead>
-                    <tbody class="list">
-                        @foreach ($adjustments as $adjustment)
-                            <tr class="showRowDetails is-clickable" data-id="{{ route('adjustments.show', $adjustment->id) }}">
-                                <td> {{ $loop->index + 1 }} </td>
-                                <td class="is-capitalized has-text-centered">
-                                    {{ $adjustment->code }}
-                                </td>
-                                <td class="is-capitalized">
-                                    @if (!$adjustment->isApproved())
-                                        <span class="tag is-small bg-purple has-text-white">
-                                            <span class="icon">
-                                                <i class="fas fa-clock"></i>
-                                            </span>
-                                            <span>
-                                                Waiting Approval
-                                            </span>
+
+            <table class="regular-datatable is-hoverable is-size-7 display nowrap" data-date="[4]" data-numeric="[]">
+                <thead>
+                    <tr>
+                        <th><abbr> # </abbr></th>
+                        <th class="has-text-centered"><abbr> Adjustment No </abbr></th>
+                        <th><abbr> Status </abbr></th>
+                        <th><abbr> Description </abbr></th>
+                        <th class="has-text-right"><abbr> Issued On </abbr></th>
+                        <th><abbr> Prepared By </abbr></th>
+                        <th><abbr> Approved By </abbr></th>
+                        <th><abbr> Adjusted By </abbr></th>
+                        <th><abbr> Edited By </abbr></th>
+                        <th><abbr> Actions </abbr></th>
+                    </tr>
+                </thead>
+                <tbody class="list">
+                    @foreach ($adjustments as $adjustment)
+                        <tr class="showRowDetails is-clickable" data-id="{{ route('adjustments.show', $adjustment->id) }}">
+                            <td> {{ $loop->index + 1 }} </td>
+                            <td class="is-capitalized has-text-centered">
+                                {{ $adjustment->code }}
+                            </td>
+                            <td class="is-capitalized">
+                                @if (!$adjustment->isApproved())
+                                    <span class="tag is-small bg-purple has-text-white">
+                                        <span class="icon">
+                                            <i class="fas fa-clock"></i>
                                         </span>
-                                    @elseif ($adjustment->isAdjusted())
-                                        <span class="tag is-small bg-green has-text-white">
-                                            <span class="icon">
-                                                <i class="fas fa-check-circle"></i>
-                                            </span>
-                                            <span>
-                                                Adjusted
-                                            </span>
+                                        <span>
+                                            Waiting Approval
                                         </span>
-                                    @else
-                                        <span class="tag is-small bg-gold has-text-white">
-                                            <span class="icon">
-                                                <i class="fas fa-exclamation-circle"></i>
-                                            </span>
-                                            <span>
-                                                Approved (Not Adjusted)
-                                            </span>
-                                        </span>
-                                    @endif
-                                </td>
-                                <td>
-                                    {!! nl2br(e(substr($adjustment->description, 0, 40))) ?? 'N/A' !!}
-                                    <span class="is-hidden">
-                                        {!! $adjustment->description ?? '' !!}
                                     </span>
-                                </td>
-                                <td class="has-text-right">
-                                    {{ $adjustment->issued_on->toFormattedDateString() }}
-                                </td>
-                                <td> {{ $adjustment->createdBy->name ?? 'N/A' }} </td>
-                                <td> {{ $adjustment->approvedBy->name ?? 'N/A' }} </td>
-                                <td> {{ $adjustment->adjustedBy->name ?? 'N/A' }} </td>
-                                <td> {{ $adjustment->updatedBy->name ?? 'N/A' }} </td>
-                                <td class="actions">
-                                    <a href="{{ route('adjustments.show', $adjustment->id) }}" data-title="View Details">
-                                        <span class="tag is-white btn-purple is-outlined is-small text-green has-text-weight-medium">
-                                            <span class="icon">
-                                                <i class="fas fa-info-circle"></i>
-                                            </span>
-                                            <span>
-                                                Details
-                                            </span>
+                                @elseif ($adjustment->isAdjusted())
+                                    <span class="tag is-small bg-green has-text-white">
+                                        <span class="icon">
+                                            <i class="fas fa-check-circle"></i>
                                         </span>
-                                    </a>
-                                    <a href="{{ route('adjustments.edit', $adjustment->id) }}" data-title="Modify Adjustments Data">
-                                        <span class="tag is-white btn-green is-outlined is-small text-green has-text-weight-medium">
-                                            <span class="icon">
-                                                <i class="fas fa-pen-square"></i>
-                                            </span>
-                                            <span>
-                                                Edit
-                                            </span>
+                                        <span>
+                                            Adjusted
                                         </span>
-                                    </a>
-                                    <x-delete-button model="adjustments" :id="$adjustment->id" />
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </section>
+                                    </span>
+                                @else
+                                    <span class="tag is-small bg-gold has-text-white">
+                                        <span class="icon">
+                                            <i class="fas fa-exclamation-circle"></i>
+                                        </span>
+                                        <span>
+                                            Approved (Not Adjusted)
+                                        </span>
+                                    </span>
+                                @endif
+                            </td>
+                            <td>
+                                {!! nl2br(e(substr($adjustment->description, 0, 40))) ?? 'N/A' !!}
+                                <span class="is-hidden">
+                                    {!! $adjustment->description ?? '' !!}
+                                </span>
+                            </td>
+                            <td class="has-text-right">
+                                {{ $adjustment->issued_on->toFormattedDateString() }}
+                            </td>
+                            <td> {{ $adjustment->createdBy->name ?? 'N/A' }} </td>
+                            <td> {{ $adjustment->approvedBy->name ?? 'N/A' }} </td>
+                            <td> {{ $adjustment->adjustedBy->name ?? 'N/A' }} </td>
+                            <td> {{ $adjustment->updatedBy->name ?? 'N/A' }} </td>
+                            <td class="actions">
+                                <a href="{{ route('adjustments.show', $adjustment->id) }}" data-title="View Details">
+                                    <span class="tag is-white btn-purple is-outlined is-small text-green has-text-weight-medium">
+                                        <span class="icon">
+                                            <i class="fas fa-info-circle"></i>
+                                        </span>
+                                        <span>
+                                            Details
+                                        </span>
+                                    </span>
+                                </a>
+                                <a href="{{ route('adjustments.edit', $adjustment->id) }}" data-title="Modify Adjustments Data">
+                                    <span class="tag is-white btn-green is-outlined is-small text-green has-text-weight-medium">
+                                        <span class="icon">
+                                            <i class="fas fa-pen-square"></i>
+                                        </span>
+                                        <span>
+                                            Edit
+                                        </span>
+                                    </span>
+                                </a>
+                                <x-delete-button model="adjustments" :id="$adjustment->id" />
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </x-content.footer>
+
+    </x-content-wrapper>
 @endsection
