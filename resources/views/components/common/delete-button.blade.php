dbacb6b7a1de8766581dee3a0478ab4d415882c2
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-18 00:36:51 +0300

    refactor delete-button

--- resources/views/components/common/delete-button.blade.php
+++ resources/views/components/common/delete-button.blade.php
@@ -3,12 +3,8 @@
 <form class="is-inline delete-form" action="{{ route($model . '.destroy', $id) }}" method="post">
     @csrf
     @method('DELETE')
-    <button class="tag bg-brown has-text-white is-small has-text-weight-medium is-pointer" data-title="Delete Permanently" style="border: none">
-        <span class="icon">
-            <i class="fas fa-trash"></i>
-        </span>
-        <span>
-            Delete
-        </span>
+    <button class="tag is-black has-text-white has-text-weight-medium is-pointer is-borderless" data-title="Delete Permanently">
+        <x-common.icon name="fas fa-trash" />
+        <span>Delete</span>
     </button>
 </form>
