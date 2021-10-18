99c50d68dc706829fdc7f96b47f7127468124f15
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-18 18:27:13 +0300

    build edit button for tables action column

---
+++ resources/views/components/common/edit-button.blade.php
@@ -0,0 +1,10 @@
+@props(['route', 'id', 'model'])
+
+<a href="{{ route($route, $id) }}" data-title="Modify {{ $model }} Data">
+    <span class="tag is-white btn-green is-outlined has-text-weight-medium">
+        <x-common.icon name="fas fa-pen-square" />
+        <span>
+            Edit
+        </span>
+    </span>
+</a>
