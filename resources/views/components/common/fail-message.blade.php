4343db89fe95b38215c7c5808c5e1c0f7c4936a8
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-22 18:17:41 +0300

    implement button component

--- resources/views/components/common/fail-message.blade.php
+++ resources/views/components/common/fail-message.blade.php
@@ -3,24 +3,16 @@
 @if (isset($message) && is_string($message))
     <div class="box is-shadowless bg-lightpurple text-purple mb-{{ $marginBottom }}">
         <p>
-            <span class="icon">
-                <i class="fas fa-exclamation-circle"></i>
-            </span>
-            <span>
-                {{ $message }}
-            </span>
+            <x-common.icon name="fas fa-exclamation-circle" />
+            <span> {{ $message }} </span>
         </p>
         {{ $slot ?? '' }}
     </div>
 @elseif (isset($message) && is_countable($message))
     <div class="box is-shadowless bg-lightpurple text-purple mb-{{ $marginBottom }}">
         @foreach ($message as $item)
-            <span class="icon">
-                <i class="fas fa-times-circle"></i>
-            </span>
-            <span>
-                {{ $item }}
-            </span>
+            <x-common.icon name="fas fa-times-circle" />
+            <span> {{ $item }} </span>
             <br>
         @endforeach
     </div>
--- resources/views/components/common/success-message.blade.php
+++ resources/views/components/common/success-message.blade.php
@@ -3,12 +3,8 @@
 @if ($message)
     <div {{ $attributes->merge(['class' => 'box is-shadowless bg-lightgreen has-text-left mb-5']) }}>
         <p class="has-text-grey text-green is-size-6">
-            <span class="icon">
-                <i class="fas fa-check-circle"></i>
-            </span>
-            <span>
-                {{ $message }}
-            </span>
+            <x-common.icon name="fas fa-check-circle" />
+            <span> {{ $message }} </span>
         </p>
     </div>
 @endif
