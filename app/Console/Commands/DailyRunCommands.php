8257155952f8924272fa4d696d57d1d510e3d1d1
Nabil Hassen <nabil.hassen08@gmail.com>
2021-10-28 19:57:49 +0300

    build a command that runs other daily commands

---
+++ app/Console/Commands/DailyRunCommands.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class DailyRunCommands extends Command
+{
+    protected $signature = 'routine:daily';
+
+    protected $description = 'Commands that run once per day';
+
+    private $commands = [
+        'cancel:expired-proforma-invoices',
+        'cancel:expired-reservations',
+        'notifications:delete-week',
+        'tender:bid-bond-validity-notification',
+        'inventory:low-level-notification',
+        'proforma-invoice:expiry-date-notification',
+        'tender:daily-deadline-notification',
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        foreach ($this->commands as $command) {
+            Artisan::call($command);
+        }
+
+        return 0;
+    }
+}
