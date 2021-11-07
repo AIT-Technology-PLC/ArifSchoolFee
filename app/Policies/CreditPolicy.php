565e2e81e112e2ce1b37c39bd2839d7b0b4dc85b
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-07 22:19:44 +0300

    implement auth polilcy for credit model

---
+++ app/Policies/CreditPolicy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Credit;
+use App\Models\User;
+use App\Traits\VerifyModelIssuer;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CreditPolicy
+{
+    use HandlesAuthorization, VerifyModelIssuer;
+
+    public function viewAny(User $user)
+    {
+        return $user->can('Read Credit');
+    }
+
+    public function view(User $user, Credit $credit)
+    {
+        return $this->isIssuedByMyCompany($user, $credit) && $user->can('Read Credit');
+    }
+
+    public function create(User $user)
+    {
+        return $user->can('Create Credit');
+    }
+
+    public function update(User $user, Credit $credit)
+    {
+        return $this->isIssuedByMyCompany($user, $credit, true) && $user->can('Update Credit');
+    }
+
+    public function delete(User $user, Credit $credit)
+    {
+        return $this->isIssuedByMyCompany($user, $credit, true) && $user->can('Update Credit');
+    }
+}
