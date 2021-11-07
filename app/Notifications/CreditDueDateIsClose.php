88f19678fd2275f44fd246f7c9200d0df4abaf48
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-07 22:36:36 +0300

    create credit due date notification

---
+++ app/Notifications/CreditDueDateIsClose.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Support\Str;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class CreditDueDateIsClose extends Notification
+{
+    use Queueable;
+
+    private $totalCredits;
+
+    public function __construct($totalCredits)
+    {
+        $this->totalCredits = $totalCredits;
+    }
+
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    public function toArray($notifiable)
+    {
+        $message = Str::of($this->totalCredits)
+            ->append(
+                Str::plural('credit', $this->totalCredits),
+                ' will be due',
+                ' after 5 days or less'
+            );
+
+        return [
+            'icon' => 'fas fa-money-check',
+            'message' => $message,
+            'endpoint' => '/credits/due',
+        ];
+    }
+}
