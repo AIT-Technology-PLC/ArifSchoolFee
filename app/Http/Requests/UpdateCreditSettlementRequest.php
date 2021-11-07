c6a16a20f89976c029016888ab7e76a74cf1c1fd
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-07 23:00:15 +0300

    write update credit settlement formrequest

---
+++ app/Http/Requests/UpdateCreditSettlementRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCreditSettlementRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+    
+    public function rules()
+    {
+        return [
+            'amount' => ['nullable', 'numeric'],
+            'method' => ['required', 'string'],
+            'reference_number' => ['nullable', 'string'],
+            'settled_at' => ['required', 'date'],
+            'description' => ['nullable', 'string'],
+        ];
+    }
+}
