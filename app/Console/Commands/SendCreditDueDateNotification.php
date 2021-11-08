ee9db596b9eb1ea689ef66056e6458c4080597b9
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-08 21:25:52 +0300

    send credit due notification to only unsettled credits

--- app/Console/Commands/SendCreditDueDateNotification.php
+++ app/Console/Commands/SendCreditDueDateNotification.php
@@ -32,6 +32,7 @@ class SendCreditDueDateNotification extends Command
         foreach ($companies as $company) {
             $totalCredits = Credit::query()
                 ->where('company_id', $company->id)
+                ->whereColumn('credit_amount', '>', 'credit_amount_settled')
                 ->whereRaw('DATEDIFF(due_date, CURRENT_DATE) BETWEEN 1 AND 5')
                 ->count();
 
