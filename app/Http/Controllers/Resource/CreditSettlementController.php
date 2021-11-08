3dc1a82c3bed29888c8223fa0e92fe3200e67862
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-08 15:44:33 +0300

    build credit settlement controller add credit settlement model events

---
+++ app/Http/Controllers/Resource/CreditSettlementController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Resource;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCreditSettlementRequest;
+use App\Http\Requests\UpdateCreditSettlementRequest;
+use App\Models\Credit;
+use App\Models\CreditSettlement;
+
+class CreditSettlementController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('isFeatureAccessible:Credit Management');
+    }
+
+    public function create(Credit $credit)
+    {
+        $this->authorize('create', $credit);
+
+        if ($credit->isSettled()) {
+            return back()->with('This credit is fully settled.');
+        }
+
+        return view('credit_settlements.create', compact('credit'));
+    }
+
+    public function store(Credit $credit, StoreCreditSettlementRequest $request)
+    {
+        $this->authorize('create', $credit);
+
+        if ($credit->isSettled()) {
+            return back()->with('This credit is fully settled.');
+        }
+
+        $credit->create($request->validated());
+
+        return redirect()->route('credits.index', $credit);
+    }
+
+    public function edit(CreditSettlement $creditSettlement)
+    {
+        $this->authorize('update', $creditSettlement->credit);
+
+        if ($creditSettlement->credit->isSettled()) {
+            return back()->with('This credit is fully settled.');
+        }
+
+        return view('credit_settlements.edit', compact('creditSettlement'));
+    }
+
+    public function update(CreditSettlement $creditSettlement, UpdateCreditSettlementRequest $request)
+    {
+        $this->authorize('update', $creditSettlement->credit);
+
+        if ($creditSettlement->credit->isSettled()) {
+            return back()->with('This credit is fully settled.');
+        }
+
+        $creditSettlement->update($request->validated());
+
+        return redirect()->route('credits.index', $creditSettlement);
+    }
+
+    public function destroy(CreditSettlement $creditSettlement)
+    {
+        $this->authorize('delete', $creditSettlement->credit);
+
+        $creditSettlement->forceDelete();
+
+        return back()->with('failedMessage', 'Deleted successfully.');
+    }
+}
--- app/Models/CreditSettlement.php
+++ app/Models/CreditSettlement.php
@@ -16,6 +16,24 @@ class CreditSettlement extends Model
 
     protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
 
+    protected static function booted()
+    {
+        static::created(function ($creditSettlement) {
+            $credit = $creditSettlement->credit;
+            $credit->credit_amount_settlement = $credit->creditSettlements()->sum('amount');
+            $credit->last_settled_at = $creditSettlement->created_at;
+
+            $credit->save();
+        });
+
+        static::updated(function ($creditSettlement) {
+            $credit = $creditSettlement->credit;
+            $credit->credit_amount_settlement = $credit->creditSettlements()->sum('amount');
+
+            $credit->save();
+        });
+    }
+
     public function credit()
     {
         return $this->belongsTo(Credit::class);
