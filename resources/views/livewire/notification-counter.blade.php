7bc4ef6ba327c12b6eefabda670e113ce916fb92
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-09 17:01:02 +0300

    display 9+ if notification is greater than 9

--- resources/views/livewire/notification-counter.blade.php
+++ resources/views/livewire/notification-counter.blade.php
@@ -7,6 +7,6 @@
         <i class="fas fa-bell"></i>
     </span>
     <span class="notification-counter has-text-white has-text-centered has-text-weight-bold bg-brown {{ $totalUnreadNotifications ? '' : 'is-hidden' }}">
-        {{ $totalUnreadNotifications }}
+        {{ $totalUnreadNotifications > 9 ? '9+' : $totalUnreadNotifications }}
     </span>
 </a>
