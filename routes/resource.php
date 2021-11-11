e2f04f050e03911270bf7055b37110a5ebb64a44
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-11 16:38:27 +0300

    use only method resource routes with specific actions

--- routes/resource.php
+++ routes/resource.php
@@ -3,85 +3,84 @@
 use App\Http\Controllers\Resource as Resource;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('products', Resource\ProductController::class);
+Route::resource('products', Resource\ProductController::class)->except('show');
 
 Route::resource('categories', Resource\ProductCategoryController::class);
 
 Route::resource('employees', Resource\EmployeeController::class);
 
-Route::resource('companies', Resource\CompanyController::class);
+Route::resource('companies', Resource\CompanyController::class)->only(['edit', 'update']);
 
 Route::resource('purchases', Resource\PurchaseController::class);
 
-Route::resource('purchase-details', Resource\PurchaseDetailController::class);
+Route::resource('purchase-details', Resource\PurchaseDetailController::class)->only('destroy');
 
 Route::resource('sales', Resource\SaleController::class);
 
-Route::resource('sale-details', Resource\SaleDetailController::class);
+Route::resource('sale-details', Resource\SaleDetailController::class)->only('destroy');
 
-Route::resource('notifications', Resource\NotificationController::class);
+Route::resource('notifications', Resource\NotificationController::class)->only(['index', 'update']);
 
-Route::resource('suppliers', Resource\SupplierController::class);
+Route::resource('suppliers', Resource\SupplierController::class)->except('show');
 
-Route::resource('warehouses', Resource\WarehouseController::class);
+Route::resource('warehouses', Resource\WarehouseController::class)->except('show');
 
-Route::resource('customers', Resource\CustomerController::class);
+Route::resource('customers', Resource\CustomerController::class)->except('show');
 
 Route::resource('gdns', Resource\GdnController::class);
 
-Route::resource('gdn-details', Resource\GdnDetailController::class);
+Route::resource('gdn-details', Resource\GdnDetailController::class)->only('destroy');
 
 Route::resource('transfers', Resource\TransferController::class);
 
-Route::resource('transfer-details', Resource\TransferDetailController::class);
+Route::resource('transfer-details', Resource\TransferDetailController::class)->only('destroy');
 
 Route::resource('purchase-orders', Resource\PurchaseOrderController::class);
 
-Route::resource('purchase-order-details', Resource\PurchaseOrderDetailController::class);
+Route::resource('purchase-order-details', Resource\PurchaseOrderDetailController::class)->only('destroy');
 
 Route::resource('grns', Resource\GrnController::class);
 
-Route::resource('grn-details', Resource\GrnDetailController::class);
+Route::resource('grn-details', Resource\GrnDetailController::class)->only('destroy');
 
-Route::resource('general-tender-checklists', Resource\GeneralTenderChecklistController::class);
+Route::resource('general-tender-checklists', Resource\GeneralTenderChecklistController::class)->except('show');
 
-Route::resource('tender-checklist-types', Resource\TenderChecklistTypeController::class);
+Route::resource('tender-checklist-types', Resource\TenderChecklistTypeController::class)->except('show');
 
-Route::resource('tender-statuses', Resource\TenderStatusController::class);
+Route::resource('tender-statuses', Resource\TenderStatusController::class)->except('show');
 
 Route::resource('tenders', Resource\TenderController::class);
 
-Route::resource('tender-details', Resource\TenderDetailController::class);
+Route::resource('tender-details', Resource\TenderDetailController::class)->only('destroy');
 
-Route::resource('tender-readings', Resource\TenderReadingController::class);
+Route::resource('tender-readings', Resource\TenderReadingController::class)->only(['edit', 'update']);
 
-Route::resource('tender-checklists', Resource\TenderChecklistController::class);
+Route::resource('tender-checklists', Resource\TenderChecklistController::class)->except(['index', 'show']);
 
 Route::resource('sivs', Resource\SivController::class);
 
-Route::resource('siv-details', Resource\SivDetailController::class);
+Route::resource('siv-details', Resource\SivDetailController::class)->only('destroy');
 
 Route::resource('proforma-invoices', Resource\ProformaInvoiceController::class);
 
-Route::resource('proforma-invoice-details', Resource\ProformaInvoiceDetailController::class);
+Route::resource('proforma-invoice-details', Resource\ProformaInvoiceDetailController::class)->only('destroy');
 
 Route::resource('damages', Resource\DamageController::class);
 
-Route::resource('damage-details', Resource\DamageDetailController::class);
+Route::resource('damage-details', Resource\DamageDetailController::class)->only('destroy');
 
 Route::resource('adjustments', Resource\AdjustmentController::class);
 
-Route::resource('adjustment-details', Resource\AdjustmentDetailController::class);
+Route::resource('adjustment-details', Resource\AdjustmentDetailController::class)->only('destroy');
 
 Route::resource('returns', Resource\ReturnController::class);
 
-Route::resource('return-details', Resource\ReturnDetailController::class);
+Route::resource('return-details', Resource\ReturnDetailController::class)->only('destroy');
 
 Route::resource('reservations', Resource\ReservationController::class);
 
-Route::resource('reservation-details', Resource\ReservationDetailController::class);
+Route::resource('reservation-details', Resource\ReservationDetailController::class)->only('destroy');
 
 Route::resource('credits', Resource\CreditController::class);
 
-Route::resource('credits.credit-settlements', Resource\CreditSettlementController::class)
-    ->only(['create', 'store', 'edit', 'update', 'destroy'])->shallow();
+Route::resource('credits.credit-settlements', Resource\CreditSettlementController::class)->except(['index', 'show'])->shallow();
