a9f35b6b46f968c399ae993d3ed641241b3c811f
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-16 14:43:54 +0300

    modify ReservationConverted notif message

--- app/Notifications/ReservationConverted.php
+++ app/Notifications/ReservationConverted.php
@@ -23,7 +23,7 @@ class ReservationConverted extends Notification
     {
         return [
             'icon' => 'archive',
-            'message' => 'Reservation has been converted to DO by' . ucfirst($this->reservation->convertedBy->name),
+            'message' => 'Reservation has been converted to Delivery Order by ' . ucfirst($this->reservation->convertedBy->name),
             'endpoint' => '/reservations/' . $this->reservation->id,
         ];
     }
