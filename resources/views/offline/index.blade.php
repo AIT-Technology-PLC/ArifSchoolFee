10e181e373700b6e1ad4952129858687aec968dc
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-20 22:50:32 +0300

    move connection status to component

--- resources/views/offline/index.blade.php
+++ resources/views/offline/index.blade.php
@@ -67,30 +67,7 @@
                 </div>
             </div>
         </section>
-        <div
-            id="backOnline"
-            style="position: fixed; bottom: 0;left: 0;right: 0;"
-            class="bg-green has-text-white has-text-centered is-hidden"
-        >
-            <span class="icon">
-                <i class="fas fa-globe"></i>
-            </span>
-            <span>
-                Back Online
-            </span>
-        </div>
-        <div
-            id="backOffline"
-            style="position: fixed; bottom: 0;left: 0;right: 0;"
-            class="has-background-grey-dark has-text-white has-text-centered is-hidden"
-        >
-            <span class="icon">
-                <i class="fas fa-exclamation-circle"></i>
-            </span>
-            <span>
-                You're Offline
-            </span>
-        </div>
+        <x-common.connection-status />
     </main>
     @include('assets.js')
 </body>
