f90ad2e9ab4ad1b5c90a336d873de8c74a6f6596
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-23 19:49:17 +0300

    assign is_convert_to_siv_as_approved as default

--- database/migrations/2021_11_23_192259_add_is_convert_to_siv_to_as_approved_companies.php
+++ database/migrations/2021_11_23_192259_add_is_convert_to_siv_to_as_approved_companies.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddIsConvertToSivToAsApprovedCompanies extends Migration
@@ -15,13 +14,7 @@ class AddIsConvertToSivToAsApprovedCompanies extends Migration
     public function up()
     {
         Schema::table('companies', function (Blueprint $table) {
-            $table->boolean('is_convert_to_siv_as_approved')->nullable()->after('is_discount_before_vat');
-        });
-
-        DB::statement('UPDATE companies SET is_convert_to_siv_as_approved = 1');
-
-        Schema::table('companies', function (Blueprint $table) {
-            $table->boolean('is_convert_to_siv_as_approved')->nullable(false)->change();
+            $table->boolean('is_convert_to_siv_as_approved')->default(1)->after('is_discount_before_vat');
         });
     }
 
