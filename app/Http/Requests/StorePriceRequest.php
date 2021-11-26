ca81e6642085875646b93cf6b598ba5fe02423de
Nabil Hassen <nabil.hassen08@gmail.com>
2021-11-26 19:36:02 +0300

    use array validation

--- app/Http/Requests/StorePriceRequest.php
+++ app/Http/Requests/StorePriceRequest.php
@@ -15,11 +15,11 @@ class StorePriceRequest extends FormRequest
     public function rules()
     {
         return [
-            'product_id' => ['required', 'integer', new MustBelongToCompany('products')],
-            'type' => ['required', 'string'],
-            'min_price' => ['nullable', 'numeric', 'required_if:type,range', 'prohibited_if:type,fixed', 'gt:0', 'lt:max_price'],
-            'max_price' => ['nullable', 'numeric', 'required_if:type,range', 'prohibited_if:type,fixed', 'gt:0', 'gt:min_price'],
-            'fixed_price' => ['nullable', 'numeric', 'required_if:type,fixed', 'prohibited_if:type,range'],
+            '*.product_id' => ['required', 'integer', new MustBelongToCompany('products')],
+            '*.type' => ['required', 'string'],
+            '*.min_price' => ['nullable', 'numeric', 'required_if:type,range', 'prohibited_if:type,fixed', 'gt:0', 'lt:max_price'],
+            '*.max_price' => ['nullable', 'numeric', 'required_if:type,range', 'prohibited_if:type,fixed', 'gt:0', 'gt:min_price'],
+            '*.fixed_price' => ['nullable', 'numeric', 'required_if:type,fixed', 'prohibited_if:type,range'],
         ];
     }
 }
