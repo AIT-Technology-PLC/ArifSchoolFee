68d9f4b2d9930367e25e63a19dd87da26f07b89e
Nabil Hassen <nabil.hassen08@gmail.com>
2021-12-01 21:00:03 +0300

    show pads option for users with multiple transaction warehouse permission

--- resources/views/gdns/index.blade.php
+++ resources/views/gdns/index.blade.php
@@ -54,31 +54,34 @@
             <x-common.success-message :message="session('deleted')" />
             <x-datatables.filter filters="'pad', 'status'">
                 <div class="columns is-marginless is-vcentered">
-                    <div class="column is-3 p-lr-0 pt-0">
-                        <x-forms.field class="has-text-centered">
-                            <x-forms.control>
-                                <x-forms.select
-                                    id=""
-                                    name=""
-                                    class="is-small is-fullwidth"
-                                    x-model="filters.pad"
-                                    x-on:change="add('pad')"
-                                >
-                                    <option
-                                        disabled
-                                        selected
-                                        value=""
+                    @if (auth()->user()->getAllowedWarehouses('transactions')->count() > 1)
+                        <div class="column is-3 p-lr-0 pt-0">
+                            <x-forms.field class="has-text-centered">
+                                <x-forms.control>
+                                    <x-forms.select
+                                        id=""
+                                        name=""
+                                        class="is-small is-fullwidth"
+                                        x-model="filters.pad"
+                                        x-on:change="add('pad')"
                                     >
-                                        Pads
-                                    </option>
-                                    <option value="all"> All Pads </option>
-                                    @foreach (auth()->user()->getAllowedWarehouses('transactions') as $warehouse)
-                                        <option value="{{ $warehouse->id }}"> {{ $warehouse->name }} Pad </option>
-                                    @endforeach
-                                </x-forms.select>
-                            </x-forms.control>
-                        </x-forms.field>
-                    </div>
+                                        <option
+                                            disabled
+                                            selected
+                                            value=""
+                                        >
+                                            Pads
+                                        </option>
+                                        <option value="all"> All Pads </option>
+                                        @foreach (auth()->user()->getAllowedWarehouses('transactions')
+        as $warehouse)
+                                            <option value="{{ $warehouse->id }}"> {{ $warehouse->name }} Pad </option>
+                                        @endforeach
+                                    </x-forms.select>
+                                </x-forms.control>
+                            </x-forms.field>
+                        </div>
+                    @endif
                     <div class="column is-3 p-lr-0 pt-0">
                         <x-forms.field class="has-text-centered">
                             <x-forms.control>
