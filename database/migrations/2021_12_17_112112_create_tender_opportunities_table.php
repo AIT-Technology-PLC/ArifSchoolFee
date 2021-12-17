e9c6d7a5239fc1edf4ca5c927f06067c119c415e
Nabil Hassen <nabil.hassen08@gmail.com>
2021-12-17 11:32:58 +0300

    build tender opportunities table

---
+++ database/migrations/2021_12_17_112112_create_tender_opportunities_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenderOpportunitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tender_opportunities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('warehouse_id')->nullable()->constrained()->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('tender_status_id')->nullable()->constrained()->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->string('code');
+            $table->string('source');
+            $table->dateTime('published_on')->nullable();
+            $table->longText('body');
+            $table->string('address')->nullable();
+            $table->string('currency')->nullable();
+            $table->decimal('price', 22)->nullable();
+            $table->longText('comments')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('company_id');
+            $table->index('warehouse_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tender_opportunities');
+    }
+}
