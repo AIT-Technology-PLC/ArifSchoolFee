148d771f9f51d053b54f084e17d48e5c1232506d
Nabil Hassen <nabil.hassen08@gmail.com>
2021-12-17 12:04:04 +0300

    implement validation via FormRequest

---
+++ app/Http/Requests/StoreTenderOpportunityRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\MustBelongToCompany;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTenderOpportunityRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'customer_id' => ['nullable', 'integer', new MustBelongToCompany('customers')],
+            'tender_status_id' => ['required', 'integer', new MustBelongToCompany('tender_statues')],
+            'code' => ['required', 'string', 'max:255'],
+            'source' => ['required', 'string', 'max:255'],
+            'published_on' => ['required', 'date'],
+            'body' => ['required', 'string'],
+            'address' => ['nullable', 'string', 'max:255'],
+            'currency' => ['nullable', 'string', 'max:255'],
+            'price' => ['nullable', 'numeric'],
+            'comments' => ['nullable', 'string'],
+        ];
+    }
+}
