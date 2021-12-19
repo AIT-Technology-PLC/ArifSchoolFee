2e7405ee17bc612413db9e0f6714aa819d880f29
Nabil Hassen <nabil.hassen08@gmail.com>
2021-12-19 22:57:33 +0300

    build tenderopportunities show view

--- resources/views/tender-opportunities/index.blade.php
+++ resources/views/tender-opportunities/index.blade.php
@@ -9,7 +9,7 @@
                 <h1 class="title text-green has-text-weight-medium is-size-5">
                     Tender Opportunities
                     <span class="tag bg-green has-text-white has-text-weight-normal ml-1 m-lr-0">
-                        <x-common.icon name="fas fa-th" />
+                        <x-common.icon name="fas fa-comment-dollar" />
                         <span>
                             {{ number_format($totalTenderOpportunities) }} {{ Str::plural('tender opportunities', $totalTenderOpportunities) }}
                         </span>
