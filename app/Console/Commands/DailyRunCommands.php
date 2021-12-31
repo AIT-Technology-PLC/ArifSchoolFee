b5c1fbcaf2a96286566b874c69de9ee299bf33a1
Nabil Hassen <nabil.hassen08@gmail.com>
2021-12-31 16:24:59 +0300

    remove RunDbBackup class

--- app/Console/Commands/DailyRunCommands.php
+++ app/Console/Commands/DailyRunCommands.php
@@ -20,7 +20,7 @@ class DailyRunCommands extends Command
         'proforma-invoice:expiry-date-notification',
         'tender:daily-deadline-notification',
         'credit:due-date-notification',
-        'backup:db',
+        'backup:run --only-db --disable-notifications',
     ];
 
     public function __construct()
