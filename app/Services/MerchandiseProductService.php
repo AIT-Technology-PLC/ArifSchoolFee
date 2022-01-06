60fd15780418336de98e7c63c0fb71573e8fd96c
Nabil Hassen <nabil.hassen08@gmail.com>
2022-01-07 02:48:14 +0300

    refactor MerchandiseProductService

--- app/Services/MerchandiseProductService.php
+++ app/Services/MerchandiseProductService.php
@@ -14,8 +14,10 @@ class MerchandiseProductService
 
         return Product::query()
             ->whereHas('merchandises', function ($query) use ($warehouseId) {
-                $query->whereIn('warehouse_id', auth()->user()->getAllowedWarehouses('read')->pluck('id'))
-                    ->when($warehouseId, fn($query) => $query->where('warehouse_id', $warehouseId))
+                $query->when($warehouseId, fn($query) => $query->where('warehouse_id', $warehouseId))
+                    ->when(auth()->check(), function ($query) {
+                        $query->whereIn('warehouse_id', auth()->user()->getAllowedWarehouses('read')->pluck('id'));
+                    })
                     ->where(function ($query) {
                         $query->where('available', '>', 0)
                             ->orWhere('reserved', '>', 0);
@@ -34,8 +36,10 @@ class MerchandiseProductService
                 $query->select('product_id')
                     ->from('merchandises')
                     ->where('company_id', userCompany()->id)
-                    ->whereIn('warehouse_id', auth()->user()->getAllowedWarehouses('read')->pluck('id'))
                     ->when($warehouseId, fn($query) => $query->where('warehouse_id', $warehouseId))
+                    ->when(auth()->check(), function ($query) {
+                        $query->whereIn('warehouse_id', auth()->user()->getAllowedWarehouses('read')->pluck('id'));
+                    })
                     ->where(function ($query) {
                         $query->where('available', '>', 0)
                             ->orWhere('reserved', '>', 0);
