8aad162fe8c6eb4ac16e52050fceb370a1736e2c
Nabil Hassen <nabil.hassen08@gmail.com>
2022-04-19 11:18:31 +0300

    build pricing ticket methods for transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\Branchable;
 use App\Traits\HasUserstamps;
 use App\Traits\MultiTenancy;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -51,5 +52,64 @@ class Transaction extends Model
     public function isClosed()
     {
         return $this->transactionFields()->where('key', 'closed_by')->exists();
+    }
+
+    public function subtotalPrice(): Attribute
+    {
+        $unitPricePadField = PadField::firstWhere('label', 'Unit Price');
+        $quantityPadField = PadField::firstWhere('label', 'Quantity');
+
+        $unitPrice = $this->transactionFields()->where('pad_field_id', $unitPricePadField->id)->sum('value');
+        $quantity = $this->transactionFields()->where('pad_field_id', $quantityPadField->id)->sum('value');
+
+        return Attribute::make(
+            get:fn() => number_format(
+                $unitPrice * $quantity,
+                2,
+                thousands_separator:''
+            )
+        );
+    }
+
+    public function vat(): Attribute
+    {
+        return Attribute::make(
+            get:fn() => number_format(
+                $this->subtotalPrice * 0.15,
+                2,
+                thousands_separator:''
+            )
+        );
+    }
+
+    public function grandTotalPrice(): Attribute
+    {
+        return Attribute::make(
+            get:fn() => number_format(
+                $this->subtotalPrice + $this->vat,
+                2,
+                thousands_separator:''
+            )
+        );
+    }
+
+    public function grandTotalPriceAfterDiscount(): Attribute
+    {
+        $discountPadField = PadField::firstWhere([
+            ['label', 'Discount'],
+            ['is_master_field', 0],
+        ]);
+
+        $discount = $this->transactionFields()->firstWhere('pad_field_id', $discountPadField->id)->value;
+
+        $discountAmount = number_format($this->grandTotalPrice * $discount, 2, thousands_separator:'');
+
+        return Attribute::make(
+            get:fn() => number_format(
+                $this->grandTotalPrice - $discountAmount,
+                2,
+                thousands_separator:''
+            )
+        );
     }
 }
