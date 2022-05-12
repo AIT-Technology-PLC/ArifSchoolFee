64675d503b9e00734837d0c1e335e3f821225b6b
molla-12 <mollaman@gmail.com>
2022-05-12 10:35:13 +0300

    change on store and Update ReservationRequest change cash_received_type to cash_received and cash_received" and "cash_received_type" are required only if "payment_method" is "Credit Payment"

--- app/Http/Requests/UpdateReservationRequest.php
+++ app/Http/Requests/UpdateReservationRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use App\Rules\MustBelongToCompany;
 use App\Rules\UniqueReferenceNum;
 use App\Rules\ValidatePrice;
+use App\Rules\VerifyCashReceivedAmountIsValid;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -29,18 +30,18 @@ class UpdateReservationRequest extends FormRequest
             'customer_id' => ['nullable', 'integer', new MustBelongToCompany('customers')],
             'issued_on' => ['required', 'date'],
             'expires_on' => ['required', 'date', 'after_or_equal:issued_on'],
-            'payment_type' => ['required', 'string', function ($attribute, $value, $fail) {
-                if ($this->get('cash_received_in_percentage') == 100 && $value == 'Credit Payment') {
-                    $fail('If "Cash Received" is 100%, then "Payment Type" should be "Cash Payment"');
-                }
+            'payment_type' => ['required', 'string'],
 
-                if ($this->get('cash_received_in_percentage') < 100 && $value == 'Cash Payment') {
-                    $fail('If "Cash Received" is less than 100%, then "Payment Type" should be "Credit Payment"');
+            'cash_received_type' => ['required_if:payment_type,Credit Payment', 'exclude_if:payment_type,Cash Payment', 'string', new VerifyCashReceivedAmountIsValid($this->get('discount'), $this->get('reservation'), $this->get('cash_received_type')), function ($attribute, $value, $fail) {
+                if ($this->get('cash_received') == 100 && $value == 'percent') {
+                    $fail('If "Cash Received" is 100%, then "Payment Type" should be "Cash Payment"');
+
                 }
             }],
+
             'description' => ['nullable', 'string'],
-            'cash_received_in_percentage' => ['required', 'numeric', 'between:0,100'],
-            'due_date' => ['nullable', 'date', 'after:issued_on', 'required_unless:cash_received_in_percentage,100', 'prohibited_if:cash_received_in_percentage,100'],
+            'cash_received' => ['required_if:payment_type,Credit Payment', 'nullable', 'numeric', 'exclude_if:payment_type,Cash Payment'],
+            'due_date' => ['nullable', 'date', 'after:issued_on', 'required_if:payment_type,Credit Payment'],
             'discount' => ['nullable', 'numeric', 'min:0', 'max:100'],
         ];
     }
