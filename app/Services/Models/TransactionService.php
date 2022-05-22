f3e7b6cae1acad98039e11691a6ab5e9b1005373
Nabil Hassen <nabil.hassen08@gmail.com>
2022-05-23 00:00:28 +0300

    build transaction subtract method

--- app/Services/Models/TransactionService.php
+++ app/Services/Models/TransactionService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services\Models;
 
+use App\Models\GdnDetail;
 use App\Models\Transaction;
+use App\Services\Inventory\InventoryOperationService;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
@@ -32,6 +34,38 @@ class TransactionService
         });
     }
 
+    public function subtract($transaction, $user)
+    {
+        $transactionDetails = $this->formatTransactionDetails($transaction);
+
+        if (!$user->hasWarehousePermission('subtract',
+            $transactionDetails->pluck('warehouse_id')->toArray())) {
+            return [false, 'You do not have permission to subtract from one or more of the warehouses.'];
+        }
+
+        if ($transaction->pad->isApprovable() && !$transaction->isApproved()) {
+            return [false, 'This transaction is not approved yet.'];
+        }
+
+        if ($transaction->pad->isInventoryOperationSubtract() && $transaction->isSubtracted()) {
+            return [false, 'This transaction is already subtracted from inventory'];
+        }
+
+        $unavailableProducts = InventoryOperationService::unavailableProducts($transactionDetails);
+
+        if ($unavailableProducts->isNotEmpty()) {
+            return [false, $unavailableProducts];
+        }
+
+        DB::transaction(function () use ($transaction, $transactionDetails) {
+            InventoryOperationService::subtract($transactionDetails);
+
+            $transaction->subtract();
+        });
+
+        return [true, ''];
+    }
+
     private function storeTransactionFields($transaction, $data)
     {
         $line = 0;
@@ -58,5 +92,25 @@ class TransactionService
                 $line++;
             }
         }
+    }
+
+    private function formatTransactionDetails($transaction)
+    {
+        $padFields = $transaction->pad->padFields()->detailFields()->get();
+
+        return $transaction
+            ->transactionFields()
+            ->detailFields()
+            ->get()
+            ->groupBy('line')
+            ->map(function ($transactionField) use ($padFields) {
+                $data['quantity'] = $transactionField->firstWhere('pad_field_id', $padFields->firstWhere('label', 'Quantity')->id)->value;
+
+                $data['product_id'] = $transactionField->firstWhere('pad_field_id', $padFields->firstWhere('label', 'Product')->id)->value;
+
+                $data['warehouse_id'] = $transactionField->firstWhere('pad_field_id', $padFields->firstWhere('label', 'Warehouse')->id)->value;
+
+                return new GdnDetail($data);
+            });
     }
 }
