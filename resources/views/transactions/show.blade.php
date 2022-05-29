2872b58d841f4ca8be3e2caf726353dd55940b26
Nabil Hassen <nabil.hassen08@gmail.com>
2022-05-29 23:33:01 +0300

    add dropdown to transactions show view

--- resources/views/transactions/show.blade.php
+++ resources/views/transactions/show.blade.php
@@ -60,75 +60,92 @@
     </x-common.content-wrapper>
 
     <x-common.content-wrapper class="mt-5">
-        <x-content.header title="Details">
-            @if ($transaction->pad->isApprovable() && !$transaction->isApproved())
-                @can('approve', $transaction)
-                    <x-common.transaction-button
-                        :route="route('transactions.approve', $transaction->id)"
-                        action="approve"
-                        intention="approve this transaction"
-                        icon="fas fa-signature"
-                        label="Approve Transaction"
-                        class="has-text-weight-medium"
-                    />
-                @endcan
-            @elseif($transaction->pad->isInventoryOperationSubtract() && !$transaction->isSubtracted())
-                @can('subtract', $transaction)
-                    <x-common.transaction-button
-                        :route="route('transactions.subtract', $transaction->id)"
-                        action="subtract"
-                        intention="subtract this transaction"
-                        icon="fas fa-signature"
-                        label="Subtract Transaction"
-                        class="has-text-weight-medium"
-                    />
-                @endcan
-            @elseif($transaction->pad->isInventoryOperationAdd() && !$transaction->isAdded())
-                @can('add', $transaction)
-                    <x-common.transaction-button
-                        :route="route('transactions.add', $transaction->id)"
-                        action="add"
-                        intention="add this transaction"
-                        icon="fas fa-signature"
-                        label="Add Transaction"
-                        class="has-text-weight-medium"
-                    />
-                @endcan
-            @elseif($transaction->pad->isClosable() && !$transaction->isClosed())
-                @can('close', $transaction)
-                    <x-common.transaction-button
-                        :route="route('transactions.close', $transaction->id)"
-                        action="close"
-                        intention="close this transaction"
-                        icon="fas fa-signature"
-                        label="Close Transaction"
-                        class="has-text-weight-medium"
-                    />
-                @endcan
-            @elseif($transaction->pad->isCancellable() && !$transaction->isCancelled())
-                @can('cancel', $transaction)
-                    <x-common.transaction-button
-                        :route="route('transactions.cancel', $transaction->id)"
-                        action="cancel"
-                        intention="cancel this transaction"
-                        icon="fas fa-signature"
-                        label="Cancel Transaction"
-                        class="has-text-weight-medium"
-                    />
-                @endcan
-            @endif
-            @if (!$transaction->isApproved() && !$transaction->isCancelled() && !$transaction->isClosed() && !$transaction->isAdded() && !$transaction->isSubtracted())
-                @can('update', $transaction)
-                    <x-common.button
-                        tag="a"
-                        href="{{ route('transactions.edit', $transaction->id) }}"
-                        mode="button"
-                        icon="fas fa-pen"
-                        label="Edit"
-                        class="is-small bg-green has-text-white"
-                    />
-                @endcan
-            @endif
+        <x-content.header
+            title="Details"
+            is-mobile
+        >
+            <x-common.dropdown name="Actions">
+                @if ($transaction->pad->isApprovable() && !$transaction->isApproved())
+                    @can('approve', $transaction)
+                        <x-common.dropdown-item>
+                            <x-common.transaction-button
+                                :route="route('transactions.approve', $transaction->id)"
+                                action="approve"
+                                intention="approve this transaction"
+                                icon="fas fa-signature"
+                                label="Approve"
+                                class="has-text-weight-medium is-small text-green is-borderless is-transparent-color is-block is-fullwidth has-text-left"
+                            />
+                        </x-common.dropdown-item>
+                    @endcan
+                @elseif($transaction->pad->isInventoryOperationSubtract() && !$transaction->isSubtracted())
+                    @can('subtract', $transaction)
+                        <x-common.dropdown-item>
+                            <x-common.transaction-button
+                                :route="route('transactions.subtract', $transaction->id)"
+                                action="subtract"
+                                intention="subtract this transaction"
+                                icon="fas fa-signature"
+                                label="Subtract"
+                                class="has-text-weight-medium is-small text-green is-borderless is-transparent-color is-block is-fullwidth has-text-left"
+                            />
+                        </x-common.dropdown-item>
+                    @endcan
+                @elseif($transaction->pad->isInventoryOperationAdd() && !$transaction->isAdded())
+                    @can('add', $transaction)
+                        <x-common.dropdown-item>
+                            <x-common.transaction-button
+                                :route="route('transactions.add', $transaction->id)"
+                                action="add"
+                                intention="add this transaction"
+                                icon="fas fa-signature"
+                                label="Add"
+                                class="has-text-weight-medium is-small text-green is-borderless is-transparent-color is-block is-fullwidth has-text-left"
+                            />
+                        </x-common.dropdown-item>
+                    @endcan
+                @elseif($transaction->pad->isClosable() && !$transaction->isClosed())
+                    @can('close', $transaction)
+                        <x-common.dropdown-item>
+                            <x-common.transaction-button
+                                :route="route('transactions.close', $transaction->id)"
+                                action="close"
+                                intention="close this transaction"
+                                icon="fas fa-signature"
+                                label="Close"
+                                class="has-text-weight-medium is-small text-green is-borderless is-transparent-color is-block is-fullwidth has-text-left"
+                            />
+                        </x-common.dropdown-item>
+                    @endcan
+                @elseif($transaction->pad->isCancellable() && !$transaction->isCancelled())
+                    @can('cancel', $transaction)
+                        <x-common.dropdown-item>
+                            <x-common.transaction-button
+                                :route="route('transactions.cancel', $transaction->id)"
+                                action="cancel"
+                                intention="cancel this transaction"
+                                icon="fas fa-signature"
+                                label="Cancel"
+                                class="has-text-weight-medium is-small text-green is-borderless is-transparent-color is-block is-fullwidth has-text-left"
+                            />
+                        </x-common.dropdown-item>
+                    @endcan
+                @endif
+                @if (!$transaction->isApproved() && !$transaction->isCancelled() && !$transaction->isClosed() && !$transaction->isAdded() && !$transaction->isSubtracted())
+                    @can('update', $transaction)
+                        <x-common.dropdown-item>
+                            <x-common.button
+                                tag="a"
+                                href="{{ route('transactions.edit', $transaction->id) }}"
+                                mode="button"
+                                icon="fas fa-pen"
+                                label="Edit"
+                                class="has-text-weight-medium is-small text-green is-borderless is-transparent-color is-block is-fullwidth has-text-left"
+                            />
+                        </x-common.dropdown-item>
+                    @endcan
+                @endif
+            </x-common.dropdown>
         </x-content.header>
         <x-content.footer>
             <x-common.fail-message :message="session('failedMessage')" />
