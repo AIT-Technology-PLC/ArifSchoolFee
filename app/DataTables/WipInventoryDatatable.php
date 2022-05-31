0657d1241a9b816506601488fefca8ad61e457f1
mikiyas <mikiyasleul@gmail.com>
2022-05-31 11:27:00 +0300

    Add "WIP" tab for premium plan and wipInventory datatable

---
+++ app/DataTables/WipInventoryDatatable.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Traits\DataTableHtmlBuilder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\Services\DataTable;
+
+class WipInventoryDatatable extends DataTable
+{
+    use DataTableHtmlBuilder;
+
+    public function __construct()
+    {
+        $this->warehouses = auth()->user()->getAllowedWarehouses('read');
+    }
+
+    public function dataTable($query)
+    {
+        return $this
+            ->editWarehouses(datatables()->collection($query->all()))
+            ->editColumn('product', function ($row) {
+                return view('components.datatables.product-code', [
+                    'product' => $row['product'],
+                    'code' => $row['code'],
+                ]);
+            })
+            ->rawColumns([
+                ...$this->warehouses->pluck('name')->toArray(),
+                'total balance',
+                'product',
+            ])
+            ->addIndexColumn();
+    }
+
+    private function editWarehouses($datatable)
+    {
+        $this->warehouses->each(function ($warehouse) use ($datatable) {
+            $datatable
+                ->editColumn($warehouse->name, function ($row) use ($warehouse) {
+                    return view('components.datatables.green-outlined-tag', [
+                        'amount' => Arr::has($row, $warehouse->name) ? $row[$warehouse->name] : 0.00,
+                        'unit' => $row['unit'],
+                    ]);
+                })
+                ->editColumn('total balance', function ($row) {
+                    return view('components.datatables.green-solid-tag', [
+                        'amount' => $row['total balance'] ?: 0.00,
+                        'unit' => $row['unit'],
+                    ]);
+                });
+        });
+
+        return $datatable;
+    }
+
+    public function query()
+    {
+        $wipMerchandises = DB::table('merchandises')
+            ->join('products', 'merchandises.product_id', '=', 'products.id')
+            ->join('product_categories', 'products.product_category_id', '=', 'product_categories.id')
+            ->join('warehouses', 'merchandises.warehouse_id', '=', 'warehouses.id')
+            ->where('merchandises.company_id', '=', userCompany()->id)
+            ->where('merchandises.wip', '>', 0)
+            ->whereIn('warehouses.id', auth()->user()->getAllowedWarehouses('read')->pluck('id'))
+            ->select([
+                'merchandises.wip as wip',
+                'products.id as product_id',
+                'products.name as product',
+                'products.code as code',
+                'products.unit_of_measurement as unit',
+                'product_categories.name as category',
+                'warehouses.name as warehouse',
+            ])
+            ->get();
+
+        $wipMerchandises = $wipMerchandises->groupBy('product_id')->map->keyBy('warehouse');
+
+        $organizedWipMerchandise = collect();
+
+        foreach ($wipMerchandises as $merchandiseKey => $merchandiseValue) {
+            $currentMerchandiseItem = [
+                'product' => $merchandiseValue->first()->product,
+                'code' => $merchandiseValue->first()->code ?? '',
+                'product_id' => $merchandiseValue->first()->product_id,
+                'unit' => $merchandiseValue->first()->unit,
+                'category' => $merchandiseValue->first()->category,
+                'total balance' => $merchandiseValue->sum('wip'),
+            ];
+
+            foreach ($merchandiseValue as $key => $value) {
+
+                $currentMerchandiseItem = Arr::add($currentMerchandiseItem, $key, $value->wip);
+            }
+
+            $organizedWipMerchandise->push($currentMerchandiseItem);
+        }
+
+        return $organizedWipMerchandise;
+    }
+
+    protected function getColumns()
+    {
+        $warehouses = $this->warehouses->pluck('name');
+
+        return [
+            '#' => [
+                'sortable' => false,
+            ],
+            'product',
+            'category',
+            ...$warehouses,
+            'total balance',
+        ];
+
+    }
+
+    protected function filename()
+    {
+        return 'InventoryLevel_' . date('YmdHis');
+    }
+}
--- resources/views/merchandises/index.blade.php
+++ resources/views/merchandises/index.blade.php
@@ -177,31 +177,33 @@
                                     </x-forms.control>
                                 </x-forms.field>
                             </div>
-                            <div class="column is-3 p-lr-0 pt-0">
-                                <x-forms.field class="has-text-centered">
-                                    <x-forms.control>
-                                        <x-forms.select
-                                            id=""
-                                            name=""
-                                            class="is-size-7-mobile is-fullwidth"
-                                            x-model="filters.type"
-                                            x-on:change="add('type')"
-                                        >
-                                            <option
-                                                disabled
-                                                selected
-                                                value=""
+                            @if (userCompany()->plan->isPremium())
+                                <div class="column is-3 p-lr-0 pt-0">
+                                    <x-forms.field class="has-text-centered">
+                                        <x-forms.control>
+                                            <x-forms.select
+                                                id=""
+                                                name=""
+                                                class="is-size-7-mobile is-fullwidth"
+                                                x-model="filters.type"
+                                                x-on:change="add('type')"
                                             >
-                                                Type
-                                            </option>
-                                            <option value="all"> All </option>
-                                            @foreach (['Finished Goods', 'Raw Material'] as $type)
-                                                <option value="{{ str()->lower($type) }}"> {{ $type }} </option>
-                                            @endforeach
-                                        </x-forms.select>
-                                    </x-forms.control>
-                                </x-forms.field>
-                            </div>
+                                                <option
+                                                    disabled
+                                                    selected
+                                                    value=""
+                                                >
+                                                    Type
+                                                </option>
+                                                <option value="all"> All </option>
+                                                @foreach (['Finished Goods', 'Raw Material'] as $type)
+                                                    <option value="{{ str()->lower($type) }}"> {{ $type }} </option>
+                                                @endforeach
+                                            </x-forms.select>
+                                        </x-forms.control>
+                                    </x-forms.field>
+                                </div>
+                            @endif
                         </div>
                     </x-datatables.filter>
                 </x-content.footer>
@@ -218,6 +220,13 @@
                     <li class="reserved {{ request()->is('merchandises/reserved') ? 'is-active' : '' }}">
                         <a href="{{ route('merchandises.index', 'reserved') }}">
                             <span>Reserved</span>
+                        </a>
+                    </li>
+                @endif
+                @if (userCompany()->plan->isPremium())
+                    <li class="wip {{ request()->is('merchandises/wip') ? 'is-active' : '' }}">
+                        <a href="{{ route('merchandises.index', 'wip') }}">
+                            <span>Work in Process</span>
                         </a>
                     </li>
                 @endif
