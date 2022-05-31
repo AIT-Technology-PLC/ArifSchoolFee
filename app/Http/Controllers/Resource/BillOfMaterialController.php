35913af21b54d9a6c0890f7323500bcdca775785
molla-12 <mollaman19@gmail.com>
2022-05-31 10:05:38 +0300

    BillOfMaterialController  methode implemented

--- app/Http/Controllers/Resource/BillOfMaterialController.php
+++ app/Http/Controllers/Resource/BillOfMaterialController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\DataTables\BillOfMaterialDatatable;
+use App\DataTables\BillOfMaterialDetailDatatable;
+use App\Http\Requests\StoreBillOfMaterialRequest;
+use App\Http\Requests\UpdateBillOfMaterialRequest;
 use App\Models\BillOfMaterial;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BillOfMaterialController extends Controller
 {
     public function __construct()
     {
         $this->middleware('isFeatureAccessible:Bill Of Material Management');
+
+        $this->authorizeResource(BillOfMaterial::class, 'billOfMaterial');
     }
 
     public function index(BillOfMaterialDatatable $datatable)
@@ -24,31 +29,57 @@ class BillOfMaterialController extends Controller
 
     public function create()
     {
-        //
+        return view('bill-of-materials.create');
     }
 
-    public function store(Request $request)
+    public function store(StoreBillOfMaterialRequest $request)
     {
-        //
+        $billOfMaterial = DB::transaction(function () use ($request) {
+            $billOfMaterial = BillOfMaterial::create($request->safe()->except('billOfMaterial'));
+
+            $billOfMaterial->billOfMaterialDetails()->createMany($request->billOfMaterial);
+
+            return $billOfMaterial;
+        });
+
+        return redirect()->route('bill-of-materials.show', $billOfMaterial->id);
     }
 
-    public function show($id)
+    public function show(BillOfMaterial $billOfMaterial, BillOfMaterialDetailDatatable $datatable)
     {
-        //
+        $datatable->builder()->setTableId('bill-of-material-details-datatable');
+
+        $billOfMaterial->load(['billOfMaterialDetails.product']);
+
+        return $datatable->render('bill-of-materials.show', compact('billOfMaterial'));
     }
 
-    public function edit($id)
+    public function edit(BillOfMaterial $billOfMaterial)
     {
-        //
+        $billOfMaterial->load(['billOfMaterialDetails.product']);
+
+        return view('bill-of-materials.edit', compact('billOfMaterial'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UpdateBillOfMaterialRequest $request, BillOfMaterial $billOfMaterial)
     {
-        //
+        DB::transaction(function () use ($request, $billOfMaterial) {
+            $billOfMaterial->update($request->safe()->except('billOfMaterial'));
+
+            for ($i = 0; $i < count($request->billOfMaterial); $i++) {
+                $billOfMaterial->billOfMaterialDetails[$i]->update($request->billOfMaterial[$i]);
+            }
+        });
+
+        return redirect()->route('bill-of-materials.show', $billOfMaterial->id);
     }
 
-    public function destroy($id)
+    public function destroy(BillOfMaterial $billOfMaterial)
     {
-        //
+        abort_if(!auth()->user()->can('Delete BOM'), 403);
+
+        $billOfMaterial->forceDelete();
+
+        return back()->with('deleted', 'Deleted successfully.');
     }
 }
