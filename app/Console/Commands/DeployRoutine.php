8760380dfc9dbb73d800c25745974acfd12f1b55
Nabil Hassen <nabil.hassen08@gmail.com>
2022-06-04 12:19:51 +0300

    Automate deployment routine with artisan command

---
+++ app/Console/Commands/DeployRoutine.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class DeployRoutine extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'deploy';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deploy changes from onrica/smartwork repo main branch';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call('down');
+
+        if (env('APP_ENV') == 'production') {
+            $githubData = env('GITHUB_USERNAME') . ':' . env('GITHUB_PASSWORD');
+
+            shell_exec('composer install --no-dev');
+
+            shell_exec('git pull https://' . $githubData . '@github.com/onrica/smartwork.git');
+        }
+
+        Artisan::call('migrate --force');
+
+        Artisan::call('db:seed Plans');
+        Artisan::call('db:seed Limits');
+        Artisan::call('db:seed Features');
+        Artisan::call('db:seed Integrations');
+        Artisan::call('db:seed Permissions');
+
+        Artisan::call('optimize:cache');
+
+        Artisan::call('up');
+
+        $this->info('Deploy completed successfully');
+
+        return 0;
+    }
+}
