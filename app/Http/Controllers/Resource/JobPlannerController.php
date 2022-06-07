360ba2257950269a8a6e118233a718e4e20fff74
molla-12 <mollaman19@gmail.com>
2022-06-07 09:45:28 +0300

    empity JobPlannerController created

---
+++ app/Http/Controllers/Resource/JobPlannerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Resource;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreJobPlannerRequest;
+
+class JobPlannerController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function store(StoreJobPlannerRequest $request)
+    {
+
+    }
+}
