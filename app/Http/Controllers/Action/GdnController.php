e400765ef7effed4378f981787dbef0a3c6c1cad
molla-12 <mollaman19@gmail.com>
2022-06-27 09:46:52 +0300

    import gdn . valigate price class and gdn controller

--- app/Http/Controllers/Action/GdnController.php
+++ app/Http/Controllers/Action/GdnController.php
@@ -5,16 +5,26 @@ namespace App\Http\Controllers\Action;
 use App\Actions\ApproveTransactionAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UploadImportFileRequest;
+use App\Imports\GdnImport;
 use App\Models\Credit;
 use App\Models\Gdn;
 use App\Models\Siv;
 use App\Notifications\GdnApproved;
+use App\Notifications\GdnPrepared;
 use App\Notifications\GdnSubtracted;
+use App\Rules\CheckCustomerCreditLimit;
+use App\Rules\MustBelongToCompany;
+use App\Rules\UniqueReferenceNum;
+use App\Rules\ValidatePrice;
+use App\Rules\VerifyCashReceivedAmountIsValid;
 use App\Services\Models\GdnService;
 use App\Utilities\Notifiables;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class GdnController extends Controller
 {
@@ -114,15 +124,76 @@ class GdnController extends Controller
         return redirect()->route('credits.show', $gdn->credit->id);
     }
 
-    public function import(UploadImportFileRequest $request)
+    public function import(UploadImportFileRequest $importFileRequest)
     {
         $this->authorize('import', Gdn::class);
 
         ini_set('max_execution_time', '-1');
 
-        DB::transaction(function () use ($request) {
+        $sheets = (new GdnImport)->toArray($importFileRequest->safe()['file']);
+        $data = $sheets['master'][0];
+        $data['gdn'] = $sheets['detail'];
+        $data['code'] = nextReferenceNumber('gdns');
+
+        $validatedData = $this->validatedData($data);
+
+        $gdn = DB::transaction(function () use ($validatedData) {
+            $gdn = Gdn::create(Arr::except($validatedData, 'gdn'));
+
+            $gdn->gdnDetails()->createMany($validatedData['gdn']);
+
+            Notification::send(Notifiables::byNextActionPermission('Approve GDN'), new GdnPrepared($gdn));
+
+            return $gdn;
         });
 
-        return back()->with('imported', __('messages.file_imported'));
+        return redirect()->route('gdns.show', $gdn->id);
+    }
+
+    public function validatedData($data)
+    {
+        return Validator::make($data, [
+            'code' => ['required', 'integer', new UniqueReferenceNum('gdns')],
+            'gdn' => ['required', 'array'],
+            'gdn.*.product_id' => ['required', 'integer', 'distinct', new MustBelongToCompany('products')],
+            'gdn.*.warehouse_id' => ['required', 'integer', Rule::in(auth()->user()->getAllowedWarehouses('sales')->pluck('id'))],
+            'gdn.*.unit_price' => ['nullable', 'numeric', new ValidatePrice(['gdn' => $data['gdn']])],
+            'gdn.*.quantity' => ['required', 'numeric', 'gt:0'],
+            'gdn.*.description' => ['nullable', 'string'],
+            'gdn.*.discount' => ['nullable', 'numeric', 'min:0', 'max:100'],
+            'customer_id' => ['nullable', 'integer', new MustBelongToCompany('customers'), new CheckCustomerCreditLimit($data['discount'],
+                $data['gdn'],
+                $data['payment_type'],
+                $data['cash_received_type'],
+                $data['cash_received']),
+            ],
+            'sale_id' => ['nullable', 'integer', new MustBelongToCompany('sales')],
+            'issued_on' => ['required', 'date'],
+            'payment_type' => ['required', 'string', function ($attribute, $value, $fail) use ($data) {
+                if ($value == 'Credit Payment' && is_null($data['customer_id'])) {
+                    $fail('Creating a credit for delivery order that has no customer is not allowed.');
+                }
+            }],
+
+            'cash_received_type' => ['required', 'string', function ($attribute, $value, $fail) use ($data) {
+                if ($data['payment_type'] == 'Cash Payment' && $value != 'percent') {
+                    $fail('When payment type is "Cash Payment", the type should be "Percent".');
+                }
+            }],
+
+            'description' => ['nullable', 'string'],
+
+            'cash_received' => ['required', 'numeric', 'gte:0', new VerifyCashReceivedAmountIsValid($data['discount'], $data['gdn'], $data['cash_received_type']), function ($attribute, $value, $fail) use ($data) {
+                if ($data['cash_received_type'] == 'percent' && $value > 100) {
+                    $fail('When type is "Percent", the percentage amount must be between 0 and 100.');
+                }
+                if ($data['payment_type'] == 'Cash Payment' && $value != 100) {
+                    $fail('When payment type is "Cash Payment", the percentage amount must be 100.');
+                }
+            }],
+
+            'due_date' => ['nullable', 'date', 'after:issued_on', 'required_if:payment_type,Credit Payment', 'prohibited_if:payment_type,Cash Payment'],
+            'discount' => ['nullable', 'numeric', 'min:0', 'max:100'],
+        ])->validated();
     }
 }
--- app/Imports/GdnImport.php
+++ app/Imports/GdnImport.php
@@ -2,88 +2,20 @@
 
 namespace App\Imports;
 
-use App\Models\Customer;
-use App\Models\Gdn;
-use App\Models\Product;
-use App\Models\Warehouse;
-use App\Rules\UniqueReferenceNum;
-use App\Rules\ValidatePrice;
-use Illuminate\Validation\Rule;
+use App\Imports\GdnDetailImport;
+use App\Imports\GdnMasterImport;
 use Maatwebsite\Excel\Concerns\Importable;
-use Maatwebsite\Excel\Concerns\OnEachRow;
-use Maatwebsite\Excel\Concerns\WithChunkReading;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\WithValidation;
-use Maatwebsite\Excel\Row;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class GdnImport implements WithHeadingRow, OnEachRow, WithValidation, WithChunkReading
+class GdnImport implements WithMultipleSheets
 {
     use Importable;
 
-    private $gdn;
-
-    private $warehouses;
-
-    private $customers;
-
-    private $products;
-
-    public function __construct($gdn)
-    {
-        $this->gdn = $gdn;
-
-        $this->products = Product::all(['id', 'name']);
-
-        $this->warehouses = Warehouse::all(['id', 'name']);
-
-        $this->customers = Customer::all(['id', 'company_name']);
-    }
-
-    public function onRow(Row $row)
-    {
-        $gdn = Gdn::create([
-            'code' => nextReferenceNumber('gdns'),
-            'customer_id' => $this->products->firstWhere('company_name', $row['customer_name'])->id,
-            'issued_on' => $row['issued_on'],
-            'description' => $row['description'],
-            'payment_type' => $row['payment_type'],
-            'cash_received_type' => $row['cash_received_type'],
-            'cash_received' => $row['cash_received'],
-            'due_date' => $row['due_date'],
-            'discount' => $row['discount'],
-        ]);
-
-        $gdn->gdnDetail()->create([
-            'product_id' => $this->products->firstWhere('name', $row['product_name'])->id,
-            'warehouse_id' => $this->warehouses->firstWhere('name', $row['warehouse_name'])->id,
-            'quantity' => $row['quantity'],
-            'unit_price' => $row['unit_price'],
-            'description' => $row['description'],
-            'discount' => $row['discount'],
-
-        ]);
-    }
-
-    public function rules(): array
+    public function sheets(): array
     {
         return [
-            'code' => ['required', 'integer', new UniqueReferenceNum('gdns')],
-            'product_name' => ['required', 'string', 'max:255', Rule::in($this->products->pluck('name'))],
-            'warehouse_name' => ['required', 'string', Rule::in($this->warehouses->pluck('name'))],
-            'quantity' => ['required', 'numeric', 'gt:0'],
-            'unit_price' => ['nullable', 'numeric', new ValidatePrice],
-            'description' => ['nullable', 'string'],
-            'discount' => ['nullable', 'numeric', 'gt:0'],
+            'master' => new GdnMasterImport(),
+            'detail' => new GdnDetailImport(),
         ];
-    }
-
-    public function chunkSize(): int
-    {
-        return 500;
-    }
-
-    public function batchSize(): int
-    {
-        return 500;
     }
 }
