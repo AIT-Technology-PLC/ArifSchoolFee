c0f3a218c9a3c8d6a0fc2c3903a09da5b2ed22ec
mikiyas <mikiyasleul@gmail.com>
2022-07-21 09:35:28 +0300

    new feature added upto building models

---
+++ app/Models/Advancement.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Approvable;
+use App\Traits\Branchable;
+use App\Traits\Cancellable;
+use App\Traits\HasUserstamps;
+use App\Traits\MultiTenancy;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Advancement extends Model
+{
+    use MultiTenancy, SoftDeletes, HasUserstamps, Approvable, Cancellable, Branchable;
+
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    protected $casts = [
+        'issued_on' => 'datetime',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($advancement) {
+            if (auth()->check()) {
+                $advancement['code'] = nextReferenceNumber('advancements');
+            }
+        });
+    }
+
+    public function advancementDetails()
+    {
+        return $this->hasMany(AdvancementDetail::class);
+    }
+}
---
+++ database/migrations/2022_07_21_085604_create_advancements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('advancements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('warehouse_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('approved_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('cancelled_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->bigInteger('code');
+            $table->dateTime('issued_on')->nullable();
+            $table->string('type');
+            $table->longText('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('company_id');
+            $table->index('warehouse_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('advancements');
+    }
+};
