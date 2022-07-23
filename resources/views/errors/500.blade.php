44225ae8c00d9d1320abc844c0f857762000de22
mikiyas <mikiyasleul@gmail.com>
2022-07-23 10:18:31 +0300

    adding new feature for 500 status code

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Internal Server Error </title>
+    @include('assets.css')
+    @include("pwa.tags")
+</head>
+
+<body>
+    <main>
+        <section class="hero bg-lightgreen is-fullheight">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title has-text-grey-lighter is-size-1" style="font-size: 150px !important;">
+                        5<span class="text-green">0</span>0
+                    </h1>
+                    <h1 class="title text-green">
+                        <span>
+                            Internal Server Error
+                        </span>
+                    </h1>
+                    <h2 class="subtitle has-text-grey-light has-text-weight-normal">
+                        Server encountered something unexpected that prevented it from fulfilling the request.
+                    </h2>
+                    <button x-data class="button btn-green is-outlined is-uppercase has-text-weight-medium px-5 py-5" @click="history.back()">
+                        <span class="icon">
+                            <i class="fas fa-arrow-left"></i>
+                        </span>
+                        <span>
+                            Go Back
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </section>
+    </main>
+    @include('assets.js')
+</body>
+
+</html>
