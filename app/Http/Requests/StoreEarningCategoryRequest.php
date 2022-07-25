925ad09e8b4dfbfca2d33b5b19043f2329b3de03
mikiyas <mikiyasleul@gmail.com>
2022-07-25 09:32:42 +0300

    request validation featuures added

---
+++ app/Http/Requests/StoreEarningCategoryRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreEarningCategoryRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'earningCategory' => ['required', 'array'],
+            'earningCategory.*.name' => ['required', 'string', 'distinct', 'max:255', Rule::unique('earning_categories', 'name')->where(function ($query) {
+                return $query->where('company_id', userCompany()->id);
+            })],
+            'earningCategory.*.type' => ['required', 'string'],
+        ];
+    }
+}
