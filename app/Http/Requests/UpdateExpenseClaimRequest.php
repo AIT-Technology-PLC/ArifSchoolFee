3c8bfe5410221629be6512f069c5886977b53374
molla-12 <mollaman19@gmail.com>
2022-07-25 17:37:51 +0300

    store and update expense claim form request created

---
+++ app/Http/Requests/UpdateExpenseClaimRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\MustBelongToCompany;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateExpenseClaimRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'issued_on' => ['required', 'date'],
+            'employee_id' => ['required', 'integer', 'distinct', new MustBelongToCompany('employees')],
+            'description' => ['nullable', 'string'],
+            'expenseClaim' => ['required', 'array'],
+            'expenseClaim.*.item' => ['required', 'string', 'max:255'],
+            'expenseClaim.*.price' => ['required', 'numeric', 'gt:0'],
+        ];
+    }
+}
