e8772553f17350f23a4e427ba0a0959906387eef
molla-12 <mollaman19@gmail.com>
2022-07-26 08:35:51 +0300

    empity controller created

---
+++ app/Http/Controllers/Resource/ExpenseClaimDetailController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Resource;
+
+use App\Http\Controllers\Controller;
+
+class ExpenseClaimDetailController extends Controller
+{
+    public function destroy($id)
+    {
+        //
+    }
+}
