5caad582a3db08cde3283922756fe231f6843342
molla-12 <mollaman19@gmail.com>
2022-08-01 10:49:14 +0300

    migration created

---
+++ database/migrations/2022_08_01_101835_create_company_compensations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('company_compensations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('approved_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->string('name');
+            $table->string('type');
+            $table->boolean('is_taxable');
+            $table->boolean('is_adjustable');
+            $table->boolean('can_be_inputted_manually');
+            $table->decimal('percentage', 22)->nullable();
+            $table->string('default_value');
+            $table->timestamps();
+
+            $table->index('depends_on');
+            $table->index('company_id');
+        });
+        Schema::table('company_compensations', function (Blueprint $table) {
+            $table->foreignId('depends_on')->nullable()->after('id')->constrained('company_compensations')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('company_compensations');
+    }
+};
