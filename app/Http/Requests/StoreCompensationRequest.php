5aaa18f52609bb1a63a2a58d3d704ff0c4f65594
molla-12 <mollaman19@gmail.com>
2022-08-01 11:48:46 +0300

    form request created

---
+++ app/Http/Requests/StoreCompensationRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\MustBelongToCompany;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreCompensationRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'compensation' => ['required', 'array'],
+            'compensation.*.depends_on' => ['required', 'integer', new MustBelongToCompany('company_compensations')],
+            'compensation.*.name' => ['required', 'string', 'max:255'],
+            'compensation.*.type' => ['required', 'string', 'max:255', Rule::In(['earning', 'deduction'])],
+            'compensation.*.is_taxable' => ['required', 'boolean'],
+            'compensation.*.is_adjustable' => ['required', 'boolean'],
+            'compensation.*.can_be_inputted_manually' => ['required', 'boolean'],
+            'compensation.*.percentage' => ['nullable', 'numeric', 'required_unless:depends_on,null'],
+            'compensation.*.default_value' => ['required', 'string', 'max:255'],
+        ];
+    }
+}
