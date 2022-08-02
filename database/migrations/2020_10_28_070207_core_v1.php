6c055add6556e6517cc1b4aadf9e76bf36f90919
molla-12 <mollaman19@gmail.com>
2022-08-02 10:38:22 +0300

    core migration updated

--- database/migrations/2020_10_28_070207_core_v1.php
+++ database/migrations/2020_10_28_070207_core_v1.php
@@ -1202,6 +1202,28 @@ return new class extends Migration
             $table->index('warehouse_id');
         });
 
+        Schema::create('company_compensations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->string('name');
+            $table->string('type');
+            $table->boolean('is_taxable');
+            $table->boolean('is_adjustable');
+            $table->boolean('can_be_inputted_manually');
+            $table->decimal('percentage', 22)->nullable();
+            $table->decimal('default_value', 22)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('company_id');
+        });
+
+        Schema::table('company_compensations', function (Blueprint $table) {
+            $table->foreignId('depends_on')->nullable()->after('id')->constrained('company_compensations')->onDelete('cascade')->onUpdate('cascade');
+        });
+
         Schema::enableForeignKeyConstraints();
     }
 
@@ -1286,5 +1308,6 @@ return new class extends Migration
         Schema::drop('earnings');
         Schema::drop('announcements');
         Schema::drop('announcement_warehouse');
+        Schema::drop('company_compensations');
     }
 };
