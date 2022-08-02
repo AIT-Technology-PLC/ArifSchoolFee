f9dea09fb7193143da8f8aca7f1d53fb1a6c3d86
Nabil Hassen <nabil.hassen08@gmail.com>
2022-08-02 14:53:07 +0300

    create pad status tables

---
+++ database/migrations/2022_08_02_144548_create_pad_statuses_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pad_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pad_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->string('name');
+            $table->string('text_color');
+            $table->string('bg_color');
+            $table->bigInteger('order');
+            $table->boolean('is_editable');
+            $table->boolean('is_deletable');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->string('status')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pad_statuses');
+
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
