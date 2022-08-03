572702424652ec0ae012c1e80c56029ab5b69a5f
molla-12 <mollaman19@gmail.com>
2022-08-03 15:16:44 +0300

    compensationAdjustment and compensationAdjustment models implemented

---
+++ app/Models/CompensationAdjustmentDetail.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\TouchParentUserstamp;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CompensationAdjustmentDetail extends Model
+{
+    use SoftDeletes, TouchParentUserstamp;
+
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    public function compensationAdjustment()
+    {
+        return $this->belongsTo(CompensationAdjustment::class);
+    }
+
+    public function parentModel()
+    {
+        return $this->compensationAdjustment();
+    }
+}
