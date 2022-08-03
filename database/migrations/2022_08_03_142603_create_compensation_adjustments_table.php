10983015b57968b0b48ba364ca39aed233bc618d
molla-12 <mollaman19@gmail.com>
2022-08-03 15:09:34 +0300

    migration created

---
+++ database/migrations/2022_08_03_142603_create_compensation_adjustments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('compensation_adjustments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('approved_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('cancelled_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->string('reference_number');
+            $table->dateTime('issued_on')->nullable();
+            $table->dateTime('starting_period')->nullable();
+            $table->dateTime('ending_period')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['starting_period', 'starting_period']);
+            $table->index('company_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('compensation_adjustments');
+    }
+};
---
+++ database/migrations/2022_08_03_142705_create_compensation_adjustment_details_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('compensation_adjustment_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('compensation_adjustment_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('employee_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('compensation_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->decimal('amount', 22);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['employee_id', 'compensation_id']);
+            $table->index('compensation_adjustment_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('compensation_adjustment_details');
+    }
+};
