4800d01d827c1fe09d210af11c7a8188d54c9c09
Nabil Hassen <nabil.hassen08@gmail.com>
2022-08-07 18:50:47 +0300

    fix bill of materials issue

--- routes/action.php
+++ routes/action.php
@@ -323,7 +323,7 @@ Route::post('/advancements/{advancement}/approve',
 // BOM
 Route::post('/bill-of-materials/{bill_of_material}/approve',
     [Action\BillOfMaterialController::class, 'approve'])
-    ->name('bill_of_materials.approve');
+    ->name('bill-of-materials.approve');
 
 // Expense Claim
 Route::post('/expense-claims/{expense_claim}/approve',
