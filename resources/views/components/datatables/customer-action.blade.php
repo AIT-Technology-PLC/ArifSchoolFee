cdf8681ada167d73801db1e0cadb78b7d15f0163
Nabil Hassen <nabil.hassen08@gmail.com>
2022-08-09 10:16:21 +0300

    refactor buttons

--- resources/views/components/datatables/customer-action.blade.php
+++ resources/views/components/datatables/customer-action.blade.php
@@ -1,24 +1,18 @@
 @if (isFeatureEnabled('Credit Management') && $customer->credits()->sum('credit_amount') != $customer->credits()->sum('credit_amount_settled'))
-    <a
-        href="{{ route('customers.settle', $customer->id) }}"
-        data-title="Settle Customer Credit"
-    >
-        <span class="tag is-white btn-purple is-outlined is-small text-purple has-text-weight-medium">
-            <span class="icon">
-                <i class="fas fa-money-check"></i>
-            </span>
-            <span>
-                Settle Credit
-            </span>
-        </span>
-    </a>
+    @can('Settle Credit')
+        <x-common.button
+            tag="a"
+            href="{{ route('customers.settle', $customer->id) }}"
+            mode="button"
+            data-title="Settle Credit"
+            icon="fas fa-money-check"
+            class="text-green has-text-weight-medium is-not-underlined is-small px-2 py-0 is-transparent-color"
+        />
+    @endcan
 @endif
 
-<x-common.edit-button
-    route="customers.edit"
-    :id="$customer->id"
-/>
-<x-common.delete-button
-    route="customers.destroy"
+<x-common.action-buttons
+    :buttons="['delete', 'edit']"
+    model="customers"
     :id="$customer->id"
 />
