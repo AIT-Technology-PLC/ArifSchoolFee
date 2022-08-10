28558bead8883e1b13669e2025358699c8c07b55
mikiyas <mikiyasleul@gmail.com>
2022-08-10 09:08:55 +0300

    HR warehouse permission control on warning feature

--- app/Http/Requests/UpdateWarningRequest.php
+++ app/Http/Requests/UpdateWarningRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Employee;
 use App\Rules\MustBelongToCompany;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -16,7 +17,11 @@ class UpdateWarningRequest extends FormRequest
     public function rules()
     {
         return [
-            'employee_id' => ['required', 'integer', new MustBelongToCompany('employees')],
+            'employee_id' => ['required', 'integer', new MustBelongToCompany('employees'), function ($attribute, $value, $fail) {
+                if (!authUser()->getAllowedWarehouses('hr')->where('id', Employee::firstWhere('id', $value)->user->warehouse_id)->count()) {
+                    $fail('You do not have permission to modify a warning request of this employee.');
+                }
+            }],
             'type' => ['required', 'string', 'max:255', Rule::in(['Initial Warning', 'Affirmation Warning', 'Final Warning'])],
             'issued_on' => ['required', 'date'],
             'letter' => ['required', 'string'],
