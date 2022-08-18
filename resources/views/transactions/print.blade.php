cf6b14c67e8047a2e7eeb9efd54e3a0dbd716996
Nabil Hassen <nabil.hassen08@gmail.com>
2022-08-18 22:55:39 +0300

    refactor print transactions

--- resources/views/transactions/print.blade.php
+++ resources/views/transactions/print.blade.php
@@ -27,250 +27,177 @@
         href="{{ asset('css/app.css') }}"
         rel="stylesheet"
     >
-    <style>
-        .page-break {
-            page-break-inside: avoid;
-        }
 
-        @media print {
-            .table-breaked {
-                page-break-before: auto;
-            }
-        }
-
-        .summernote-table table td,
-        .summernote-table table th {
-            border-width: 1px !important;
-            padding: 0 !important;
-        }
-
-        td {
-            padding-top: 0.25rem !important;
-            padding-bottom: 0.25rem !important;
-        }
-    </style>
+    @include('assets.print-css')
 </head>
 
-<body>
-    <x-print.header :warehouse="$transaction->warehouse" />
+<body class="{{ userCompany()->hasPrintTemplate() ? 'company-background company-y-padding company-x-padding' : 'px-6' }}">
+    @if (!userCompany()->hasPrintTemplate())
+        <x-print.header :warehouse="$transaction->warehouse" />
+    @endif
 
-    <main>
-        <x-print.customer :customer="$transaction->customer ?? ''" />
+    <hr
+        class="my-0 has-background-grey-lighter"
+        style="margin-left: -10%;margin-right: -10%"
+    >
+    <hr
+        class="my-0 has-background-grey-lighter"
+        style="margin-left: -10%;margin-right: -10%"
+    >
 
-        <hr class="my-0">
+    <x-print.customer :customer="$transaction->customer ?? ''" />
 
-        <section class="is-clearfix has-background-white-bis pl-6 py-3">
-            <aside
-                class="is-pulled-left"
-                style="width: 25% !important"
-            >
-                <h1 class="is-uppercase has-text-grey-light has-text-weight-bold is-size-7">
-                    N<u>o</u>
-                </h1>
-                <h1 class="has-text-black is-size-6 pr-2">
-                    {{ $transaction->code }}
-                </h1>
-            </aside>
-            <aside
-                class="is-pulled-left"
-                style="width: 25% !important"
-            >
-                <h1 class="is-uppercase has-text-grey-light has-text-weight-bold is-size-7">
-                    Issued On
-                </h1>
-                <h1 class="has-text-black is-size-6 pr-2">
-                    {{ $transaction->issued_on->toFormattedDateString() }}
-                </h1>
-            </aside>
-            @if ($transaction->payment_type)
-                <aside
-                    class="is-pulled-left"
-                    style="width: 25% !important"
-                >
-                    <h1 class="is-uppercase has-text-grey-light has-text-weight-bold is-size-7">
-                        Payment Type
-                    </h1>
-                    <h1 class="has-text-black is-size-6 pr-2">
-                        {{ $transaction->payment_type }}
-                    </h1>
-                </aside>
-            @endif
-        </section>
-
-        <section class="pt-5 has-text-centered">
-            <h1 class="is-uppercase has-text-grey-dark has-text-weight-bold is-size-4 is-underlined">
-                {{ $transaction->pad->name }}
+    <section class="is-clearfix py-3">
+        <aside
+            class="is-pulled-left"
+            style="width: 25% !important"
+        >
+            <h1 class="is-uppercase has-text-black-lighter has-text-weight-bold is-underlined is-size-7">
+                N<u>o</u>
             </h1>
-        </section>
+            <h1 class="has-text-black is-size-6 pr-2">
+                {{ $transaction->code }}
+            </h1>
+        </aside>
+        <aside
+            class="is-pulled-left"
+            style="width: 25% !important"
+        >
+            <h1 class="is-uppercase has-text-black-lighter has-text-weight-bold is-underlined is-size-7">
+                Issued On
+            </h1>
+            <h1 class="has-text-black is-size-6 pr-2">
+                {{ $transaction->issued_on->toFormattedDateString() }}
+            </h1>
+        </aside>
+        @if ($transaction->payment_type)
+            <aside
+                class="is-pulled-left"
+                style="width: 25% !important"
+            >
+                <h1 class="is-uppercase has-text-black-lighter has-text-weight-bold is-underlined is-size-7">
+                    Payment Type
+                </h1>
+                <h1 class="has-text-black is-size-6 pr-2">
+                    {{ $transaction->payment_type }}
+                </h1>
+            </aside>
+        @endif
+    </section>
 
-        <section class="px-6 table-breaked">
-            <table class="table is-bordered is-hoverable is-fullwidth is-narrow is-size-7">
-                <thead>
-                    <tr class="is-borderless">
-                        <td
-                            colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) + 1 : count($columns) }}"
-                            class="is-borderless"
-                        >&nbsp;</td>
-                    </tr>
-                    <tr class="is-borderless">
-                        <td
-                            colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) + 1 : count($columns) }}"
-                            class="is-borderless"
-                        >&nbsp;</td>
-                    </tr>
+    <section class="pt-5 has-text-centered">
+        <h1 class="is-uppercase has-text-grey-dark has-text-weight-bold is-size-4 is-underlined">
+            {{ $transaction->pad->name }}
+        </h1>
+    </section>
+
+    <section class="table-breaked has-background-white">
+        <table class="table is-bordered is-hoverable is-fullwidth is-narrow is-size-7">
+            <thead>
+                <tr class="is-borderless">
+                    <td
+                        colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) + 1 : count($columns) }}"
+                        class="is-borderless"
+                    >&nbsp;</td>
+                </tr>
+                <tr class="is-borderless">
+                    <td
+                        colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) + 1 : count($columns) }}"
+                        class="is-borderless"
+                    >&nbsp;</td>
+                </tr>
+                <tr>
+                    <th>#</th>
+                    @foreach ($columns as $column)
+                        @continue (!userCompany()->isDiscountBeforeVAT() && $column == 'discount')
+                        <th>{{ str($column)->replace('_', ' ')->title() }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($transaction->transactionDetails as $detail)
                     <tr>
-                        <th>#</th>
+                        <th>
+                            {{ $loop->iteration }}
+                        </th>
                         @foreach ($columns as $column)
                             @continue (!userCompany()->isDiscountBeforeVAT() && $column == 'discount')
-                            <th>{{ str($column)->replace('_', ' ')->title() }}</th>
+                            <td>
+                                {{ $detail[$column] }}
+                            </td>
                         @endforeach
                     </tr>
-                </thead>
-                <tbody>
-                    @foreach ($transaction->transactionDetails as $detail)
-                        <tr>
-                            <th>
-                                {{ $loop->iteration }}
-                            </th>
-                            @foreach ($columns as $column)
-                                @continue (!userCompany()->isDiscountBeforeVAT() && $column == 'discount')
-                                <td>
-                                    {{ $detail[$column] }}
-                                </td>
-                            @endforeach
-                        </tr>
-                    @endforeach
-                    @if ($transaction->pad->hasPrices())
-                        <tr>
-                            <td
-                                colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) - 1 : count($columns) - 2 }}"
-                                class="is-borderless"
-                            ></td>
-                            <td class="has-text-weight-bold">Sub-Total</td>
-                            <td class="has-text-right">{{ number_format($transaction->subtotalPrice, 2) }}</td>
-                        </tr>
-                        <tr>
-                            <td
-                                colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) - 1 : count($columns) - 2 }}"
-                                class="is-borderless"
-                            ></td>
-                            <td class="has-text-weight-bold">VAT 15%</td>
-                            <td class="has-text-right">{{ number_format($transaction->vat, 2) }}</td>
-                        </tr>
+                @endforeach
+                @if ($transaction->pad->hasPrices())
+                    <tr>
+                        <td
+                            colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) - 1 : count($columns) - 2 }}"
+                            class="is-borderless"
+                        ></td>
+                        <td class="has-text-weight-bold">Sub-Total</td>
+                        <td class="has-text-right">{{ number_format($transaction->subtotalPrice, 2) }}</td>
+                    </tr>
+                    <tr>
+                        <td
+                            colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) - 1 : count($columns) - 2 }}"
+                            class="is-borderless"
+                        ></td>
+                        <td class="has-text-weight-bold">VAT 15%</td>
+                        <td class="has-text-right">{{ number_format($transaction->vat, 2) }}</td>
+                    </tr>
+                    <tr>
+                        <td
+                            colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) - 1 : count($columns) - 2 }}"
+                            class="is-borderless"
+                        ></td>
+                        <td class="has-text-weight-bold">Grand Total</td>
+                        <td class="has-text-right has-text-weight-bold">{{ number_format($transaction->grandTotalPrice, 2) }}</td>
+                    </tr>
+                    @if (!userCompany()->isDiscountBeforeVAT())
                         <tr>
                             <td
                                 colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) - 1 : count($columns) - 2 }}"
                                 class="is-borderless"
                             ></td>
-                            <td class="has-text-weight-bold">Grand Total</td>
-                            <td class="has-text-right has-text-weight-bold">{{ number_format($transaction->grandTotalPrice, 2) }}</td>
+                            <td class="has-text-weight-bold">Discount</td>
+                            <td class="has-text-right has-text-weight-bold">{{ number_format($transaction->discount * 100, 2) }}%</td>
                         </tr>
-                        @if (!userCompany()->isDiscountBeforeVAT())
-                            <tr>
-                                <td
-                                    colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) - 1 : count($columns) - 2 }}"
-                                    class="is-borderless"
-                                ></td>
-                                <td class="has-text-weight-bold">Discount</td>
-                                <td class="has-text-right has-text-weight-bold">{{ number_format($transaction->discount * 100, 2) }}%</td>
-                            </tr>
-                            <tr>
-                                <td
-                                    colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) - 1 : count($columns) - 2 }}"
-                                    class="is-borderless"
-                                ></td>
-                                <td class="has-text-weight-bold">
-                                    Grand Total
-                                    <br>
-                                    <span class="has-text-grey">
-                                        After Discount
-                                    </span>
-                                </td>
-                                <td class="has-text-right has-text-weight-bold">{{ number_format($transaction->grandTotalPriceAfterDiscount, 2) }}</td>
-                            </tr>
-                        @endif
+                        <tr>
+                            <td
+                                colspan="{{ userCompany()->isDiscountBeforeVAT() ? count($columns) - 1 : count($columns) - 2 }}"
+                                class="is-borderless"
+                            ></td>
+                            <td class="has-text-weight-bold">
+                                Grand Total
+                                <br>
+                                <span class="has-text-grey">
+                                    After Discount
+                                </span>
+                            </td>
+                            <td class="has-text-right has-text-weight-bold">{{ number_format($transaction->grandTotalPriceAfterDiscount, 2) }}</td>
+                        </tr>
                     @endif
-                </tbody>
-            </table>
-        </section>
-    </main>
+                @endif
+            </tbody>
+        </table>
+    </section>
 
-    <div style="margin-bottom: 27% !important">&nbsp;</div>
-
-    @if ($transaction->pad->isInventoryOperationSubtract() || $transaction->customer)
-        <footer style="position:absolute;bottom: 14%;left: 0;right: 0;">
-            <aside class="pl-6 my-5">
-                <h1 class="title is-size-7 is-uppercase mb-6">
-                    I received the above goods/services in good condition
-                    <br>
-                </h1>
-                <div style="border: 1px solid lightgrey;width: 50%"></div>
-            </aside>
-        </footer>
-    @endif
-
-    @if ($transaction->createdBy->is($transaction->approvedBy))
-        <footer style="position:absolute;bottom: 0%;left: 0;right: 0;">
-            <aside class="pl-6">
-                <h1 class="is-size-7 is-uppercase has-text-grey-light mt-3">
-                    Prepared & Approved By
-                </h1>
-                <h1 class="has-text-weight-bold has-text-grey-dark is-capitalized">
-                    {{ $transaction->createdBy->name }}
-                </h1>
-                <h1 class="title is-size-7 is-uppercase has-text-grey-light mb-4 mt-5">
-                    <div>
-                        Signature
-                    </div>
-                    <div
-                        class="mt-6"
-                        style="border: 1px solid lightgrey;width: 30%"
-                    ></div>
-                </h1>
-            </aside>
-        </footer>
-    @else
-        <footer style="position:absolute;bottom: 0%;left: 0;right: 0;">
-            <aside class="pl-6">
-                <h1 class="is-size-7 is-uppercase has-text-grey-light mt-3">
-                    Prepared By
-                </h1>
-                <h1 class="has-text-weight-bold has-text-grey-dark is-capitalized">
-                    {{ $transaction->createdBy->name }}
-                </h1>
-                <h1 class="title is-size-7 is-uppercase has-text-grey-light mb-4 mt-5">
-                    <div>
-                        Signature
-                    </div>
-                    <div
-                        class="mt-6"
-                        style="border: 1px solid lightgrey;width: 30%"
-                    ></div>
-                </h1>
-            </aside>
-        </footer>
-        @if ($transaction->pad->isApprovable())
-            <footer style="position:absolute;bottom: 0%;left: 15%;right: 0;margin-left: 40%">
-                <aside class="pl-6">
-                    <h1 class="is-size-7 is-uppercase has-text-grey-light mt-3">
-                        Approved By
-                    </h1>
-                    <h1 class="has-text-weight-bold has-text-grey-dark is-capitalized">
-                        {{ $transaction->approvedBy?->name }}
-                    </h1>
-                    <h1 class="title is-size-7 is-uppercase has-text-grey-light mb-4 mt-5">
-                        <div>
-                            Signature
-                        </div>
-                        <div
-                            class="mt-6"
-                            style="border: 1px solid lightgrey;width: 70%"
-                        ></div>
-                    </h1>
-                </aside>
-            </footer>
+    <footer class="my-6">
+        @if ($transaction->pad->isInventoryOperationSubtract() || $transaction->customer)
+            <h1 class="title is-size-7 is-uppercase">
+                I received the above goods/services in good condition:
+                <span
+                    class="is-inline-block"
+                    style="border: 1px solid lightgrey;width: 20%"
+                ></span>
+            </h1>
         @endif
-    @endif
+    </footer>
+
+    <x-print.user
+        :created-by="$transaction->createdBy ?? null"
+        :approved-by="$transaction->approvedBy ?? null"
+    />
 </body>
 
 </html>
