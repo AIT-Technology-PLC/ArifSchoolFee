d7a21572defa99886ed718a4dd62485899f6d31b
molla-12 <mollaman19@gmail.com>
2022-08-22 15:01:39 +0300

    payroll migration created

---
+++ database/migrations/2022_08_22_145038_create_payrolls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('approved_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('paid_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->bigInteger('code');
+            $table->string('bank');
+            $table->dateTime('issued_on')->nullable();
+            $table->date('starting_period')->nullable();
+            $table->date('ending_period')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['starting_period', 'ending_period', 'company_id']);
+            $table->index('company_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('payrolls');
+    }
+};
