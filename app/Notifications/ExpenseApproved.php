550985f1b138469ac061a31780a8d7bebd7eab87
molla-12 <mollaman19@gmail.com>
2022-08-23 16:33:33 +0300

    ExpenseCreated and Approved notification created

---
+++ app/Notifications/ExpenseApproved.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\WebPush\WebPushChannel;
+use NotificationChannels\WebPush\WebPushMessage;
+
+class ExpenseApproved extends Notification
+{
+    use Queueable;
+
+    public function __construct($expense)
+    {
+        $this->expense = $expense;
+    }
+
+    public function via($notifiable)
+    {
+        return ['database', WebPushChannel::class];
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            'icon' => '',
+            'message' => 'Expense has been approved by ' . ucfirst($this->expense->approvedBy->name),
+            'endpoint' => '/expenses/' . $this->expense->id,
+        ];
+    }
+
+    public function toWebPush($notifiable, $notification)
+    {
+        return (new WebPushMessage)
+            ->title('Expense Approved')
+            ->icon(asset('pwa/pwa-512x512.png'))
+            ->body('Expense has been approved by ' . ucfirst($this->expense->approvedBy->name))
+            ->badge(asset('pwa/pwa-512x512.png'))
+            ->action('View', '/notifications/' . $notification->id)
+            ->vibrate([500, 250, 500, 250]);
+    }
+}
