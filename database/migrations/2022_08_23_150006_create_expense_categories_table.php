e656fddf90557b532ed62d332366eb388276c1cd
molla-12 <mollaman19@gmail.com>
2022-08-23 15:23:31 +0300

    expense_categories, expenses and expense_details migration crated

---
+++ database/migrations/2022_08_23_150006_create_expense_categories_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('expense_categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null')->onUpdate('cascade');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('company_id');
+            $table->unique(['company_id', 'name']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('expense_categories');
+    }
+};
