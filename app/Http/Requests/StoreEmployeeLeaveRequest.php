85224e74758f432e50e8cc4b32643df837f877fc
Nabil Hassen <nabil.hassen08@gmail.com>
2022-08-25 22:39:27 +0300

    fix leave bugs

--- app/Http/Requests/StoreEmployeeLeaveRequest.php
+++ app/Http/Requests/StoreEmployeeLeaveRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Rules\MustBelongToCompany;
+use App\Rules\ValidateTimeOffAmount;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreEmployeeLeaveRequest extends FormRequest
@@ -18,6 +19,8 @@ class StoreEmployeeLeaveRequest extends FormRequest
             'leave_category_id' => ['required', 'integer', new MustBelongToCompany('leave_categories')],
             'starting_period' => ['required', 'date'],
             'ending_period' => ['required', 'date', 'after:starting_period'],
+            'is_paid_time_off' => ['required', 'boolean'],
+            'time_off_amount' => ['nullable', 'numeric', 'required_if:is_paid_time_off,1', new ValidateTimeOffAmount(authUser()->employee->id, $this->input('is_paid_time_off'))],
         ];
     }
 }
