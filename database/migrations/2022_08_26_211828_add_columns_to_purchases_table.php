ff6377bdce0b094d5692a0c43710e4f7a5e3a75c
mikiyas <mikiyasleul@gmail.com>
2022-08-26 21:27:29 +0300

    add columns to purchases table

---
+++ database/migrations/2022_08_26_211828_add_columns_to_purchases_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('purchases', function (Blueprint $table) {
+            $table->decimal('freight_cost', 22)->nullable()->after('exchange_rate');
+            $table->decimal('freight_insurance_cost', 22)->nullable()->after('freight_cost');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('purchases', function (Blueprint $table) {
+            $table->dropColumn('freight_cost');
+            $table->dropColumn('freight_insurance_cost');
+        });
+    }
+};
