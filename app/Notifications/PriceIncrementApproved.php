52b6b1fc6ace3613056c26d2f42641bc6225177d
molla-12 <mollaman19@gmail.com>
2022-10-07 14:57:45 +0300

    notification created

---
+++ app/Notifications/PriceIncrementApproved.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\WebPush\WebPushChannel;
+use NotificationChannels\WebPush\WebPushMessage;
+
+class PriceIncrementApproved extends Notification
+{
+    use Queueable;
+
+    public function __construct($priceIncrement)
+    {
+        $this->priceIncrement = $priceIncrement;
+    }
+
+    public function via($notifiable)
+    {
+        return ['database', WebPushChannel::class];
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            'icon' => 'tags',
+            'message' => 'Price Increment has been approved by ' . ucfirst($this->priceIncrement->approvedBy->name),
+            'endpoint' => '/price-increments/' . $this->priceIncrement->id,
+        ];
+    }
+
+    public function toWebPush($notifiable, $notification)
+    {
+        return (new WebPushMessage)
+            ->title('Price Increment Approved')
+            ->icon(asset('pwa/pwa-512x512.png'))
+            ->body('Price Increment has been approved by ' . ucfirst($this->priceIncrement->approvedBy->name))
+            ->badge(asset('pwa/pwa-512x512.png'))
+            ->action('View', '/notifications/' . $notification->id)
+            ->vibrate([500, 250, 500, 250]);
+    }
+}
