3b7bcba2e158e097bc17fd0c016f907eaaf48f01
molla-12 <mollaman19@gmail.com>
2022-10-11 12:20:15 +0300

    datatable implemented

---
+++ app/DataTables/PriceIncrementDetailDatatable.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\PriceIncrementDetail;
+use App\Traits\DataTableHtmlBuilder;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Services\DataTable;
+
+class PriceIncrementDetailDatatable extends DataTable
+{
+    use DataTableHtmlBuilder;
+
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->editColumn('product', fn($priceIncrementDetail) => $priceIncrementDetail->product->name)
+            ->editColumn('actions', function ($priceIncrementDetail) {
+                return view('components.common.action-buttons', [
+                    'model' => 'price-increment-details',
+                    'id' => $priceIncrementDetail->id,
+                    'buttons' => ['delete'],
+                ]);
+            })
+            ->addIndexColumn();
+    }
+
+    public function query(PriceIncrementDetail $priceIncrementDetail)
+    {
+        return $priceIncrementDetail
+            ->newQuery()
+            ->select('price_increment_details.*')
+            ->with([
+                'product',
+            ]);
+    }
+
+    protected function getColumns()
+    {
+        return [
+            Column::computed('#'),
+            Column::make('product', 'product.name'),
+            Column::computed('actions'),
+        ];
+    }
+
+    protected function filename()
+    {
+        return 'PriceIncrementDetail_' . date('YmdHis');
+    }
+}
