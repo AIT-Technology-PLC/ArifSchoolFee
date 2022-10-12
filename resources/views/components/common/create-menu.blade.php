4958c7e75e84d4a2054b11fde969ac56d0bce2da
molla-12 <mollaman19@gmail.com>
2022-10-12 11:47:41 +0300

    remove price on create menu

--- resources/views/components/common/create-menu.blade.php
+++ resources/views/components/common/create-menu.blade.php
@@ -417,8 +417,8 @@
                 @endcanany
             @endif
 
-            @if (isFeatureEnabled('Sale Management', 'Gdn Management', 'Proforma Invoice', 'Reservation Management', 'Return Management', 'Credit Management', 'Price Management', 'Customer Management', 'Contact Management'))
-                @canany(['Create Sale', 'Create GDN', 'Create Proforma Invoice', 'Create Reservation', 'Create Return', 'Create Credit', 'Create Price', 'Create Customer', 'Create Contact'])
+            @if (isFeatureEnabled('Sale Management', 'Gdn Management', 'Proforma Invoice', 'Reservation Management', 'Return Management', 'Credit Management', 'Customer Management', 'Contact Management'))
+                @canany(['Create Sale', 'Create GDN', 'Create Proforma Invoice', 'Create Reservation', 'Create Return', 'Create Credit', 'Create Customer', 'Create Contact'])
                     <x-content.header>
                         <x-slot name="header">
                             <x-common.icon
