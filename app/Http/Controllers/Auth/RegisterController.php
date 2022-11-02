14f13bcaae5b63a64c3aaed1b9a51a108bc5562d
Nabil Hassen <nabil.hassen08@gmail.com>
2022-11-02 14:42:59 +0300

    refactor create new company as an action

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Actions\CreateUserAction;
+use App\Actions\CreateCompanyAction;
 use App\Http\Controllers\Controller;
-use App\Models\Company;
-use App\Models\Plan;
-use App\Models\Warehouse;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\DB;
@@ -20,7 +17,7 @@ class RegisterController extends Controller
 
     private $action;
 
-    public function __construct(CreateUserAction $action)
+    public function __construct(CreateCompanyAction $action)
     {
         $this->action = $action;
 
@@ -40,106 +37,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return DB::transaction(function () use ($data) {
-            $company = Company::create([
-                'name' => $data['company_name'],
-                'currency' => 'ETB',
-                'enabled' => 0,
-                'plan_id' => Plan::firstWhere('name', 'professional')->id,
-            ]);
-
-            $warehouse = Warehouse::create([
-                'company_id' => $company->id,
-                'name' => 'Main Warehouse',
-                'location' => 'Unknown',
-                'is_sales_store' => 0,
-                'is_active' => 1,
-            ]);
-
-            $user = $this->action->execute([
-                'name' => $data['name'],
-                'email' => $data['email'],
-                'password' => $data['password'],
-                'warehouse_id' => $warehouse->id,
-                'enabled' => 1,
-                'position' => 'Onrica Support Department',
-                'role' => 'System Manager',
-                'gender' => 'female',
-                'address' => 'Gerji, Taxi Tera',
-                'job_type' => 'remote',
-                'phone' => '0976006522',
-            ]);
-
-            $user->employee->company()->associate($company)->save();
-
-            $warehouse->createdBy()->associate($user)->save();
-
-            $warehouse->updatedBy()->associate($user)->save();
-
-            $this->createCompensations($company);
-
-            return $user;
+            return $this->action->execute($data);
         });
-    }
-
-    private function createCompensations($company)
-    {
-        $basicSalaryCompensation = $company->compensations()->create([
-            'name' => 'Basic Salary',
-            'type' => 'earning',
-            'is_active' => 1,
-            'is_taxable' => 1,
-            'is_adjustable' => 0,
-            'can_be_inputted_manually' => 1,
-        ]);
-
-        $company->compensations()->createMany([
-            [
-                'name' => 'Employer Pension Contribution',
-                'depends_on' => $basicSalaryCompensation->id,
-                'type' => 'earning',
-                'is_active' => 1,
-                'is_taxable' => 1,
-                'is_adjustable' => 0,
-                'can_be_inputted_manually' => 0,
-                'percentage' => 11,
-            ],
-            [
-                'name' => 'Overtime',
-                'type' => 'earning',
-                'is_active' => 1,
-                'is_taxable' => 1,
-                'is_adjustable' => 1,
-                'can_be_inputted_manually' => 1,
-            ],
-            [
-                'name' => 'Taxable Transportation Allowance',
-                'type' => 'earning',
-                'is_active' => 1,
-                'is_taxable' => 1,
-                'is_adjustable' => 0,
-                'can_be_inputted_manually' => 1,
-            ],
-            [
-                'name' => 'Non-Taxable Transportation Allowance',
-                'depends_on' => $basicSalaryCompensation->id,
-                'type' => 'earning',
-                'is_active' => 1,
-                'is_taxable' => 0,
-                'is_adjustable' => 0,
-                'can_be_inputted_manually' => 1,
-                'percentage' => 25,
-                'maximum_amount' => 2250,
-            ],
-            [
-                'name' => 'Pension Contribution',
-                'depends_on' => $basicSalaryCompensation->id,
-                'type' => 'deduction',
-                'is_active' => 1,
-                'is_taxable' => 0,
-                'is_adjustable' => 0,
-                'can_be_inputted_manually' => 0,
-                'percentage' => 18,
-            ],
-        ]);
     }
 }
--- database/seeders/CreateNewCompany.php
+++ database/seeders/CreateNewCompany.php
@@ -2,118 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Actions\CreateUserAction;
-use App\Models\Company;
-use App\Models\Plan;
+use App\Actions\CreateCompanyAction;
 use App\Models\User;
-use App\Models\Warehouse;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class CreateNewCompany extends Seeder
 {
-    public function run(Faker $faker, CreateUserAction $action)
+    public function run(Faker $faker, CreateCompanyAction $action)
     {
         DB::transaction(function () use ($faker, $action) {
-            $company = Company::create([
-                'name' => $faker->company,
-                'currency' => 'ETB',
-                'enabled' => 1,
-                'plan_id' => Plan::firstWhere('name', 'professional')->id,
-            ]);
-
-            $warehouse = Warehouse::create([
-                'company_id' => $company->id,
-                'name' => 'Main Warehouse',
-                'location' => 'Unknown',
-                'is_sales_store' => 0,
-                'is_active' => 1,
-            ]);
-
-            $user = $action->execute([
+            $action->execute([
+                'company_name' => $faker->company,
                 'name' => $faker->name,
                 'email' => User::count() ? $faker->unique()->safeEmail : 'admin@onrica.com',
                 'password' => 'password',
-                'warehouse_id' => $warehouse->id,
-                'enabled' => 1,
-                'position' => 'Onrica Support Department',
-                'role' => 'System Manager',
-                'gender' => 'male',
-                'address' => 'Bole',
-                'job_type' => 'full time',
-                'phone' => '0312131415',
             ]);
-
-            $user->employee->company()->associate($company)->save();
-
-            $warehouse->createdBy()->associate($user)->save();
-
-            $warehouse->updatedBy()->associate($user)->save();
-
-            $this->createCompensations($company);
         });
-    }
-
-    private function createCompensations($company)
-    {
-        $basicSalaryCompensation = $company->compensations()->create([
-            'name' => 'Basic Salary',
-            'type' => 'earning',
-            'is_active' => 1,
-            'is_taxable' => 1,
-            'is_adjustable' => 0,
-            'can_be_inputted_manually' => 1,
-        ]);
-
-        $company->compensations()->createMany([
-            [
-                'name' => 'Employer Pension Contribution',
-                'depends_on' => $basicSalaryCompensation->id,
-                'type' => 'earning',
-                'is_active' => 1,
-                'is_taxable' => 1,
-                'is_adjustable' => 0,
-                'can_be_inputted_manually' => 0,
-                'percentage' => 11,
-            ],
-            [
-                'name' => 'Overtime',
-                'type' => 'earning',
-                'is_active' => 1,
-                'is_taxable' => 1,
-                'is_adjustable' => 1,
-                'can_be_inputted_manually' => 1,
-            ],
-            [
-                'name' => 'Taxable Transportation Allowance',
-                'type' => 'earning',
-                'is_active' => 1,
-                'is_taxable' => 1,
-                'is_adjustable' => 0,
-                'can_be_inputted_manually' => 1,
-            ],
-            [
-                'name' => 'Non-Taxable Transportation Allowance',
-                'depends_on' => $basicSalaryCompensation->id,
-                'type' => 'earning',
-                'is_active' => 1,
-                'is_taxable' => 0,
-                'is_adjustable' => 0,
-                'can_be_inputted_manually' => 1,
-                'percentage' => 25,
-                'maximum_amount' => 2250,
-            ],
-            [
-                'name' => 'Pension Contribution',
-                'depends_on' => $basicSalaryCompensation->id,
-                'type' => 'deduction',
-                'is_active' => 1,
-                'is_taxable' => 0,
-                'is_adjustable' => 0,
-                'can_be_inputted_manually' => 0,
-                'percentage' => 18,
-            ],
-        ]);
     }
 }
