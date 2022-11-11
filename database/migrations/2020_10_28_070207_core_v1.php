b9960ccfdd0afa3ccf4920c49e6c359c7411447a
molla-12 <mollaman19@gmail.com>
2022-11-11 12:15:44 +0300

    core migration updated

--- database/migrations/2020_10_28_070207_core_v1.php
+++ database/migrations/2020_10_28_070207_core_v1.php
@@ -1476,6 +1476,22 @@ return new class extends Migration
             $table->index('product_id');
         });
 
+        Schema::create('inventory_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('warehouse_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('product_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->string('model_type');
+            $table->bigInteger('model_id');
+            $table->boolean('is_subtract');
+            $table->decimal('quantity', 22);
+            $table->dateTime('issued_on')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('product_id');
+            $table->index('warehouse_id');
+        });
+
         Schema::enableForeignKeyConstraints();
     }
 
@@ -1574,5 +1590,6 @@ return new class extends Migration
         Schema::drop('brands');
         Schema::drop('payrolls');
         Schema::drop('job_detail_histories');
+        Schema::drop('inventory_histories');
     }
 };
