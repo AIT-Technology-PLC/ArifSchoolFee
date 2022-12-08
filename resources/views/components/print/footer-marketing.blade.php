db27ad9ae5cdde664b1369535f0c87e363dff48e
Nabil Hassen <nabil.hassen08@gmail.com>
2022-12-08 12:10:51 +0300

    fix footer marketing note spacing

--- resources/views/components/print/footer-marketing.blade.php
+++ resources/views/components/print/footer-marketing.blade.php
@@ -1,7 +1,7 @@
-<div style="position:absolute;bottom: 1%;left:0;">
-    <aside class="pl-6">
-        <h1 class="is-uppercase has-text-grey-light has-text-weight-dark is-size-7 mt-1">
-            <div>Powered by Onrica SmartWork | sales@onrica.com | 0908108433/0978147214</div>
+<div style="position:absolute;bottom: 1%;left:23%;">
+    <aside>
+        <h1 class="has-text-grey-light has-text-weight-dark is-size-7 mt-1">
+            Powered by Onrica SmartWork | sales@onrica.com | 0908108433,0978147214
         </h1>
     </aside>
 </div>
