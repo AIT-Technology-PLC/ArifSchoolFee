cc5fc2045b440e6dba3a96546eb72f876984d886
Nabil Hassen <nabil.hassen08@gmail.com>
2023-01-13 21:16:16 +0300

    fix bug in update user action

--- app/Actions/UpdateUserAction.php
+++ app/Actions/UpdateUserAction.php
@@ -33,8 +33,9 @@ class UpdateUserAction
                 Arr::only($data, ['transactions', 'read', 'subtract', 'add', 'sales', 'adjustment', 'siv', 'hr'])
             );
 
-            if (auth()->check() && isFeatureEnabled('Compensation Management') && isset($data['employeeCompensation'])) {
-                $user->employee->employeeCompensations()->forceDelete();
+            $user->employee->employeeCompensations()->forceDelete();
+
+            if (isFeatureEnabled('Compensation Management') && isset($data['employeeCompensation'])) {
                 $user->employee->employeeCompensations()->createMany($data['employeeCompensation']);
             }
 
