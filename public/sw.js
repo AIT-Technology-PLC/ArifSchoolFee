36550784f652d3894f5c837fb1dbcfc8a7ec1d1d
Nabil Hassen <nabil.hassen08@gmail.com>
2023-03-01 13:48:55 +0300

    update sw revision version

--- public/sw.js
+++ public/sw.js
@@ -10,7 +10,7 @@ const { registerRoute, setCatchHandler } = workbox.routing;
 skipWaiting();
 clientsClaim();
 
-const VERSION = 96;
+const VERSION = 97;
 const PRECACHE = "precache-v5";
 const RUNTIME = "runtime-v5";
 
