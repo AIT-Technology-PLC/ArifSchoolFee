6ead6341fdb139665a1b5f34ceda90f01408f2a5
molla-12 <mollaman19@gmail.com>
2023-03-23 16:56:30 +0300

    inventory transfers report

---
+++ app/Http/Controllers/Report/InventoryTransferReportController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Report;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\InventoryTransferReportRequest;
+use App\Models\Employee;
+use App\Models\Product;
+use App\Reports\InventoryTransferReport;
+
+class InventoryTransferReportController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('isFeatureAccessible:Inventory Transfer Report');
+    }
+
+    public function index(InventoryTransferReportRequest $request)
+    {
+        abort_if(authUser()->cannot('Read Inventory Transfer Report'), 403);
+
+        $warehouses = authUser()->getAllowedWarehouses('transactions');
+
+        $users = Employee::with('user:id,name')->get()->pluck('user')->sortBy('name');
+
+        $products = Product::whereHas('transferDetails')->get()->sortBy('name');
+
+        $inventoryTransferReport = new InventoryTransferReport($request->validated());
+
+        return view('reports.inventory-transfer', compact('warehouses', 'inventoryTransferReport', 'users', 'products'));
+    }
+}
