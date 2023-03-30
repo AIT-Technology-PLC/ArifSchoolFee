8b852d44cf2eb49b8011861048b1cc7273016b3a
molla-12 <mollaman19@gmail.com>
2023-03-30 14:15:26 +0300

    adjustment , damage ,gdn, grn service update

--- app/Services/Models/DamageService.php
+++ app/Services/Models/DamageService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Models;
 
+use App\Actions\ApproveTransactionAction;
 use App\Services\Inventory\InventoryOperationService;
 use Illuminate\Support\Facades\DB;
 
@@ -29,6 +30,38 @@ class DamageService
         }
 
         DB::transaction(function () use ($damage) {
+            InventoryOperationService::subtract($damage->damageDetails, $damage);
+
+            $damage->subtract();
+        });
+
+        return [true, ''];
+    }
+
+    public function approveAndSubtract($damage, $user)
+    {
+        if (!$user->hasWarehousePermission('subtract',
+            $damage->damageDetails->pluck('warehouse_id')->toArray())) {
+            return [false, 'You do not have permission to subtract from one or more of the warehouses.'];
+        }
+
+        if ($damage->isApproved()) {
+            return [false, 'This damage is already approved.'];
+        }
+
+        if ($damage->isSubtracted()) {
+            return [false, 'This damage is already subtracted from inventory'];
+        }
+
+        $unavailableProducts = InventoryOperationService::unavailableProducts($damage->damageDetails);
+
+        if ($unavailableProducts->isNotEmpty()) {
+            return [false, $unavailableProducts];
+        }
+
+        DB::transaction(function () use ($damage) {
+            (new ApproveTransactionAction)->execute($damage);
+
             InventoryOperationService::subtract($damage->damageDetails, $damage);
 
             $damage->subtract();
