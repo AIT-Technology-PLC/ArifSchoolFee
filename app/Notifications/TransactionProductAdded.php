a145bd5a4be8ecf12d07cad306c484d8b14c8041
Nabil Hassen <nabil.hassen08@gmail.com>
2023-08-04 12:06:17 +0300

    fix transaction pad notification

--- app/Notifications/TransactionProductAdded.php
+++ app/Notifications/TransactionProductAdded.php
@@ -26,7 +26,7 @@ class TransactionProductAdded extends Notification
     {
         return [
             'icon' => $this->transactionDetail['transaction']->pad->icon,
-            'message' => Product::find($this->transactionDetail['product'])->name . ' in ' . str()->singular($this->transactionDetail['transaction']->pad->name) . ' #' . $this->transactionDetail['transaction']->code . ' is added to inventory by ' . authUser()->name,
+            'message' => Product::find($this->transactionDetail['product_id'])->name . ' in ' . str()->singular($this->transactionDetail['transaction']->pad->name) . ' #' . $this->transactionDetail['transaction']->code . ' is added to inventory by ' . authUser()->name,
             'endpoint' => '/transactions/' . $this->transactionDetail['transaction']->id,
         ];
     }
@@ -36,7 +36,7 @@ class TransactionProductAdded extends Notification
         return (new WebPushMessage)
             ->title('Transaction Product Added')
             ->icon(asset('pwa/pwa-512x512.png'))
-            ->body(Product::find($this->transactionDetail['product'])->name . ' in ' . str()->singular($this->transactionDetail['transaction']->pad->name) . ' #' . $this->transactionDetail['transaction']->code . ' is added to inventory by ' . authUser()->name)
+            ->body(Product::find($this->transactionDetail['product_id'])->name . ' in ' . str()->singular($this->transactionDetail['transaction']->pad->name) . ' #' . $this->transactionDetail['transaction']->code . ' is added to inventory by ' . authUser()->name)
             ->badge(asset('pwa/pwa-512x512.png'))
             ->action('View', '/notifications/' . $notification->id)
             ->vibrate([500, 250, 500, 250]);
