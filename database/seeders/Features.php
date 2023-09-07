9b0d24ea46d842fddc7859d643ed3ba9df1aee13
Nabil Hassen <nabil.hassen08@gmail.com>
2023-09-07 22:10:33 +0300

    disable cost update feature by default

--- database/seeders/Features.php
+++ database/seeders/Features.php
@@ -282,7 +282,7 @@ class Features extends Seeder
 
             Feature::updateOrCreate(
                 ['name' => 'Cost Update Management'],
-                ['is_enabled' => 1]
+                ['is_enabled' => 0]
             );
 
             $standard = Plan::firstWhere('name', 'standard');
