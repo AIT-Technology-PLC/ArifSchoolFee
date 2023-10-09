74472f3ba2d7e6232d1751fd75dd10f726fbd8d8
Nabil Hassen <nabil.hassen08@gmail.com>
2023-10-09 13:13:12 +0300

    add telebirr payment method

--- public/sw.js
+++ public/sw.js
@@ -10,7 +10,7 @@ const { registerRoute, setCatchHandler } = workbox.routing;
 skipWaiting();
 clientsClaim();
 
-const VERSION = 110;
+const VERSION = 112;
 const PRECACHE = "precache-v5";
 const RUNTIME = "runtime-v5";
 
