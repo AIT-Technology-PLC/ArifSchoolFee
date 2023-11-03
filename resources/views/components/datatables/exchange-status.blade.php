532bc8eec151069bced0bc5ec8a1f3425567a37a
molla-12 <mollaman19@gmail.com>
2023-11-03 23:50:43 +0300

    exchange status .index and show page implemented

---
+++ resources/views/components/datatables/exchange-status.blade.php
@@ -0,0 +1,28 @@
+@if (!$exchange->isApproved())
+    <span class="tag is-small bg-purple has-text-white">
+        <span class="icon">
+            <i class="fas fa-clock"></i>
+        </span>
+        <span>
+            Waiting Approval
+        </span>
+    </span>
+@elseif ($exchange->isExecuted())
+    <span class="tag is-small bg-green has-text-white">
+        <span class="icon">
+            <i class="fas fa-check-circle"></i>
+        </span>
+        <span>
+            Executed
+        </span>
+    </span>
+@else
+    <span class="tag is-small bg-gold has-text-white">
+        <span class="icon">
+            <i class="fas fa-exclamation-circle"></i>
+        </span>
+        <span>
+            Approved (not Executed)
+        </span>
+    </span>
+@endif
