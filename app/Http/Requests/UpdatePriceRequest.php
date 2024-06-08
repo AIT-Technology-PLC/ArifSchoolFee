cdb833e3b539591e2f6571ddc47d86da03cb8b56
Nabil Hassen <nabil.hassen08@gmail.com>
2024-06-08 08:24:35 +0300

    fix: require prices array

--- app/Http/Requests/UpdatePriceRequest.php
+++ app/Http/Requests/UpdatePriceRequest.php
@@ -16,6 +16,7 @@ class UpdatePriceRequest extends FormRequest
     public function rules()
     {
         return [
+            'price' => ['required', 'array'],
             'price.*.product_id' => ['required', 'integer', new MustBelongToCompany('products')],
             'price.*.fixed_price' => ['required', 'numeric', 'gt:0', 'max:99999999999999999999.99', new ValidatePriceBelowCost],
             'price.*.name' => ['nullable', 'string'],
